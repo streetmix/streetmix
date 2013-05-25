5b535b9cec7099912deccc671bcb02ccc80c7300
ycombinator <ycombinator@gmail.com>
2013-05-25 09:23:43 -0700

    Adding REST API config for staging env.

--- config/staging.js
+++ config/staging.js
@@ -1,3 +1,4 @@
 module.exports = {
-  app_host_port: 'streetmix-staging.herokuapp.com'
+  app_host_port: 'streetmix-staging.herokuapp.com',
+  restapi_baseuri: 'http://streetmix-api-staging.herokuapp.com'
 }
