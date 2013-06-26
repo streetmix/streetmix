16c069eb3c8749c241afd3b59050b67393cda431
ycombinator <ycombinator@gmail.com>
2013-06-26 05:17:22 -0700

    Revert "Testing domain issue with requests to API."
    This reverts commit 1f706d227451535688524c51b7cb09ff651d17f7.

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  app_host_port: 'streetmix-v2.herokuapp.com',
+  app_host_port: 'streetmix.net',
   header_host_port: 'streetmix-v2.herokuapp.com',
   restapi_baseuri: 'http://streetmix-api.herokuapp.com',
   facebook_app_id: '162729607241489'
