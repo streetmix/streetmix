f6cf270c5a46eaa7b781a13ea1a2028086aef9bb
ycombinator <ycombinator@gmail.com>
2013-07-24 10:38:12 -0700

    Revert "Removing sandbox environment."
    This reverts commit 47fc5260288f45a9167371abad9701fbe225518f.

---
+++ config/sandbox.js
@@ -0,0 +1,5 @@
+module.exports = {
+  app_host_port: 'streetmix-sandbox.herokuapp.com',
+  header_host_port: 'streetmix-sandbox.herokuapp.com',
+  restapi_baseuri: 'http://streetmix-api-sandbox.herokuapp.com'
+}
