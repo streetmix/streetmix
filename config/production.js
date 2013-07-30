9be6ef223a409cd3a1c1f81215a1a407b51ebb65
ycombinator <ycombinator@gmail.com>
2013-07-30 14:30:28 -0700

    Pointing API to new API.

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   app_host_port: 'streetmix.net',
   header_host_port: 'streetmix-v2.herokuapp.com',
-  restapi_baseuri: 'http://streetmix-api.herokuapp.com',
+  restapi_baseuri: 'http://streetmix-api-v2.herokuapp.com',
   facebook_app_id: '162729607241489',
   google_analytics_account: 'UA-38087461-1'
 }
