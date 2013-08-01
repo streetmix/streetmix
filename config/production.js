68c624e275fc8efd200d237f5991582be0f9bf9c
ycombinator <ycombinator@gmail.com>
2013-08-01 16:17:15 -0700

    Updating expected header host port in production.

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   app_host_port: 'streetmix.net',
-  header_host_port: 'streetmix-v2.herokuapp.com',
+  header_host_port: 'streetmix.net',
   restapi_baseuri: 'http://streetmix-api-v2.herokuapp.com',
   facebook_app_id: '162729607241489',
   google_analytics_account: 'UA-38087461-1'
