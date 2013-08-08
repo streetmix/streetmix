c746d422b036b4a9d8f2b87c49887f40ab836ab7
Marcin Wichary <mwichary@gmail.com>
2013-08-08 15:54:58 -0700

    Welcome message if you arrive at Streetmix looking at someone else’s street instead of new street.

--- public/js/scripts.mustache.js
+++ public/js/scripts.mustache.js
@@ -5993,32 +5993,72 @@ var main = (function(){
     _saveStreetToServer(false);
   }
 
+  // TODO move
+  var WELCOME_NONE = 0;
+  var WELCOME_NEW_STREET = 1;
+  var WELCOME_FIRST_TIME_NEW_STREET = 2;
+  var WELCOME_FIRST_TIME_EXISTING_STREET = 3;
+
   function _showWelcome() {
-    var firstTime = true;
+    var welcomeType = WELCOME_NONE;
 
-    _loadSettingsWelcomeDismissed();
+     _loadSettingsWelcomeDismissed();
 
-    if (signedIn || settingsWelcomeDismissed) {
-      firstTime = false;
+    if (mode == MODE_NEW_STREET) {
+      if (signedIn || settingsWelcomeDismissed) {
+        welcomeType = WELCOME_NEW_STREET;
+      } else {
+        welcomeType = WELCOME_FIRST_TIME_NEW_STREET;
+      }
+    } else {
+      if (!settingsWelcomeDismissed) {
+        welcomeType = WELCOME_FIRST_TIME_EXISTING_STREET;
+      }
     }
 
-    if (firstTime) {
-      document.querySelector('#welcome').classList.add('first-time');
-    } else {
-      document.querySelector('#welcome').classList.add('next-time');
 
-      switch (settings.newStreetPreference) {
-        case NEW_STREET_EMPTY:
-          document.querySelector('#new-street-empty').checked = true;
-          break;
-        case NEW_STREET_DEFAULT:
-          document.querySelector('#new-street-default').checked = true;
-          break;
-      }
+    if (welcomeType == WELCOME_NONE) {
+      return;
+    }
 
-      if (settings.priorLastStreetId && settings.priorLastStreetId != street.id) {
-        document.querySelector('#new-street-last').parentNode.classList.add('visible');
-      }
+    switch (welcomeType) {
+      case WELCOME_FIRST_TIME_NEW_STREET:
+        document.querySelector('#welcome').classList.add('first-time-new-street');
+        break;
+      case WELCOME_FIRST_TIME_EXISTING_STREET:
+        document.querySelector('#welcome').classList.add('first-time-existing-street');
+
+        document.querySelector('#welcome-new-street').addEventListener('click', function() {
+          settingsWelcomeDismissed = true;
+          _saveSettingsWelcomeDismissed();
+          _goNewStreet(true);
+        });
+
+        $('#welcome-street-name').text(street.name);
+
+        if (street.creatorId) {
+          document.querySelector('#welcome-avatar-creator').classList.add('visible');
+          $('#welcome-avatar').attr('userId', street.creatorId);
+          $('#welcome-creator').text(street.creatorId);
+        }
+        _fetchAvatars();
+        break;
+      case WELCOME_NEW_STREET:
+        document.querySelector('#welcome').classList.add('new-street');
+
+        switch (settings.newStreetPreference) {
+          case NEW_STREET_EMPTY:
+            document.querySelector('#new-street-empty').checked = true;
+            break;
+          case NEW_STREET_DEFAULT:
+            document.querySelector('#new-street-default').checked = true;
+            break;
+        }
+
+        if (settings.priorLastStreetId && settings.priorLastStreetId != street.id) {
+          document.querySelector('#new-street-last').parentNode.classList.add('visible');
+        }
+        break;
     }
 
     document.querySelector('#welcome').classList.add('visible');
@@ -8646,13 +8686,11 @@ var main = (function(){
 
   function _onEverythingLoaded() {
     switch (mode) {
-      case MODE_NEW_STREET:
-        _showWelcome();
-        break;
       case MODE_NEW_STREET_COPY_LAST:
         _onNewStreetLastClick();
         break;
     }
+    _showWelcome();
 
     _onResize();
     _resizeStreetWidth();
@@ -9375,8 +9413,12 @@ var main = (function(){
     location.href = '/' + URL_SIGN_IN_REDIRECT;
   }
 
-  function _goNewStreet() {
-    location.href = '/' + URL_NEW_STREET;
+  function _goNewStreet(sameWindow) {
+    if (sameWindow) {
+      location.replace('/' + URL_NEW_STREET);
+    } else {
+      location.href = '/' + URL_NEW_STREET;
+    }
   }
 
   function _goExampleStreet() {
