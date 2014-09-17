2f176e80144995b2a2998ae6724fcc232d185b89
Drew Dara-Abrams <dda@dara-abrams.com>
2014-09-17 10:54:25 -0700

    dividing gallery code

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,7 @@
 //= require_tree app
 //= require_tree segments
 //= require_tree streets
+//= require_tree gallery
 //= require_tree users
 
 var app = {};
--- assets/js/gallery/view.js
+++ assets/js/gallery/view.js
@@ -1,3 +1,8 @@
+var THUMBNAIL_WIDTH = 180;
+var THUMBNAIL_HEIGHT = 110;
+var THUMBNAIL_MULTIPLIER = .1 * 2;
+var BACKGROUND_DIRT_COLOUR = 'rgb(53, 45, 39)';
+
 var TRACK_ACTION_OPEN_GALLERY = 'Open gallery';
 
 var galleryVisible = false;
@@ -6,94 +11,8 @@ var galleryUserId = null;
 var galleryStreetId = null;
 var galleryStreetLoaded = false;
 
-function _fetchGalleryData() {
-  if (galleryUserId) {
-    $.ajax({
-      // TODO const
-      url: API_URL + 'v1/users/' + galleryUserId + '/streets',
-      dataType: 'json',
-      type: 'GET',
-      headers: { 'Authorization': _getAuthHeader() }
-    }).done(_receiveGalleryData).fail(_errorReceiveGalleryData);
-  } else {
-    $.ajax({
-      // TODO const
-      url: API_URL + 'v1/streets?count=200',
-      dataType: 'json',
-      type: 'GET'
-    }).done(_receiveGalleryData).fail(_errorReceiveGalleryData);
-  }
-}
-
-function _errorReceiveGalleryData(data) {
-  if ((mode == MODES.USER_GALLERY) && (data.status == 404)) {
-    mode = MODES.NOT_FOUND;
-    _processMode();
-    _hideGallery(true);
-  } else {
-    document.querySelector('#gallery .loading').classList.remove('visible');
-    document.querySelector('#gallery .error-loading').classList.add('visible');
-  }
-}
-
 function _repeatReceiveGalleryData() {
   _loadGalleryContents();
-}
-
-function _fetchGalleryStreet(streetId) {
-  _showBlockingShield();
-
-  $.ajax({
-    // TODO const
-    url: API_URL + 'v1/streets/' + streetId,
-    dataType: 'json',
-    type: 'GET',
-    headers: { 'Authorization': _getAuthHeader() }
-  }).done(_receiveGalleryStreet)
-  .fail(_errorReceiveGalleryStreet);
-}
-
-function _errorReceiveGalleryStreet() {
-  _hideBlockingShield();
-  galleryStreetLoaded = true;
-  galleryStreetId = street.id;
-
-  _updateGallerySelection();
-}
-
-// TODO similar to receiveLastStreet
-function _receiveGalleryStreet(transmission) {
-  if (transmission.id != galleryStreetId) {
-    return;
-  }
-
-  galleryStreetLoaded = true;
-
-  _hideBlockingShield();
-
-  ignoreStreetChanges = true;
-
-  _hideError();
-
-  _unpackServerStreetData(transmission, null, null, true);
-
-  _propagateUnits();
-
-  _recalculateOccupiedWidth();
-
-  // TODO this is stupid, only here to fill some structures
-  _createDomFromData();
-  _createDataFromDom();
-
-  _hideWelcome();
-  _resizeStreetWidth();
-  _updateStreetName();
-  _createDomFromData();
-  _segmentsChanged();
-  _updateShareMenu();
-
-  ignoreStreetChanges = false;
-  lastStreet = _trimStreetData(street);
 }
 
 function _updateGallerySelection() {
