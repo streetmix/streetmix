839b3e9dc9ec95774e43cc55be7cc7d1706ae29c
Drew Dara-Abrams <dda@dara-abrams.com>
2014-09-21 16:17:04 -0700

    set up for testing mail

---
+++ config/initializers/mail_config.rb
@@ -0,0 +1,16 @@
+if Rails.env.test?
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+else
+  ActionMailer::Base.smtp_settings = {
+    user_name: Figaro.env.sendgrid_username,
+    password: Figaro.env.sendgrid_password,
+    domain: 'codeforamerica.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
--- config/initializers/smtp_config.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  user_name: Figaro.env.sendgrid_username,
-  password: Figaro.env.sendgrid_password,
-  domain: 'codeforamerica.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication: :plain,
-  enable_starttls_auto: true
-}
