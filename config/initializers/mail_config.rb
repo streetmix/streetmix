cc5b66dfa6e32f2024ecfdc1af98eb2ff7163b82
Drew Dara-Abrams <dda@dara-abrams.com>
2014-09-22 06:52:04 -0700

    fix mail_config
    failed on Travis: https://travis-ci.org/codeforamerica/streetmix/builds/35904113
    
        NameError: undefined local variable or method `config' for main:Object
        /home/travis/build/codeforamerica/streetmix/config/initializers/mail_config.rb:5:in `<top (required)>'

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,8 +1,10 @@
 if Rails.env.test?
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  Streetmix::Application.configure do
+    # Tell Action Mailer not to deliver emails to the real world.
+    # The :test delivery method accumulates sent emails in the
+    # ActionMailer::Base.deliveries array.
+    config.action_mailer.delivery_method = :test
+  end
 else
   ActionMailer::Base.smtp_settings = {
     user_name: Figaro.env.sendgrid_username,
