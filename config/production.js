6f8d13468a174efa786863943b5cd2944a7751b6
Drew Dara-Abrams <dda@dara-abrams.com>
2014-09-27 12:41:49 -0700

    switch production to `www.streetmix.net` #466

--- config/production.js
+++ config/production.js
@@ -1,9 +1,9 @@
 module.exports = {
-  app_host_port: 'streetmix.net',
-  header_host_port: 'streetmix.net',
-  restapi_baseuri: 'http://streetmix.net/api',
+  app_host_port: 'www.streetmix.net',
+  header_host_port: 'www.streetmix.net',
+  restapi_baseuri: 'http://www.streetmix.net/api',
   restapi: {
-    baseuri: 'http://streetmix.net/api'
+    baseuri: 'http://www.streetmix.net/api'
   },
   facebook_app_id: '162729607241489',
   google_analytics_account: 'UA-38087461-1'
