0cf49dd75c6f99b6d639dc35c2c1bd20ce07e823
Lou Huang <lou@louhuang.com>
2014-11-05 17:44:31 -0500

    Add a version of the translate API that allows Streetmix to pull live data from Transifex

--- app/resources/v1/translate.js
+++ app/resources/v1/translate.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var config = require('config'),
-    fs     = require('fs'),
-    logger = require('../../../lib/logger.js')()
+var config  = require('config'),
+    fs      = require('fs'),
+    btoa    = require('btoa'),
+    request = require('superagent'),
+    logger  = require('../../../lib/logger.js')()
 
 exports.get = function (req, res) {
 
@@ -30,13 +32,48 @@ exports.get = function (req, res) {
 
     })
 
-  } // END function - handleGetlanguage
+  } // END function - handleGetTranslation
+
+  // Use the Transifex API
+  var handleGetFromTransifex = function (locale) {
+
+    if (!process.env.TRANSIFEX_USERNAME || !process.env.TRANSIFEX_PASSWORD) {
+      res.status(501).json({status: 501, msg: 'Language API is not correctly implemented.'})
+      return
+    }
+
+    var authToken  = btoa(process.env.TRANSIFEX_USERNAME + ':' + process.env.TRANSIFEX_PASSWORD),
+        apiBaseURI = 'https://www.transifex.com/api/2/project/streetmix/resource/main/translation/'
+
+    request
+      .get(apiBaseURI + locale)
+      .set('Authorization', 'Basic ' + authToken)
+      .set('Accept', 'application/json')
+      .end(function (err, data) {
+        if (err) {
+          res.status(500).json({status: 500, msg: 'There was an error retrieving the language.'})
+          return
+        }
+
+        var translation = JSON.parse(data.body.content)
+
+        res.set({
+          'Content-Type': 'application/json; charset=utf-8',
+          'Location': config.restapi.baseuri + '/v1/translate/' + locale,
+          'Cache-Control': 'max-age=86400'
+        })
+
+        res.status(200).send(translation)
+      })
+
+  } // END function - handleGetFromTransifex
 
   if (!req.params.locale_code) {
     res.status(400).json({status: 400, msg: 'Please provide locale code.'})
     return
   }
 
-  handleGetTranslation(req.params.locale_code)
+  //handleGetTranslation(req.params.locale_code)
+  handleGetFromTransifex(req.params.locale_code)
 
 }
