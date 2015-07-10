9b22064ec5c7b8bc1682f189f4759aabb3298247
Lou Huang <lou@louhuang.com>
2015-07-10 08:54:02 -0400

    Messing around with JSDoc and documentation.js

--- assets/js/app/keypress.js
+++ assets/js/app/keypress.js
@@ -1,8 +1,15 @@
-/*
+/**
  *  Keypress (singleton)
  *
  *  Handles registration, removal, and processing of keyboard inputs.
- *  Some inspiration from Keypress Javascript library (http://dmauro.github.io/Keypress/)
+ *  Some inspiration:
+ *    - Mousetrap.js (https://craig.is/killing/mice)
+ *    - Keypress.js (http://dmauro.github.io/Keypress/)
+ *    - keymaster.js (https://github.com/madrobby/keymaster)
+ *
+ *  @module Keypress
+ *  @requires EventTracking
+ *  @exports Keypress
  */
 
 // module.exports = Keypress
@@ -68,18 +75,89 @@ var Keypress = (function () {
   // Keep track of all registered commands here
   var inputs = {}
 
-  // Set global listener for keypress
+  // Utility functions
+  var noop = function () {}
+  var returnTrue = function () { return true }
+
+  /**
+   *  Initiates keypress manager. Sets a global event listener on the window.
+   *  This should only have to be called once, when the application bootstraps.
+   *
+   *  @public
+   */
   function init () {
     // TODO: remove useCapture if we don't need it
     window.addEventListener('keydown', _onGlobalKeyDown, true)
   }
 
-  // Placeholder utility functions
-  function noop () {}
-
-  function returnTrue () { return true }
-
-  // (public) Registers a key command listener with the Keypress Manager
+  /**
+   *  Registers a key command listener with the Keypress Manager
+   *
+   *  @public
+   *  @example Keypress.register('shift d', { trackMsg: 'Shift-D is pressed' }, function () { console.log('Shift-D is pressed!') })
+   *  @example Keypress.register('esc', hide)
+   *  @param {(string|string[])} commands
+   *    Human readable key or key combination to listen for, in the form of "a"
+   *    or "shift a" or "control alt a". If multiple keys should perform the
+   *    same action, pass in an array of strings, e.g. `['a', 'b', 'meta d']`
+   *  @param {object} [options]
+   *    Options that fine tune the behavior of the keypress. If you are unhappy
+   *    with a default setting, they can be overridden here. Note that it is
+   *    possible for overrides to conflict with other parameters, for instance,
+   *    if you set `commands` to "shift p" but `options.shiftKey` to `false`.
+   *    Don't do this. This is confusing and I won't guarantee preserving how
+   *    this conflict is addressed.
+   *  @param {(boolean|string)} [options.shiftKey=false]
+   *    If `true`, the `Shift` key should be pressed in a key combination. This
+   *    is automatically set to `true` if `shift` is specified in the `commands`
+   *    parameter. The boolean test is strict. The string 'optional' can be
+   *    passed so that the key will fire regardless of whether `Shift` is
+   *    pressed.
+   *  @param {(boolean|string)} [options.metaKey=false]
+   *    If `true`, the "meta" key (`Command` or `Control`, depending on the OS)
+   *    should be pressed in a key combination. This is automatically set to
+   *    `true` if `meta`, `control`, or `command` is specified in the `commands`
+   *    parameter. The boolean test is strict. The string 'optional' can be
+   *    passed so that the key will fire regardless of whether `Command` or
+   *    `Control` is pressed.
+   *  @param {(boolean|string)} [options.altKey=false]
+   *    If `true`, the `Alt` key should be pressed in a key combination. This
+   *    is automatically set to `true` if `alt` is specified in the `commands`
+   *    parameter. The boolean test is strict. The string 'optional' can be
+   *    passed so that the key will fire regardless of whether `Alt` is pressed.
+   *  @param {(boolean)} [options.preventDefault=true]
+   *    If `true`, `event.preventDefault()` should be called to prevent the
+   *    key's default behavior.
+   *  @param {(boolean)} [options.stopPropagation=false]
+   *    If `true`, `event.stopPropagation()` should be called to prevent other
+   *    key handlers from triggering. Defaults to `false`, but as a special
+   *    case, this is automatically set to `true` if the command is `esc`.
+   *  @param {(boolean)} [options.requireFocusOnBody=true]
+   *    If `true`, the key handler is not triggered if the browser has focused
+   *    on a specific element, like an input field.  Defaults to `true`, but as
+   *    a special case, this is automatically set to `false` if the command is
+   *    `esc`.
+   *  @param {(string)} [options.trackMsg=null]
+   *    If a string is provided, the action is logged in event tracking when
+   *    triggered.
+   *  @param {(number)} [options.trackValue=null]
+   *    If set, and an event is logged, this fills in the `value` property of a
+   *    tracked event in Google Analytics.
+   *  @param {(boolean)} [options.trackOnce=true]
+   *    If `true`, and an event is logged, further identical actions will not
+   *    be logged.
+   *  @param {(function)} [options.onKeyPress]
+   *    It is possible to set the callback function to execute on key press on
+   *    the `options` object instead of in the `callback` parameter.
+   *  @param {(function)} [options.condition]
+   *    If additional conditions are required before executing a callback
+   *    function after a key is pressed, this function is evaluated. It must
+   *    return a `true` or truthy value for the callback to execute.
+   *  @param {function} [callback]
+   *    Function to execute when key is pressed. Technically, this is optional
+   *    (and you might prefer to set it on `options` instead). If there is no
+   *    callback function the keypress simply does nothing.
+   */
   function register (commands, options, callback) {
     // Defaults
     // For shiftKey, metaKey, and altKey, specifies what it should
@@ -158,8 +236,24 @@ var Keypress = (function () {
     }
   }
 
-  // If command and callback matches, get rid of it.
-  // Callback is optional. Without one, deregister any command that matches, regardless of command.
+  /**
+   *  Deregisters a key command listener, given matching `commands` and
+   *  `callback` parameters.
+   *
+   *  @public
+   *  @example Keypress.deregister('shift d') // Deregisters all triggers for `shift d`
+   *  @example Keypress.deregister('esc', hide) // Deregisters triggers matching callback function `hide` and key `esc`
+   *  @param {(string|string[])} commands
+   *    Human readable key or key combination to listen for, in the form of "a"
+   *    or "shift a" or "control alt a". If multiple keys should perform the
+   *    same action, pass in an array of strings, e.g. `['a', 'b', 'meta d']`
+   *  @param {function} [callback]
+   *    Callback function to execute when key is pressed. This is if you want
+   *    to only remove key handlers that match the same callback. If you do not
+   *    provide a callback, all handlers that match `commands` are removed.
+   *  @todo Because of how function equality works, not all functions passed
+   *    in this way result in a true test of equality.
+   */
   function deregister (commands, callback) {
     var commandObj = _processCommands(commands)
 
@@ -195,6 +289,17 @@ var Keypress = (function () {
     }
   }
 
+
+  /**
+   *  Processes commands
+   *
+   *  @private
+   *  @param {(string|string[])} commands
+   *    Human readable key or key combination to listen for, in the form of "a"
+   *    or "shift a" or "control alt a". If multiple keys should perform the
+   *    same action, pass in an array of strings, e.g. `['a', 'b', 'meta d']`
+   *  //@returns object
+   */
   function _processCommands (commands) {
     // If a string, force to one-element array, otherwise expect an array of strings
     if (typeof commands === 'string') {
@@ -281,25 +386,38 @@ var Keypress = (function () {
       }
     }
 
-    // Exit now if an input is not found
-    if (!input) return
+    // Execute input's callback, if found
+    if (input) {
+      _execute(input, event)
+    }
+  }
+
+  /**
+   *  Executes an input's callback function
+   *
+   *  @private
+   *  @param {object} input - The input object to execute
+   *  @param {Event} [event] - The browser's `Event` object created when `keydown` is fired
+   */
+  function _execute (input, event) {
+    // Check if condition is satisfied
+    if (!input.condition()) return
 
     // Check if focus is on the correct place
     if (input.requireFocusOnBody === true && _isFocusOnBody() === false) return
 
-    // Execute callback
-    if (input) {
-      if (input.preventDefault) {
-        event.preventDefault()
-      }
-      if (input.stopPropagation) {
-        event.stopPropagation()
-      }
-      if (input.trackMsg) {
-        EventTracking.track(TRACK_CATEGORY_INTERACTION, input.trackMsg, TRACK_LABEL_KEYBOARD, input.trackValue, input.trackOnce)
-      }
-      input.onKeypress()
+    if (event && input.preventDefault) {
+      event.preventDefault()
     }
+    if (event && input.stopPropagation) {
+      event.stopPropagation()
+    }
+    if (input.trackMsg) {
+      EventTracking.track(TRACK_CATEGORY_INTERACTION, input.trackMsg, TRACK_LABEL_KEYBOARD, input.trackValue, input.trackOnce)
+    }
+
+    // Execute callback
+    input.onKeypress()
   }
 
   // Utility function
