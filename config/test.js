9e6281ea2109bad90d833dbb8db362b3aa6908c5
Lou Huang <lou@louhuang.com>
2015-09-24 07:48:48 -0400

    Try a different test host

--- config/test.js
+++ config/test.js
@@ -2,14 +2,14 @@ var port = 3000
 
 module.exports = {
   port: port,
-  app_host_port: '127.0.0.1:' + port,
-  header_host_port: '127.0.0.1:' + port,
+  app_host_port: 'localhost:' + port,
+  header_host_port: 'localhost:' + port,
   restapi: {
     port: port,
     baseuri: '/api'
   },
   db: {
-    url: 'mongodb://127.0.0.1/streetmix_test'
+    url: 'mongodb://localhost/streetmix_test'
   },
   email: {
     feedback_recipient: 'nobody@example.com'
