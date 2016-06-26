53a4ddc598278846536e76f552b3dfa2a7f34a11
Lou Huang <lou@louhuang.com>
2016-06-26 12:22:06 -0400

    Lint JS on the server side as well

--- app/controllers/twitter_sign_in.js
+++ app/controllers/twitter_sign_in.js
@@ -4,7 +4,7 @@ var oauth = require('../../lib/oauth.js')
 var logger = require('../../lib/logger.js')()
 
 var oauthRequestTokenHandler = function (req, res) {
-  return function (err, oauth_token, oauth_token_secret, results) {
+  return function (err, oauthToken, oauthTokenSecret, results) {
     if (err) {
       console.error('Error obtaining request token from Twitter:')
       console.error(err)
@@ -14,12 +14,12 @@ var oauthRequestTokenHandler = function (req, res) {
     }
 
     req.session.oauth = {}
-    req.session.oauth.request_token = oauth_token
-    req.session.oauth.request_token_secret = oauth_token_secret
+    req.session.oauth.request_token = oauthToken
+    req.session.oauth.request_token_secret = oauthTokenSecret
     req.session.oauth.redirect_uri = req.query.redirectUri || '/'
 
     // Redirect user to Twitter sign-in flow
-    res.redirect(sprintf(config.twitter.oauth_authenticate_uri, oauth_token))
+    res.redirect(sprintf(config.twitter.oauth_authenticate_uri, oauthToken))
     return
   }
 }
