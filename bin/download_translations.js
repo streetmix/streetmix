c4c1b24a6c0b94e1e443acfdfb580f44a50dea55
Lou Huang <lou@louhuang.com>
2016-06-26 23:47:32 -0400

    Lint all JS files throughout the project

--- bin/download_translations.js
+++ bin/download_translations.js
@@ -1,23 +1,27 @@
 'use strict'
 
 var fs = require('fs')
+var path = require('path')
 var downloadTransifex = require('../lib/transifex.js')
 
-var envFile = __dirname + '/../.env'
+var envFile = path.join(__dirname, '/../.env')
 if (fs.existsSync(envFile)) {
   var env = require('node-env-file')
-  env(__dirname + '/../.env')
+  env(envFile)
 }
 
 var resources = ['main', 'segment-info']
 var languages = ['en@pirate', 'fi', 'de', 'es', 'es_MX', 'pl', 'pt_BR']
 
 var downloadSuccess = function (locale, resource, data) {
-  var localeDir = __dirname + '/../assets/locales/' + locale
+  var localeDir = path.join(__dirname, '/../assets/locales/', locale)
   var translationFile = localeDir + '/' + resource + '.json'
   var translationText = JSON.stringify(data, null, 2) + '\n'
 
   fs.stat(localeDir, function (err, stats) {
+    if (err) {
+      console.log('Error accessing ' + localeDir + '.')
+    }
     if (!stats) {
       fs.mkdirSync(localeDir)
     }
