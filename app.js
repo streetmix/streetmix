5c7cb22d0d7009e1689cbcec6f8aab21a4e1d585
Lou Huang <lou@louhuang.com>
2017-03-31 20:11:10 -0400

    Permanently redirect http to https in production

--- app.js
+++ app.js
@@ -32,10 +32,26 @@ app.use(requestHandlers.login_token_parser)
 app.use(requestHandlers.request_log)
 app.use(requestHandlers.request_id_echo)
 
+// Permanently redirect http to https in production.
+app.use(function (req, res, next) {
+  if (app.locals.config.env === 'production') {
+    // req.secure is Express's flag for a secure request, but this is not available
+    // on Heroku, which uses a header instead.
+    if (req.secure === true || req.headers['x-forwarded-proto'] !== 'https') {
+      res.redirect(301, 'https://' + req.hostname + req.originalUrl)
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, '/app/views'))
 
 // Redirect to environment-appropriate domain, if necessary
+// In production, this redirects streetmix-v2.herokuapp.com to https://streetmix.net/
 app.all('*', function (req, res, next) {
   if (config.header_host_port !== req.headers.host && app.locals.config.env !== 'development') {
     var redirectUrl = 'https://' + config.header_host_port + req.url
