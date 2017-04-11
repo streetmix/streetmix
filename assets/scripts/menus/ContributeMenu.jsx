5c6f7d1756efa6fadb937b72318153427c88b61a
Lou Huang <lou@louhuang.com>
2017-04-11 00:27:26 -0400

    Reorder menu entries

--- assets/scripts/menus/ContributeMenu.jsx
+++ assets/scripts/menus/ContributeMenu.jsx
@@ -6,14 +6,14 @@ export default class ContributeMenu extends React.PureComponent {
   render () {
     return (
       <Menu {...this.props}>
-        <a href='https://www.stickermule.com/user/1069909781/stickers' target='_blank'>
-          <span data-i18n='menu.contribute.stickers'>Buy a sticker sheet!</span>
+        <a href='https://github.com/streetmix/streetmix/blob/master/CONTRIBUTING.md' target='_blank'>
+          <span data-i18n='menu.contribute.opensource'>Contribute to open source</span>
         </a>
         <a href='https://opencollective.com/streetmix/' target='_blank'>
           <span data-i18n='menu.contribute.donate'>Donate</span>
         </a>
-        <a href='https://github.com/streetmix/streetmix/blob/master/CONTRIBUTING.md' target='_blank'>
-          <span data-i18n='menu.contribute.opensource'>Contribute to open source</span>
+        <a href='https://www.stickermule.com/user/1069909781/stickers' target='_blank'>
+          <span data-i18n='menu.contribute.stickers'>Buy a sticker sheet!</span>
         </a>
       </Menu>
     )
