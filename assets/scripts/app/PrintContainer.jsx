192c8195dfcbeacc9ee20fc6f3bb41e42566fd03
Lou Huang <lou@louhuang.com>
2017-08-02 21:59:47 -0400

    Port print logic to PrintContainer

--- assets/scripts/app/PrintContainer.jsx
+++ assets/scripts/app/PrintContainer.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { getStreetImage } from '../streets/image'
 
-export default class PrintContainer extends React.PureComponent {
+export default class PrintContainer extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isPrinting: false
+    }
+
+    this.createPrintImage = this.createPrintImage.bind(this)
+  }
+
+  componentDidMount () {
+    // Add event listeners
+    // Chrome does not have the 'beforeprint' or 'afterprint' events
+    window.addEventListener('beforeprint', () => {
+      this.setState({ isPrinting: true })
+    })
+    window.addEventListener('afterprint', () => {
+      this.setState({ isPrinting: false })
+    })
+
+    // Listening for media query change for Chrome
+    var mediaQueryList = window.matchMedia('print')
+    mediaQueryList.addListener((mql) => {
+      if (mql.matches) {
+        this.setState({ isPrinting: true })
+      } else {
+        this.setState({ isPrinting: false })
+      }
+    })
+  }
+
+  createPrintImage () {
+    const dataUrl = getStreetImage(true, true).toDataURL('image/png')
+
+    return (this.state.isPrinting) ? <img src={dataUrl} /> : null
+  }
+
   render () {
     return (
       <div id='print' className='print-container'>
-        <div className='print-image' />
+        <div className='print-image'>
+          {this.createPrintImage()}
+        </div>
       </div>
     )
   }
--- assets/scripts/app/print.js
+++ assets/scripts/app/print.js
@@ -1,47 +1,5 @@
 import { infoBubble } from '../info_bubble/info_bubble'
-import { getStreetImage } from '../streets/image'
 import { hideAllMenus } from '../menus/menu_controller'
-
-export function attachPrintEventListeners () {
-  // Add event listeners
-  // Chrome does not have the 'beforeprint' or 'afterprint' events
-  window.addEventListener('beforeprint', () => {
-    onBeforePrint(false)
-  })
-
-  // Listening for media query change for Chrome
-  var mediaQueryList = window.matchMedia('print')
-  mediaQueryList.addListener(function (mql) {
-    if (mql.matches) {
-      onBeforePrint(true)
-    }
-  })
-}
-
-function updatePrintImage () {
-  document.querySelector('#print > div').innerHTML = ''
-
-  var el = getStreetImage(true, true)
-  var dataUrl = el.toDataURL('image/png')
-
-  var imgEl = document.createElement('img')
-  imgEl.src = dataUrl
-  document.querySelector('#print > div').appendChild(imgEl)
-}
-
-function onBeforePrint (mediaMatch) {
-  // So that max-height: 100% works
-  if (mediaMatch) {
-    document.querySelector('#print > div').style.width = window.innerWidth + 'px'
-    document.querySelector('#print > div').style.height = window.innerHeight + 'px'
-  }
-
-  updatePrintImage()
-
-  if (!mediaMatch) {
-    document.querySelector('#print > div > img').style.width = '100%'
-  }
-}
 
 export function printImage (event) {
   event.preventDefault()
