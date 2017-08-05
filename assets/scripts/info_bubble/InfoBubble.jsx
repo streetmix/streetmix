fbdf7c20846b0dd1b571d0f6ce759fa1d7883edf
Lou Huang <lou@louhuang.com>
2017-08-05 08:09:23 -0400

    InfoBubble: component adds event listeners on mount

--- assets/scripts/info_bubble/InfoBubble.jsx
+++ assets/scripts/info_bubble/InfoBubble.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { infoBubble } from './info_bubble'
 
 export default class InfoBubble extends React.PureComponent {
+  componentDidMount () {
+    // This listener hides the info bubble when the mouse leaves the
+    // document area. Do not normalize it to a pointerleave event
+    // because it doesn't make sense for other pointer types
+    document.addEventListener('mouseleave', infoBubble.hide)
+  }
+
   render () {
     return (
-      <div className='info-bubble' />
+      <div
+        className='info-bubble'
+        onMouseEnter={infoBubble.onMouseEnter}
+        onMouseLeave={infoBubble.onMouseLeave}
+        onTouchStart={infoBubble.onTouchStart}
+      />
     )
   }
 }
