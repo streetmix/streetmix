c1a47eee1e2b61570cb04653641a129cff136c6a
Lou Huang <lou@louhuang.com>
2017-12-25 22:10:14 -0500

    MessageBar: use react-transition-group to animate dismissal

--- assets/scripts/app/MessageBar.jsx
+++ assets/scripts/app/MessageBar.jsx
@@ -1,30 +1,44 @@
 import React from 'react'
+import { Transition } from 'react-transition-group'
+
+const TRANSITION_DURATION = 250
+const TRANSITION_BASE_STYLE = {
+  transition: `margin ${TRANSITION_DURATION}ms ease-out`
+}
 
 export default class MessageBar extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      visible: true
+      visible: true,
+      height: 0
     }
+
+    this.el = null
   }
 
   onClickDismiss = (event) => {
+    const height = this.el.getBoundingClientRect().height
+
     this.setState({
-      visible: false
+      visible: false,
+      height
     })
   }
 
   render () {
-    if (!this.state.visible) return null
+    const margin = (this.state.visible) ? 0 : `-${this.state.height}px`
 
     return (
-      <div className="message-bar">
-        <strong className="message-bar-intro">Help defend net neutrality!</strong>
-        <span className="message-bar-text">Projects like Streetmix couldn’t exist without Internet freedom.</span>
-        <a href="https://www.battleforthenet.com/" target="_blank" className="message-bar-link">Learn more.</a>
-        <button className="close" onClick={this.onClickDismiss} title="Dismiss">×</button>
-      </div>
+      <Transition in={this.state.visible} timeout={TRANSITION_DURATION} unmountOnExit>
+        <div className="message-bar" ref={(ref) => { this.el = ref }} style={{ ...TRANSITION_BASE_STYLE, marginTop: margin }}>
+          <strong className="message-bar-intro">Heads up!</strong>
+          <span className="message-bar-text">Streetmix will be offline for maintainance on January 1, 2018 at 19:00 GMT.</span>
+          <a href="https://twitter.com/streetmix/" target="_blank" className="message-bar-link">Follow us on Twitter for updates.</a>
+          <button className="close" onClick={this.onClickDismiss} title="Dismiss">×</button>
+        </div>
+      </Transition>
     )
   }
 }
