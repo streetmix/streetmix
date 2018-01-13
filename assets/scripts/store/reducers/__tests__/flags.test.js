aa02ae982429bc8e669007d8e6acc94957490b50
Lou Huang <lou@louhuang.com>
2018-01-13 10:21:24 -0500

    add tests for flag reducer

---
+++ assets/scripts/store/reducers/__tests__/flags.test.js
@@ -0,0 +1,49 @@
+/* eslint-env jest */
+import reducer from '../flags'
+import * as actions from '../../actions/flags'
+
+jest.mock('../../../app/flags', () => {
+  return {
+    FEATURE_FLAGS: {
+      FOO_BAR: {
+        label: 'foo — bar',
+        defaultValue: false
+      },
+      BAZ_QUX: {
+        label: 'baz — qux',
+        defaultValue: true
+      }
+    }
+  }
+})
+
+describe('flags reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      'BAZ_QUX': {
+        'source': 'initial',
+        'value': true
+      },
+      'FOO_BAR': {
+        'source': 'initial',
+        'value': false
+      }
+    })
+  })
+
+  it('should set a user-defined value', () => {
+    const action = reducer(undefined, actions.setFeatureFlag('BAZ_QUX', false))
+    const result = {
+      'BAZ_QUX': {
+        'source': 'user',
+        'value': false
+      },
+      'FOO_BAR': {
+        'source': 'initial',
+        'value': false
+      }
+    }
+
+    expect(action).toEqual(result)
+  })
+})
