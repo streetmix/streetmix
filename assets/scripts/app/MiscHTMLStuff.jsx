aa75cfa1dd9b4ce99ac7dfd40a6ff56df9e395a3
Mandy Kong <mk5506@nyu.edu>
2018-01-16 15:05:16 -0500

    copied scroll functions to MiscHTMLStuff component and passed relevant functions to ScrollIndicators component through props

--- assets/scripts/app/MiscHTMLStuff.jsx
+++ assets/scripts/app/MiscHTMLStuff.jsx
@@ -7,28 +7,105 @@
  */
 import React from 'react'
 import ScrollIndicators from './ScrollIndicators'
+import { connect } from 'react-redux'
 import { infoBubble } from '../info_bubble/info_bubble'
+import { system } from '../preinit/system_capabilities'
+import { animate } from '../util/helpers'
+import { MAX_CUSTOM_STREET_WIDTH } from '../streets/width'
 
 class MiscHTMLStuff extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      street_section_outer: null
+      streetSectionScroll: false
     }
   }
 
-  componentDidMount () {
+  onStreetSectionScroll = (event) => {
+    infoBubble.suppress()
+
+    var scrollPos = this.street_section_outer.scrollLeft
+
+    var frontPos = -scrollPos * 0.5
+    this.front_clouds.style[this.props.system.cssTransform] =
+      'translateX(' + frontPos + 'px)'
+
+    var rearPos = -scrollPos * 0.25
+    this.rear_clouds.style[this.props.system.cssTransform] =
+      'translateX(' + rearPos + 'px)'
+
+    if (event) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
     this.setState({
-      street_section_outer: this.street_section_outer
+      streetSectionScroll: true
     })
-    console.log(this.street_section_outer)
+
+  }
+
+  updateStreetScrollIndicators = (left, right) => {
+    const el = this.street_section_outer
+    let posLeft
+    let posRight
+
+    if (el.scrollWidth <= el.offsetWidth) {
+      posLeft = 0
+      posRight = 0
+    } else {
+      var left = el.scrollLeft / (el.scrollWidth - el.offsetWidth)
+
+      // TODO const off max width street
+      var posMax = Math.round(this.props.street.width / MAX_CUSTOM_STREET_WIDTH * 6)
+      if (posMax < 2) {
+        posMax = 2
+      }
+
+      posLeft = Math.round(posMax * left)
+      if ((left > 0) && (posLeft === 0)) {
+        posLeft = 1
+      }
+      if ((left < 1.0) && (posLeft === posMax)) {
+        posLeft = posMax - 1
+      }
+      posRight = posMax - posLeft
+      
+      this.left_indicator.innerHTML = Array(posLeft + 1).join('‹')
+      this.right_indicator.innerHTML = Array(posRight + 1).join('›')
+    }
+
+    this.setState({
+      streetSectionScroll: false
+    })
+  }
+
+  scrollStreet = (left, far = false) => {
+    const el = this.street_section_outer
+    let newScrollLeft
+
+    if (left) {
+      if (far) {
+        newScrollLeft = 0
+      } else {
+        newScrollLeft = el.scrollLeft - (el.offsetWidth * 0.5)
+      }
+    } else {
+      if (far) {
+        newScrollLeft = el.scrollWidth - el.offsetWidth
+      } else {
+        newScrollLeft = el.scrollLeft + (el.offsetWidth * 0.5)
+      }
+    }
+
+    animate(el, { scrollLeft: newScrollLeft }, 300)
   }
 
   render () {
     return (
       <React.Fragment>
-        <section id="street-section-outer" ref={(ref) => { this.street_section_outer = ref }}>
+        <section id="street-section-outer" onScroll={this.onStreetSectionScroll} ref={(ref) => { this.street_section_outer = ref }}>
           <section id="street-section-inner">
             <section id="street-section-canvas">
               <section id="street-section-left-building" className="street-section-building">
@@ -48,10 +125,23 @@ class MiscHTMLStuff extends React.Component {
           <div className="rear-clouds" ref={(ref) => { this.rear_clouds = ref }} />
           <div className="front-clouds" ref={(ref) => { this.front_clouds = ref }} />
         </section>
-        <ScrollIndicators outerSection={this.state.street_section_outer} onStreetSectionScroll={this.onStreetSectionScroll} />
+        <ScrollIndicators 
+          handleScroll={this.state.streetSectionScroll} 
+          updateStreetScrollIndicators={this.updateStreetScrollIndicators} 
+          scrollStreet={this.scrollStreet}
+          leftIndicator={(ref) => { this.left_indicator = ref }}
+          rightIndicator={(ref) => {this.right_indicator = ref }}
+        />
       </React.Fragment>
     )
   }
 }
 
-export default MiscHTMLStuff
+function mapStateToProps (state) {
+  return {
+    street: state.street,
+    system: state.system
+  }
+}
+
+export default connect(mapStateToProps)(MiscHTMLStuff)
