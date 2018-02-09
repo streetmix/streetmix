dc2486815485f2e27a54d73af614f2b9f52270c4
Lou Huang <lou@louhuang.com>
2018-02-09 13:12:45 -0500

    fix broken test

--- assets/scripts/segments/__tests__/info.test.js
+++ assets/scripts/segments/__tests__/info.test.js
@@ -5,7 +5,7 @@ describe('segment info', () => {
   it('gets a sprite definition with a string id', () => {
     const sprite = getSpriteDef('trees--palm-tree')
 
-    expect(sprite).toEqual({ id: 'trees--palm-tree', offsetX: 0, offsetY: -20.25, width: 14, height: 31 })
+    expect(sprite).toEqual({ id: 'trees--palm-tree', offsetX: 0, offsetY: -20.25, width: 14 })
   })
 
   it('overwrites sprite definition properties with an object', () => {
@@ -14,6 +14,6 @@ describe('segment info', () => {
       offsetY: 5.25
     })
 
-    expect(sprite).toEqual({ id: 'bikes--bike-rack-perpendicular-left', width: 6, height: 6, offsetY: 5.25 })
+    expect(sprite).toEqual({ id: 'bikes--bike-rack-perpendicular-left', width: 6, offsetY: 5.25 })
   })
 })
