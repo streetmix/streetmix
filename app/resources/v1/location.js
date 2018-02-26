ca3bcb2a396e83f689fdefabd0d72144e8b0b221
Mandy Kong <mk5506@nyu.edu>
2018-02-26 16:59:53 -0500

    location api now returning streets with locations in geojson format

--- app/resources/v1/location.js
+++ app/resources/v1/location.js
@@ -1,4 +1,45 @@
+require('../../../lib/db.js')
+var Street = require('../../models/street.js')
+var logger = require('../../../lib/logger.js')()
+
 exports.get = function (req, res) {
-  const result = {}
-  res.send(JSON.stringify(result))
+  var handleFindStreets = function (err, results) {
+    if (err) {
+      logger.error(err)
+      res.status(500).send('Could not find streets with locations.')
+      return
+    }
+
+    if (!results) {
+      res.status(404).send('Could not find streets with locations.')
+      return
+    }
+
+    const geojson = {
+      type: 'FeatureCollection',
+      features: []
+    }
+
+    for (let i = 0; i < results.length; i++) {
+      results[i].asJson(function (err, streetJson) {
+        if (err) {
+          logger.error(err)
+          res.status(500).send('Could not render street JSON.')
+          return
+        }
+        const { location } = streetJson.data.street
+        const feature = {
+          geometry: {
+            type: 'Point',
+            coordinates: location.latlng
+          },
+          properties: results[i]
+        }
+        geojson.features.push(feature)
+      })
+    }
+    res.status(200).send(geojson)
+  }
+
+  Street.find({ 'data.street.location': { $ne: null }, status: 'ACTIVE' }, handleFindStreets)
 }
