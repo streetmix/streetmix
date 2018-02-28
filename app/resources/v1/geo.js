2ebb6646b04466e224ec622d35d0bd1b96fb9447
Lou Huang <lou@louhuang.com>
2018-02-28 11:51:41 -0500

    api/geo: async/await the thing

--- app/resources/v1/geo.js
+++ app/resources/v1/geo.js
@@ -2,45 +2,38 @@ require('../../../lib/db.js')
 const Street = require('../../models/street.js')
 const logger = require('../../../lib/logger.js')()
 
-exports.get = function (req, res) {
-  const handleFindStreets = function (err, results) {
-    if (err) {
-      logger.error(err)
-      res.status(500).send('Could not find streets with locations.')
-      return
-    }
+exports.get = async function (req, res) {
+  let results
 
-    if (!results) {
-      res.status(404).send('Could not find streets with locations.')
-      return
-    }
-
-    const geojson = {
-      type: 'FeatureCollection',
-      features: []
-    }
-
-    for (let i = 0; i < results.length; i++) {
-      results[i].asJson(function (err, streetJson) {
-        if (err) {
-          logger.error(err)
-          res.status(500).send('Could not render street JSON.')
-          return
-        }
-        const { location } = streetJson.data.street
-        const feature = {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [location.latlng[1], location.latlng[0]]
-          },
-          properties: results[i]
-        }
-        geojson.features.push(feature)
-      })
-    }
-    res.status(200).send(geojson)
+  try {
+    results = await Street.find({ 'data.street.location': { $ne: null }, status: 'ACTIVE' })
+  } catch (err) {
+    logger.error(err)
+    res.status(500).send('Could not find streets with locations.')
   }
 
-  Street.find({ 'data.street.location': { $ne: null }, status: 'ACTIVE' }, handleFindStreets)
+  if (!results) {
+    res.status(404).send('Could not find streets with locations.')
+    return
+  }
+
+  const geojson = {
+    type: 'FeatureCollection',
+    features: []
+  }
+
+  for (let i = 0; i < results.length; i++) {
+    const { location } = results[i].data.street
+    const feature = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [location.latlng[1], location.latlng[0]]
+      },
+      properties: results[i]
+    }
+    geojson.features.push(feature)
+  }
+
+  res.status(200).json(geojson)
 }
