f4e88d7c68f676ee918f7e0b511a3021817356f1
Lou Huang <lou@louhuang.com>
2018-03-08 11:21:56 -0500

    Remove requestAnimationFrame shim in test environment

--- test/shims/raf.js
+++
@@ -1,9 +0,0 @@
-/**
- * requestAnimationFrame shim for React 16/Jest
- * see https://github.com/facebook/jest/issues/4545#issuecomment-332762365
- *
- * This must be its own file and loaded first, before any Jest setup.
- */
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0)
-}
