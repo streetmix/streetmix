4a19c5db903d232085933bd358eedb2aa6b2dfa8
Mandy Kong <mk5506@nyu.edu>
2018-03-13 11:56:51 -0400

    wrote tests for GeotagDialog

--- assets/scripts/dialogs/__tests__/GeotagDialog.test.js
+++ assets/scripts/dialogs/__tests__/GeotagDialog.test.js
@@ -2,15 +2,12 @@
 import React from 'react'
 import GeotagDialog from '../GeotagDialog'
 import { shallow } from 'enzyme'
+import { getRemixOnFirstEdit } from '../../streets/remix'
 
 // Mock dependencies that could break tests
-jest.mock('../../streets/remix', () => {
-  return {
-    getRemixOnFirstEdit: () => {
-      return false
-    }
-  }
-})
+jest.mock('../../streets/remix', () => ({
+  getRemixOnFirstEdit: jest.fn()
+}))
 
 describe('GeotagDialog', () => {
   it('renders without crashing', () => {
@@ -35,13 +32,9 @@ describe('GeotagDialog', () => {
         markerLocation={testMarker}
       />
     )
-    expect(wrapper.find('button .confirm-button').length).toEqual(0)
+    expect(wrapper.find('button .confirm-button')).toHaveLength(0)
   })
-  // Current signed-in or not signed-in user compared to street owner is based on getRemixOnFirstEdit
-  // which uses store.user.signedInData. Not sure how to replicate that in testing environment.
   it('allows a location to be confirmed when the current signed-in user is the street owner', () => {
-    // getRemixOnFirstEdit returns false - meaning the current signed-in user is the street owner
-    // getRemixOnFirstEdit returns true - current signed-user or not signed in is not the street owner
     const testMarker = { lat: 0, lng: 0 }
     const testAddressInfo = {
       street: 'test street',
@@ -61,6 +54,7 @@ describe('GeotagDialog', () => {
         markerLocation={testMarker}
       />
     )
+    getRemixOnFirstEdit.mockReturnValueOnce(false)
     wrapper.setProps({
       markerLocation: { lat: 10, lng: 10 },
       addressInformation: {
@@ -68,10 +62,123 @@ describe('GeotagDialog', () => {
         id: 'test id 2'
       }
     })
-    expect(wrapper.find('button .confirm-button').length).toEqual(1)
+    expect(wrapper.find('button .confirm-button')).toHaveLength(1)
   })
-  it('allows a location to be confirmed when the current anonymous user started this street')
-  it('does not allow a location to be confirmed when the current signed-in user is not the street owner')
-  it('does not allow a location to be confirmed when the current anonymous user is not the street owner and there is already an existing location attached')
-  it('allows a location to be confirmed when the current anonymous user is not the street owner but there is no existing location attached')
+  it('allows a location to be confirmed when the current anonymous user started this street', () => {
+    const testMarker = { lat: 0, lng: 0 }
+    const testAddressInfo = {
+      street: 'test street',
+      id: 'test id'
+    }
+    const testStreet = {
+      creatorId: null,
+      location: {
+        label: 'test location',
+        wofId: 'test id'
+      }
+    }
+    const wrapper = shallow(
+      <GeotagDialog.WrappedComponent
+        street={testStreet}
+        addressInformation={testAddressInfo}
+        markerLocation={testMarker}
+      />
+    )
+    getRemixOnFirstEdit.mockReturnValueOnce(false)
+    wrapper.setProps({
+      markerLocation: { lat: 10, lng: 10 },
+      addressInformation: {
+        street: 'test street 2',
+        id: 'test id 2'
+      }
+    })
+    expect(wrapper.find('button .confirm-button')).toHaveLength(1)
+  })
+  it('does not allow a location to be confirmed when the current signed-in user is not the street owner', () => {
+    const testMarker = { lat: 0, lng: 0 }
+    const testAddressInfo = {
+      street: 'test street',
+      id: 'test id'
+    }
+    const testStreet = {
+      creatorId: 'test creator',
+      location: {
+        label: 'test location',
+        wofId: 'test id'
+      }
+    }
+    const wrapper = shallow(
+      <GeotagDialog.WrappedComponent
+        street={testStreet}
+        addressInformation={testAddressInfo}
+        markerLocation={testMarker}
+      />
+    )
+    getRemixOnFirstEdit.mockReturnValueOnce(true)
+    wrapper.setProps({
+      markerLocation: { lat: 10, lng: 10 },
+      addressInformation: {
+        street: 'test street 2',
+        id: 'test id 2'
+      }
+    })
+    expect(wrapper.find('button .confirm-button')).toHaveLength(0)
+  })
+  it('does not allow a location to be confirmed when the current anonymous user is not the street owner and there is already an existing location attached', () => {
+    const testMarker = { lat: 0, lng: 0 }
+    const testAddressInfo = {
+      street: 'test street',
+      id: 'test id'
+    }
+    const testStreet = {
+      creatorId: 'test creator',
+      location: {
+        label: 'test location',
+        wofId: 'test id'
+      }
+    }
+    const wrapper = shallow(
+      <GeotagDialog.WrappedComponent
+        street={testStreet}
+        addressInformation={testAddressInfo}
+        markerLocation={testMarker}
+      />
+    )
+    getRemixOnFirstEdit.mockReturnValueOnce(true)
+    wrapper.setProps({
+      markerLocation: { lat: 10, lng: 10 },
+      addressInformation: {
+        street: 'test street 2',
+        id: 'test id 2'
+      }
+    })
+    expect(wrapper.find('button .confirm-button')).toHaveLength(0)
+  })
+  it('allows a location to be confirmed when the current anonymous user is not the street owner but there is no existing location attached', () => {
+    const testMarker = { lat: 0, lng: 0 }
+    const testAddressInfo = {
+      street: 'test street',
+      id: 'test id'
+    }
+    const testStreet = {
+      creatorId: 'test creator',
+      location: null
+    }
+    const wrapper = shallow(
+      <GeotagDialog.WrappedComponent
+        street={testStreet}
+        addressInformation={testAddressInfo}
+        markerLocation={testMarker}
+      />
+    )
+    getRemixOnFirstEdit.mockReturnValueOnce(true)
+    wrapper.setProps({
+      markerLocation: { lat: 10, lng: 10 },
+      addressInformation: {
+        street: 'test street 2',
+        id: 'test id 2'
+      }
+    })
+    expect(wrapper.find('button .confirm-button')).toHaveLength(1)
+  })
 })
