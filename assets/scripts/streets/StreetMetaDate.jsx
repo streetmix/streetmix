ca6d3f82ae636cb56ea2aab0f384d13a1793cdf2
Lou Huang <lou@louhuang.com>
2018-03-21 14:40:35 -0400

    StreetMetaDate: use relative time formatting if within the last 24 hours

--- assets/scripts/streets/StreetMetaDate.jsx
+++ assets/scripts/streets/StreetMetaDate.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { FormattedRelative } from 'react-intl'
+import moment from 'moment'
 import { formatDate } from '../util/date_format'
 
 export class StreetMetaDate extends React.Component {
@@ -8,8 +10,27 @@ export class StreetMetaDate extends React.Component {
     street: PropTypes.any
   }
 
+  renderDateTime = () => {
+    const ONE_DAY_AGO = 1000 * 60 * 60 * 24
+    const updatedAt = this.props.street.updatedAt
+
+    const now = moment()
+    const date = moment(updatedAt)
+    const diff = now - date
+
+    if (diff < ONE_DAY_AGO && updatedAt) {
+      return <FormattedRelative value={updatedAt} />
+    } else {
+      return formatDate(updatedAt)
+    }
+  }
+
   render () {
-    return <span className="street-metadata-date">{formatDate(this.props.street.updatedAt)}</span>
+    return (
+      <span className="street-metadata-date">
+        {this.renderDateTime()}
+      </span>
+    )
   }
 }
 
