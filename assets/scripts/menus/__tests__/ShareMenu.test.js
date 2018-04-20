1f900fbc48ef43a399564e88e16d75efb81b6744
Lou Huang <lou@louhuang.com>
2018-04-20 14:10:01 -0400

    ShareMenu: mock injected react-intl for tests

--- assets/scripts/menus/__tests__/ShareMenu.test.js
+++ assets/scripts/menus/__tests__/ShareMenu.test.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { ShareMenu } from '../ShareMenu'
+import { mockIntl } from '../../../../test/__mocks__/react-intl'
 
 jest.mock('../../app/page_title', () => {
   return {
@@ -11,18 +12,18 @@ jest.mock('../../app/page_title', () => {
 
 describe('ShareMenu', () => {
   it('renders without crashing', () => {
-    const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn street={{}} />)
+    const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn street={{}} intl={mockIntl} />)
     expect(wrapper.exists()).toEqual(true)
   })
 
   describe('sign-in promo', () => {
     it('shows the sign-in promo if user is not signed in', () => {
-      const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn={false} street={{}} />)
+      const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn={false} street={{}} intl={mockIntl} />)
       expect(wrapper.find('.share-sign-in-promo').length).toEqual(1)
     })
 
     it('does not show the sign-in promo if user is signed in', () => {
-      const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn street={{}} />)
+      const wrapper = shallow(<ShareMenu showDialog={jest.fn()} signedIn street={{}} intl={mockIntl} />)
       expect(wrapper.find('.share-sign-in-promo').length).toEqual(0)
     })
   })
