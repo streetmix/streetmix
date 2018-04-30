ed0764a00635272a9c88aa58cc35efd69b13f149
flickz <omoyajowo2015@gmail.com>
2018-04-30 19:06:38 +0100

    Update MessageBar close button to CloseButton component

--- assets/scripts/app/MessageBar.jsx
+++ assets/scripts/app/MessageBar.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Transition } from 'react-transition-group'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 
+import CloseButton from '../ui/CloseButton'
+
 const TRANSITION_DURATION = 250
 const TRANSITION_BASE_STYLE = {
   transition: `margin ${TRANSITION_DURATION}ms ease-out`
@@ -67,14 +69,10 @@ class MessageBar extends React.Component {
               {linkText || <FormattedMessage id="msg.more-info" defaultMessage="More info" />}
             </a>
           }
-
-          <button
-            className="close"
+          <CloseButton
             onClick={this.onClickDismiss}
             title={this.props.intl.formatMessage({ id: 'btn.dismiss', defaultMessage: 'Dismiss' })}
-          >
-            ×
-          </button>
+          />
         </div>
       </Transition>
     )
