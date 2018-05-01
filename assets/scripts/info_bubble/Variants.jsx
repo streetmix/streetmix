15882c5b0ad4025d8f890d56513e6d27c5691959
Mandy Kong <mk5506@nyu.edu>
2018-05-01 12:28:12 -0400

    make default userUpdated value for setBuildingVariant equal to true

--- assets/scripts/info_bubble/Variants.jsx
+++ assets/scripts/info_bubble/Variants.jsx
@@ -97,7 +97,7 @@ class Variants extends React.Component {
         break
       case INFO_BUBBLE_TYPE_LEFT_BUILDING:
         handler = (event) => {
-          this.props.setBuildingVariant('left', selection, true)
+          this.props.setBuildingVariant('left', selection)
 
           // TODO: remove legacy notification
           infoBubble.updateContents()
@@ -105,7 +105,7 @@ class Variants extends React.Component {
         break
       case INFO_BUBBLE_TYPE_RIGHT_BUILDING:
         handler = (event) => {
-          this.props.setBuildingVariant('right', selection, true)
+          this.props.setBuildingVariant('right', selection)
 
           // TODO: remove legacy notification
           infoBubble.updateContents()
