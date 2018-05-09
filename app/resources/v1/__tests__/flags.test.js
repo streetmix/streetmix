5fd739d43cd22b5c8eeec875e6419da6ceeb0c6d
flickz <omoyajowo2015@gmail.com>
2018-05-09 17:34:09 +0100

    Add API test to get flags

---
+++ app/resources/v1/__tests__/flags.test.js
@@ -0,0 +1,26 @@
+/* eslint-env jest */
+import request from 'supertest'
+import express from 'express'
+import flags from '../flags'
+
+function setupMockServer () {
+  const app = express()
+
+  app.get('/api/v1/flags', flags.get)
+
+  return app
+}
+
+describe('get api/v1/flags', function () {
+  const app = setupMockServer()
+
+  it('makes a request for a flag file', () => {
+    return request(app)
+      .get('/api/v1/flags')
+      .then((response) => {
+        expect(response.statusCode).toEqual(200)
+        expect(response.get('Content-Type').toLowerCase()).toEqual('application/json; charset=utf-8')
+        expect(response.body.GEOLOCATION.label).toEqual('Geolocation')
+      })
+  })
+})
