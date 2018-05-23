632fc9dc71b868059123e56149a2588f5645a279
flickz <omoyajowo2015@gmail.com>
2018-05-23 20:17:01 +0100

    Fix bug from redirectUrl sent to auth0

--- config/staging.js
+++ config/staging.js
@@ -2,7 +2,8 @@ module.exports = {
   app_host_port: 'streetmix-staging.herokuapp.com',
   header_host_port: 'streetmix-staging.herokuapp.com',
   restapi: {
-    baseuri: '/api'
+    baseuri: '/api',
+    protocol: 'https://'
   },
   facebook_app_id: '175861739245183',
   l10n: {
