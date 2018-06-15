56e24f4ecc3550f3b944a25d70a59115d81dc731
Lou Huang <lou@louhuang.com>
2018-06-14 23:43:45 -0400

    EnvironmentBadge: fix tests

--- assets/scripts/menus/__tests__/EnvironmentBadge.test.js
+++ assets/scripts/menus/__tests__/EnvironmentBadge.test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import React from 'react'
 import { shallow } from 'enzyme'
-import { EnvironmentBadge } from '../EnvironmentBadge'
+import EnvironmentBadge from '../EnvironmentBadge'
 
 describe('EnvironmentBadge', () => {
   it('renders nothing in standard conditions', () => {
