280faa97f08f3b3abffd59c5a13d778c6d8c6a45
Lou Huang <lou@louhuang.com>
2018-06-21 16:23:30 -0400

    deps: bump all major versions; update jest tests

--- assets/scripts/streets/__tests__/StreetName.test.js
+++ assets/scripts/streets/__tests__/StreetName.test.js
@@ -27,7 +27,7 @@ describe('StreetName', () => {
     expect(wrapper.text()).toEqual('Unnamed St')
   })
 
-  it('shows a "Click to edit" message when mouse is hovering over it')
-  it('does not do that if the street name is not editable')
-  it('responds to an onClick handler')
+  it('shows a "Click to edit" message when mouse is hovering over it', () => {})
+  it('does not do that if the street name is not editable', () => {})
+  it('responds to an onClick handler', () => {})
 })
