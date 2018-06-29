bacaadf83fbafd4b4da0f15be43d37aabcc61d60
joerodrig <joerodrig3@gmail.com>
2018-06-28 21:19:45 -0400

    Add specs for SHOW_ERROR

--- assets/scripts/store/reducers/__tests__/errors.test.js
+++ assets/scripts/store/reducers/__tests__/errors.test.js
@@ -1,11 +1,41 @@
 import reducer from '../errors'
 import * as actions from '../../actions/errors'
 
+const initialState = {
+    errorType: null,
+    abortEverything: false
+}
+
 describe('errors reducer', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({
-      errorType: null,
-      abortEverything: false
-    })
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should handle SHOW_ERROR', () => {
+    // nothing set
+    expect(reducer(undefined, actions.showError())).
+      toEqual({...initialState, errorType: undefined, abortEverything: false})
+
+    // errorType explicitly set and abortEverything set to true
+    expect(reducer(undefined, actions.showError("Bad URL", true))).
+      toEqual({...initialState, errorType: "Bad URL", abortEverything: true})
+
+    // errorType explicitly set and abortEverything set to false
+    expect(reducer(undefined, actions.showError("Bad URL", false))).
+      toEqual({...initialState, errorType: "Bad URL", abortEverything: false})
+
+    // errorType explicitly set and abortEverything not set
+    expect(reducer(undefined, actions.showError("Bad URL"))).
+      toEqual({...initialState, errorType: "Bad URL", abortEverything: false})
+
+    // errorType explicitly set and abortEverything set to invalid state
+    // FIXME: This shouldn't pass because abortEverything should always be a boolean
+    expect(reducer(undefined, actions.showError("Bad URL", 'true'))).
+      toEqual({...initialState, errorType: "Bad URL", abortEverything: 'true'})
+
+    // errorType explicitly set and abortEverything set to invalid state
+    // FIXME: This shouldn't pass because abortEverything should always be a boolean
+    expect(reducer(undefined, actions.showError("Bad URL", 101))).
+      toEqual({...initialState, errorType: "Bad URL", abortEverything: 101})
   })
 })
