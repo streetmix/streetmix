e506d8b60e200f6eba6a4751a9c4a79a47f166bd
joerodrig <joerodrig3@gmail.com>
2018-06-28 20:57:08 -0400

    Add initial state reducer for errors

---
+++ assets/scripts/store/reducers/__tests__/errors.test.js
@@ -0,0 +1,11 @@
+import reducer from '../errors'
+import * as actions from '../../actions/errors'
+
+describe('errors reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      errorType: null,
+      abortEverything: false
+    })
+  });
+});
