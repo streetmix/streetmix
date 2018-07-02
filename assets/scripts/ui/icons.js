1016632b3e30cf7541e1d00aa3dcbb069626ceb7
Lou Huang <lou@louhuang.com>
2018-07-02 13:20:22 -0400

    CloseButton: use font-awesome icon

--- assets/scripts/ui/icons.js
+++ assets/scripts/ui/icons.js
@@ -7,9 +7,10 @@ import {
   faChevronRight,
   faChevronLeft
 } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Load Font-Awesome icons
 export function initIcons () {
-  library.add(faCheck, faMinus, faPlus, faChevronRight, faChevronLeft, fab)
+  library.add(faCheck, faMinus, faPlus, faChevronRight, faChevronLeft, faTimesCircle, fab)
 }
