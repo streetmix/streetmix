9489549a449015be9efd90570a61b750edb698d3
Mandy Kong <mk5506@nyu.edu>
2018-07-17 16:23:19 -0400

    modified timeout value

--- app/resources/v1/geoip.js
+++ app/resources/v1/geoip.js
@@ -2,12 +2,14 @@ const config = require('config')
 const request = require('request')
 const logger = require('../../../lib/logger.js')()
 
+const IP_GEOLOCATION_TIMEOUT = 1000
+
 exports.get = function (req, res) {
   if (req.headers.host !== config.app_host_port) return
 
   const url = `http://${config.geoip.host}?access_key=${config.geoip.api_key}`
 
-  request(url, { json: true }, function (error, response, body) {
+  request(url, { timeout: IP_GEOLOCATION_TIMEOUT }, function (error, response, body) {
     if (error) {
       logger.error(error)
       return
