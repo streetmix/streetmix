b095cd396079ccc3381224b889aa31894a628df8
Lou Huang <lou@louhuang.com>
2018-07-18 13:21:11 -0400

    SignInDialog: refactor

--- assets/scripts/dialogs/SignInDialog.jsx
+++ assets/scripts/dialogs/SignInDialog.jsx
@@ -22,7 +22,7 @@ export class SignInDialog extends React.Component {
       emailSent: false,
       sendingEmail: false,
       error: false,
-      errorMesg: '',
+      errorMsg: '',
       signingIn: false
     }
 
@@ -99,104 +99,88 @@ export class SignInDialog extends React.Component {
     } else {
       this.setState({
         error: true,
-        errorMesg: 'Oops! That didn\'t look like a valid email address. Please try again.'
+        errorMsg: 'Oops! That didn\'t look like a valid email address. Please try again.'
       })
     }
   }
 
-  renderErrorMesg = () => {
-    const { error, errorMesg } = this.state
-
-    if (error) {
+  rendererrorMsg = () => {
+    if (this.state.error) {
       return (<p className="sign-in-error-message">
         <FormattedMessage
           id="dialogs.sign-in.email-error-message"
-          defaultMessage={errorMesg}
+          defaultMessage={this.state.errorMsg}
         />
       </p>)
     }
   }
 
   renderEmailAuth () {
-    const { emailAuthEnabled } = this.props
-    const { email, error } = this.state
-    const isEnabled = (email.length > 0)
-
-    if (emailAuthEnabled) {
-      return (
-        <React.Fragment>
-          <p className="sign-in-email-title">
-            <FormattedMessage
-              id="dialogs.sign-in.email-title"
-              defaultMessage="Or use your email address"
-            />
-          </p>
-          <p className="email-description">
-            <FormattedMessage
-              id="dialogs.sign-in.email-description"
-              defaultMessage="We'll send you a link. No password is required."
-            />
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <input type="email"
-              ref={this.emailInputEl}
-              value={email}
-              className={'form-control ' + (error ? 'sign-in-error-occured' : '')}
-              name="email"
-              onChange={this.handleChange}
-              placeholder="test@test.com"
-            />
-            {this.renderErrorMesg()}
-            <button type="submit"
-              disabled={!isEnabled}
-              className="sign-in-button sign-in-email-button"
-            >
-              <FormattedMessage id="dialogs.sign-in.button.email" defaultMessage="Sign in with email" />
-            </button>
-          </form>
-        </React.Fragment>
-      )
-    }
+    return (
+      <React.Fragment>
+        <p className="sign-in-email-title">
+          <FormattedMessage
+            id="dialogs.sign-in.email-title"
+            defaultMessage="Or use your email address"
+          />
+        </p>
+        <p className="email-description">
+          <FormattedMessage
+            id="dialogs.sign-in.email-description"
+            defaultMessage="We'll send you a link. No password is required."
+          />
+        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="email"
+            ref={this.emailInputEl}
+            value={this.state.email}
+            className={'form-control ' + (this.state.error ? 'sign-in-error-occured' : '')}
+            name="email"
+            onChange={this.handleChange}
+            placeholder="test@test.com"
+          />
+          {this.rendererrorMsg()}
+          <button type="submit"
+            disabled={this.state.email.length === 0}
+            className="sign-in-button sign-in-email-button"
+          >
+            <FormattedMessage id="dialogs.sign-in.button.email" defaultMessage="Sign in with email" />
+          </button>
+        </form>
+      </React.Fragment>
+    )
   }
 
   renderFacebookAuth () {
-    const { facebookAuthEnabled } = this.props
-    if (facebookAuthEnabled) {
-      return (
-        <button onClick={this.handleFacebookSignIn} className="sign-in-button sign-in-facebook-button">
-          <Icon icon="facebook" />
-          <FormattedMessage id="dialogs.sign-in.button.facebook" defaultMessage="Continue with Facebook" />
-        </button>
-      )
-    }
+    return (
+      <button onClick={this.handleFacebookSignIn} className="sign-in-button sign-in-facebook-button">
+        <Icon icon="facebook" />
+        <FormattedMessage id="dialogs.sign-in.button.facebook" defaultMessage="Continue with Facebook" />
+      </button>
+    )
   }
 
   renderGoogleAuth () {
-    const { googleAuthEnabled } = this.props
-    if (googleAuthEnabled) {
-      return (
-        <button onClick={this.handleGoogleSignIn} className="sign-in-button sign-in-google-button">
-          <Icon icon="google" />
-          <FormattedMessage id="dialogs.sign-in.button.google" defaultMessage="Continue with Google" />
-        </button>
-      )
-    }
+    return (
+      <button onClick={this.handleGoogleSignIn} className="sign-in-button sign-in-google-button">
+        <Icon icon="google" />
+        <FormattedMessage id="dialogs.sign-in.button.google" defaultMessage="Continue with Google" />
+      </button>
+    )
   }
 
   renderTwitterAuth () {
-    const { twitterAuthEnabled } = this.props
-    if (twitterAuthEnabled) {
-      return (
-        <button className="sign-in-button sign-in-twitter-button" onClick={this.handleTwitterSignIn}>
-          <Icon icon="twitter" />
-          <FormattedMessage id="dialogs.sign-in.button.twitter" defaultMessage="Continue with Twitter" />
-        </button>
-      )
-    }
+    return (
+      <button className="sign-in-button sign-in-twitter-button" onClick={this.handleTwitterSignIn}>
+        <Icon icon="twitter" />
+        <FormattedMessage id="dialogs.sign-in.button.twitter" defaultMessage="Continue with Twitter" />
+      </button>
+    )
   }
 
   render () {
     const { email, sendingEmail, emailSent, signingIn } = this.state
+
     if (sendingEmail || signingIn) {
       return (
         <div className="sign-in-dialog">
@@ -247,10 +231,10 @@ export class SignInDialog extends React.Component {
               defaultMessage="If you don't already have a Streetmix account, we’ll make one for you."
             />
           </p>
-          {this.renderTwitterAuth()}
-          {this.renderGoogleAuth()}
-          {this.renderFacebookAuth()}
-          {this.renderEmailAuth()}
+          {this.props.twitterAuthEnabled && this.renderTwitterAuth()}
+          {this.props.googleAuthEnabled && this.renderGoogleAuth()}
+          {this.props.facebookAuthEnabled && this.renderFacebookAuth()}
+          {this.props.emailAuthEnabled && this.renderEmailAuth()}
         </div>
       )
     }
