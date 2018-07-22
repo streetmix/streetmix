c9fbe69994fcdce2671ca895d2b19871a497f74f
flickz <omoyajowo2015@gmail.com>
2018-07-22 14:40:04 +0100

    Update street api to async await flow

--- app/resources/v1/streets.js
+++ app/resources/v1/streets.js
@@ -8,12 +8,11 @@ const User = require('../../models/user.js')
 const Sequence = require('../../models/sequence.js')
 const logger = require('../../../lib/logger.js')()
 
-exports.post = function (req, res) {
+exports.post = async function (req, res) {
   const street = new Street()
   let body
 
   street.id = uuid.v1()
-
   const requestIp = function (req) {
     if (req.headers['x-forwarded-for'] !== undefined) {
       return req.headers['x-forwarded-for'].split(', ')[0]
@@ -35,94 +34,102 @@ exports.post = function (req, res) {
     street.creator_ip = requestIp(req)
   }
 
-  const handleCreateStreet = function (err, s) {
-    if (err) {
+  const makeNamespacedId = async function () {
+    try {
+      if (street.creator_id) {
+        const row = await User.findByIdAndUpdate(street.creator_id,
+          { $inc: { 'last_street_id': 1 } },
+          { new: true, upsert: true })
+        const namespacedId = (row) ? row.last_street_id : null
+        if (!namespacedId) {
+          res.status(500).send('Could not create new street ID.')
+          return
+        }
+        return namespacedId
+      } else {
+        const row = await Sequence.findByIdAndUpdate('streets',
+          { $inc: { 'seq': 1 } },
+          { new: true, upsert: true })
+        const namespacedId = (row) ? row.seq : null
+        if (!namespacedId) {
+          res.status(500).send('Could not create new street ID.')
+          return
+        }
+        return namespacedId
+      }
+    } catch (err) {
       logger.error(err)
-      res.status(500).send('Could not create street.')
-      return
+      res.status(500).send('Could not create new street ID.')
     }
+  }
 
-    s.asJson(function (err, streetJson) {
+  const saveStreet = async function () {
+    try {
+      if (body && body.originalStreetId) {
+        const origStreet = await Street.findOne({ id: body.originalStreetId })
+        if (!origStreet) {
+          res.status(404).send('Original street not found.')
+          return
+        }
+
+        if (origStreet.status === 'DELETED') {
+          res.status(410).send('Original street not found.')
+          return
+        }
+
+        street.original_street_id = origStreet
+        const namespacedId = await makeNamespacedId()
+        street.namespaced_id = namespacedId
+        return street.save()
+      } else {
+        const namespacedId = await makeNamespacedId()
+        street.namespaced_id = namespacedId
+        return street.save()
+      }
+    } catch (err) {
+      res.status(404).send('Original street not found.')
+    }
+  }
+
+  const handleCreatedStreet = (s) => {
+    s.asJson((err, streetJson) => {
       if (err) {
         logger.error(err)
         res.status(500).send('Could not render street JSON.')
         return
       }
-
       logger.info({ street: streetJson }, 'New street created.')
       res.header('Location', config.restapi.baseuri + '/v1/streets/' + s.id)
       res.status(201).send(streetJson)
     })
-  } // END function - handleCreateStreet
-
-  const handleNewStreetNamespacedId = function (err, namespacedId) {
-    if (err || !namespacedId) {
-      logger.error(err)
-      res.status(500).send('Could not create new street ID.')
-      return
-    }
-
-    street.namespaced_id = namespacedId
-    street.save(handleCreateStreet)
-  } // END function - handleNewStreetNamespacedId
-
-  const makeNamespacedId = function () {
-    if (street.creator_id) {
-      User.findByIdAndUpdate(street.creator_id,
-        { $inc: { 'last_street_id': 1 } },
-        { new: true, upsert: true },
-        function (err, row) {
-          handleNewStreetNamespacedId(err, (row ? row.last_street_id : null))
-        })
-    } else {
-      Sequence.findByIdAndUpdate('streets',
-        { $inc: { 'seq': 1 } },
-        { new: true, upsert: true },
-        function (err, row) {
-          handleNewStreetNamespacedId(err, (row ? row.seq : null))
-        })
-    }
-  } // END function - makeNamespacedId
-
-  const handleFindStreet = function (err, origStreet) {
-    if (err || !origStreet) {
-      res.status(404).send('Original street not found.')
-      return
-    }
-
-    if (origStreet.status === 'DELETED') {
-      res.status(410).send('Original street not found.')
-      return
-    }
-
-    street.original_street_id = origStreet
-    makeNamespacedId()
-  } // END function - handleFindStreet
-
-  const saveStreet = function () {
-    if (body && body.originalStreetId) {
-      Street.findOne({ id: body.originalStreetId }, handleFindStreet)
-    } else {
-      makeNamespacedId()
-    }
-  } // END function - saveStreet
-
-  const handleFindUser = function (err, user) {
-    if (err || !user) {
-      res.status(401).send('User with that login token not found.')
-      return
-    }
-
-    street.creator_id = user
-    saveStreet()
-  } // END function - handleFindUser
-
-  if (req.loginToken) {
-    User.findOne({ login_tokens: { $in: [ req.loginToken ] } }, handleFindUser)
-  } else {
-    saveStreet()
   }
-} // END function - exports.post
+
+  const handleSaveStreetError = (err) => {
+    logger.error(err)
+    res.status(500).send('Could not create street.')
+  }
+
+  try {
+    if (req.loginToken) {
+      const user = await User.findOne({ login_tokens: { $in: [ req.loginToken ] } })
+      if (!user) {
+        res.status(401).send('User with that login token not found.')
+        return
+      }
+      street.creator_id = user
+      saveStreet()
+        .then(handleCreatedStreet)
+        .catch(handleSaveStreetError)
+    } else {
+      saveStreet()
+        .then(handleCreatedStreet)
+        .catch(handleSaveStreetError)
+    }
+  } catch (err) {
+    logger.error(err)
+    res.status(500).send('Could not create street.')
+  }
+}
 
 exports.delete = function (req, res) {
   const handleDeleteStreet = function (err, s) {
@@ -240,13 +247,36 @@ exports.find = function (req, res) {
   const start = (req.query.start && parseInt(req.query.start, 10)) || 0
   const count = (req.query.count && parseInt(req.query.count, 10)) || 20
 
-  const handleFindStreet = function (err, street) {
-    if (err) {
+  const findStreetWithCreatorId = async function (creatorId) {
+    try {
+      const user = await User.findOne({ id: creatorId })
+      if (!user) {
+        res.status(404).send('Creator not found.')
+        return
+      }
+      return Street.findOne({ namespaced_id: namespacedId, creator_id: user._id })
+    } catch (err) {
       logger.error(err)
-      res.status(500).send('Could not find street.')
-      return
+      res.status(500).send('Unable to find user.')
     }
+  }
 
+  const findStreetWithNamespacedId = async function (namespacedId) {
+    return Street.findOne({ namespaced_id: namespacedId, creator_id: null })
+  }
+
+  const findStreets = async function (start, count) {
+    return Promise.all([
+      Street.count({ status: 'ACTIVE' }),
+      Street.find({ status: 'ACTIVE' })
+        .sort({ updated_at: 'descending' })
+        .skip(start)
+        .limit(count)
+        .exec()
+    ])
+  }
+
+  const handleFindStreet = function (street) {
     if (!street) {
       res.status(404).send('Could not find street.')
       return
@@ -263,22 +293,7 @@ exports.find = function (req, res) {
     res.status(307).end()
   } // END function - handleFindStreet
 
-  const handleFindUser = function (err, user) {
-    if (err || !user) {
-      res.status(404).send('Creator not found.')
-      return
-    }
-
-    Street.findOne({ namespaced_id: namespacedId, creator_id: user._id }, handleFindStreet)
-  } // END function - handleFindUser
-
-  const handleFindStreets = function (err, results) {
-    if (err) {
-      logger.error(err)
-      res.status(500).send('Could not find streets.')
-      return
-    }
-
+  const handleFindStreets = function (results) {
     const totalNumStreets = results[0]
     const streets = results[1]
 
@@ -327,23 +342,25 @@ exports.find = function (req, res) {
       }) // END - async.map
   } // END function - handleFindStreets
 
-  if (creatorId) {
-    User.findOne({ id: creatorId }, handleFindUser)
-  } else if (namespacedId) {
-    Street.findOne({ namespaced_id: namespacedId, creator_id: null }, handleFindStreet)
-  } else {
-    async.parallel([
-      function (callback) { Street.count({ status: 'ACTIVE' }, callback) },
-      function (callback) {
-        Street.find({ status: 'ACTIVE' })
-          .sort({ updated_at: 'descending' })
-          .skip(start)
-          .limit(count)
-          .exec(callback)
-      }
-    ], handleFindStreets)
+  const handleFindStreetError = function (err) {
+    logger.error(err)
+    res.status(500).send('Could not find streets.')
   }
-} // END function - exports.find
+
+  if (creatorId) {
+    findStreetWithCreatorId(creatorId)
+      .then(handleFindStreet)
+      .catch(handleFindStreetError)
+  } else if (namespacedId) {
+    findStreetWithNamespacedId(namespacedId)
+      .then(handleFindStreet)
+      .catch(handleFindStreetError)
+  } else {
+    findStreets(start, count)
+      .then(handleFindStreets)
+      .catch(handleFindStreetError)
+  }
+}
 
 exports.put = function (req, res) {
   let body
