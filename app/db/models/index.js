abd7a169dcc85bab190616f0fae32cadb283e1a8
flickz <omoyajowo2015@gmail.com>
2018-08-06 13:55:22 +0100

    Update sequelize model to fix eslint error

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -5,7 +5,7 @@ var path = require('path')
 var Sequelize = require('sequelize')
 var basename = path.basename(__filename)
 var env = process.env.NODE_ENV || 'development'
-var config = require(path.join(__dirname, '/../config/config.json'))[env]
+var config = require(path.join(__dirname, '/../config/config'))[env]
 var db = {}
 
 if (config.use_env_variable) {
