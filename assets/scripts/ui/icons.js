d03eaf844734fb70284807ebb4bb527f4d36e34e
Lou Huang <lou@louhuang.com>
2018-08-30 14:36:51 -0400

    CloseButton: revert to times without circle border

--- assets/scripts/ui/icons.js
+++ assets/scripts/ui/icons.js
@@ -5,12 +5,13 @@ import {
   faMinus,
   faPlus,
   faChevronRight,
-  faChevronLeft
+  faChevronLeft,
+  faTimes
 } from '@fortawesome/free-solid-svg-icons'
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // Load Font-Awesome icons
 export function initIcons () {
-  library.add(faCheck, faMinus, faPlus, faChevronRight, faChevronLeft, faTimesCircle, fab)
+  library.add(faCheck, faMinus, faPlus, faChevronRight, faChevronLeft, faTimes, faTimesCircle, fab)
 }
