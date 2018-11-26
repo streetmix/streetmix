9ceae285a0748d8c5490be46a1c54f1a014ef2af
Lou Huang <lou@louhuang.com>
2018-11-25 21:52:33 -0500

    UndoRedo: use render prop to create text attributes, proof of concept of #1173

--- assets/scripts/app/UndoRedo.jsx
+++ assets/scripts/app/UndoRedo.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { injectIntl, intlShape } from 'react-intl'
+import { FormattedMessage } from 'react-intl'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { UNDO_ICON, REDO_ICON } from '../ui/icons'
 import { undo, redo } from '../store/actions/undo'
 import { isUndoAvailable, isRedoAvailable } from '../streets/undo_stack'
 
-class UndoRedo extends React.Component {
+export class UndoRedo extends React.Component {
   static propTypes = {
-    intl: intlShape.isRequired,
     undoPosition: PropTypes.number,
     undoStack: PropTypes.array,
     undo: PropTypes.func,
@@ -22,28 +22,32 @@ class UndoRedo extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <button
-          onClick={this.props.undo}
-          disabled={!isUndoAvailable()}
-          title={this.props.intl.formatMessage({ id: 'btn.undo', defaultMessage: 'Undo' })}
-        >
-          <FontAwesomeIcon icon="undo" />
-        </button>
-        <button
-          onClick={this.props.redo}
-          disabled={!isRedoAvailable()}
-          title={this.props.intl.formatMessage({ id: 'btn.redo', defaultMessage: 'Redo' })}
-        >
-          <FontAwesomeIcon icon="redo" />
-        </button>
+        <FormattedMessage id="btn.undo" defaultMessage="Undo">
+          {(title) => (
+            <button
+              onClick={this.props.undo}
+              disabled={!isUndoAvailable()}
+              title={title}
+            >
+              <FontAwesomeIcon icon={UNDO_ICON} />
+            </button>
+          )}
+        </FormattedMessage>
+        <FormattedMessage id="btn.redo" defaultMessage="Redo">
+          {(title) => (
+            <button
+              onClick={this.props.redo}
+              disabled={!isRedoAvailable()}
+              title={title}
+            >
+              <FontAwesomeIcon icon={REDO_ICON} />
+            </button>
+          )}
+        </FormattedMessage>
       </React.Fragment>
     )
   }
 }
-
-// Inject Intl via a higher-order component provided by react-intl.
-// Exported so that this component can be tested.
-export const UndoRedoWithIntl = injectIntl(UndoRedo)
 
 function mapStateToProps (state) {
   return {
@@ -57,4 +61,4 @@ const mapDispatchToProps = {
   redo
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UndoRedoWithIntl)
+export default connect(mapStateToProps, mapDispatchToProps)(UndoRedo)
