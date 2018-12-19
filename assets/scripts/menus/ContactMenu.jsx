5a50af6913e2e215ab1355037be99679190933fa
Lou Huang <lou@louhuang.com>
2018-12-19 12:17:19 -0500

    ContactMenu: put those forums back!

--- assets/scripts/menus/ContactMenu.jsx
+++ assets/scripts/menus/ContactMenu.jsx
@@ -19,17 +19,21 @@ class ContactMenu extends React.PureComponent {
   render () {
     return (
       <Menu {...this.props}>
-        <a href="https://twitter.com/intent/tweet?text=@streetmix" target="_blank" rel="noopener noreferrer">
-          <Icon icon="twitter" />
-          <FormattedMessage id="menu.contact.twitter" defaultMessage="Send a tweet to @streetmix" />
-        </a>
         <a href="https://discord.gg/NsKmV3S" target="_blank" rel="noopener noreferrer">
           <Icon icon="discord" />
           <FormattedMessage id="menu.contact.discord" defaultMessage="Join Discord chat" />
         </a>
+        <a href="https://forums.streetmix.net/" target="_blank" rel="noopener noreferrer">
+          <Icon icon="forums" />
+          <FormattedMessage id="menu.contact.forums" defaultMessage="Discuss on the forums" />
+        </a>
         <a href="https://github.com/streetmix/streetmix/" target="_blank" rel="noopener noreferrer">
           <Icon icon="github" />
           <FormattedMessage id="menu.contact.github" defaultMessage="View source code on GitHub" />
+        </a>
+        <a href="https://twitter.com/intent/tweet?text=@streetmix" target="_blank" rel="noopener noreferrer">
+          <Icon icon="twitter" />
+          <FormattedMessage id="menu.contact.twitter" defaultMessage="Send a tweet to @streetmix" />
         </a>
         <a href="#" onClick={this.props.showMinecraftDialog}>
           <FormattedMessage id="menu.contact.minecraft" defaultMessage="Play Minecraft with us!" />
