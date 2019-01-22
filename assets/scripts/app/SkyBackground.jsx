8824b4e76ac305ad4577d0f0df930c274fa814e3
Lou Huang <lou@louhuang.com>
2019-01-22 11:58:30 -0500

    Remove the moon

--- assets/scripts/app/SkyBackground.jsx
+++ assets/scripts/app/SkyBackground.jsx
@@ -88,7 +88,7 @@ class SkyBackground extends React.PureComponent {
           <div className="sky-background-default" />
         </div>
         <div className="sky-background-objects">
-          <div className="sky-superbloodwolfmoon" />
+          {/* <div className="sky-superbloodwolfmoon" /> */}
         </div>
         <div className="rear-clouds" style={rearCloudStyle} />
         <div className="front-clouds" style={frontCloudStyle} />
