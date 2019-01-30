1782c45cd594651a18c69214b05a278b9904d9bc
Lou Huang <lou@louhuang.com>
2019-01-29 21:17:27 -0500

    CloseButton: update style

--- assets/scripts/ui/CloseButton.jsx
+++ assets/scripts/ui/CloseButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { injectIntl, intlShape } from 'react-intl'
+import './CloseButton.scss'
 
 class CloseButton extends React.Component {
   static propTypes = {
