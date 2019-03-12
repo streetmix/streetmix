b3faf9947c6a9cbf99890a61d0061103e08048db
milafrerichs <mila.frerichs@gmail.com>
2019-03-12 10:49:42 +0100

    Add more tests to Variants
    add building tests and add tests for boundaries (prop functions) that
    are beeing called on click
    consider using the info.json object instead of hard-coding it (might be
    easier to maintain)

--- assets/scripts/info_bubble/__tests__/Variants.test.js
+++ assets/scripts/info_bubble/__tests__/Variants.test.js
@@ -18,15 +18,56 @@ jest.mock('../../segments/info')
 describe('Variants', () => {
   const intlProvider = new IntlProvider({ locale: 'en' }, {})
   const { intl } = intlProvider.getChildContext()
-  let segment
-  beforeEach(() => {
-    segment = { variants: ['direction', 'public-transit-asphalt'] } // coming from info.json
-    getSegmentInfo.mockImplementation(() => segment)
+  it('does not render component', () => {
+    const wrapper = shallow(<Variants intl={intl} />)
+    expect(wrapper).toMatchSnapshot()
   })
-  it('renders', () => {
+  describe('INFO_BUBBLE_TYPE_SEGMENT', () => {
+    let segment
+    beforeEach(() => {
+      segment = { variants: ['direction', 'public-transit-asphalt'] } // coming from info.json
+      getSegmentInfo.mockImplementation(() => segment)
+    })
     const type = INFO_BUBBLE_TYPE_SEGMENT
     const streetSegment = { variantString: 'inbound|regular', segmentType: 'streetcar' }
-    const wrapper = shallow(<Variants intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
-    expect(wrapper).toMatchSnapshot()
+    it('renders segment type correctly', () => {
+      const wrapper = shallow(<Variants intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+    it('onClick calls changeSegmentVariant', () => {
+      const changeSegmentVariant = jest.fn()
+      const wrapper = shallow(<Variants changeSegmentVariant={changeSegmentVariant} intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
+      wrapper.find('button').first().simulate('click')
+      expect(changeSegmentVariant).toHaveBeenCalledTimes(1)
+      expect(changeSegmentVariant).toHaveBeenCalledWith(0, 'direction', 'inbound')
+    })
+  })
+  describe('INFO_BUBBLE_TYPE_LEFT_BUILDING', () => {
+    const type = INFO_BUBBLE_TYPE_LEFT_BUILDING
+    it('onClick calls setBuildingVariant', () => {
+      const setBuildingVariant = jest.fn()
+      const wrapper = shallow(<Variants setBuildingVariant={setBuildingVariant} intl={intl} type={type} position={'left'} variant={'residential'} />)
+      wrapper.find('button').first().simulate('click')
+      expect(setBuildingVariant).toHaveBeenCalledTimes(1)
+      expect(setBuildingVariant).toHaveBeenCalledWith('left', 'waterfront')
+    })
+    it('renders left building correctly', () => {
+      const wrapper = shallow(<Variants intl={intl} type={type} position={'left'} variant={'residential'} />)
+      expect(wrapper).toMatchSnapshot()
+    })
+  })
+  describe('INFO_BUBBLE_TYPE_RIGHT_BUILDING', () => {
+    const type = INFO_BUBBLE_TYPE_RIGHT_BUILDING
+    it('onClick calls setBuildingVariant', () => {
+      const setBuildingVariant = jest.fn()
+      const wrapper = shallow(<Variants setBuildingVariant={setBuildingVariant} intl={intl} type={type} position={'right'} variant={'residential'} />)
+      wrapper.find('button').first().simulate('click')
+      expect(setBuildingVariant).toHaveBeenCalledTimes(1)
+      expect(setBuildingVariant).toHaveBeenCalledWith('right', 'waterfront')
+    })
+    it('renders left building correctly', () => {
+      const wrapper = shallow(<Variants intl={intl} type={type} position={'right'} variant={'residential'} />)
+      expect(wrapper).toMatchSnapshot()
+    })
   })
 })
