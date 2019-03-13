1979ea5441d57e351d317e538a75cc7b2424a903
Mandy Kong <mk5506@nyu.edu>
2019-03-13 12:14:47 -0400

    updated expected status codes for POST api/v1/streets/images

--- app/resources/v1/__tests__/street_images.test.js
+++ app/resources/v1/__tests__/street_images.test.js
@@ -27,7 +27,7 @@ describe('POST api/v1/streets/images/:street_id', () => {
 
   cloudinary.v2.uploader.upload.mockResolvedValue('foo')
 
-  it('should respond with 200 Ok when a data url is sent', () => {
+  it('should respond with 201 Created when a data url is sent', () => {
     cloudinary.v2.api.resource.mockResolvedValueOnce('baz')
 
     return request(app)
@@ -36,11 +36,11 @@ describe('POST api/v1/streets/images/:street_id', () => {
       .type('text/plain')
       .send('bar')
       .then((response) => {
-        expect(response.statusCode).toEqual(200)
+        expect(response.statusCode).toEqual(201)
       })
   })
 
-  it('should respond with 200 Ok when street thumbnail does not exist', () => {
+  it('should respond with 201 Created when street thumbnail does not exist', () => {
     cloudinary.v2.api.resource.mockReturnValueOnce(null)
 
     return request(app)
@@ -48,11 +48,11 @@ describe('POST api/v1/streets/images/:street_id', () => {
       .type('text/plain')
       .send('bar')
       .then((response) => {
-        expect(response.statusCode).toEqual(200)
+        expect(response.statusCode).toEqual(201)
       })
   })
 
-  it('should respond with 404 when user is not owner of street', () => {
+  it('should respond with 403 Forbidden when user is not owner of street', () => {
     cloudinary.v2.api.resource.mockResolvedValueOnce('baz')
 
     return request(app)
@@ -61,7 +61,7 @@ describe('POST api/v1/streets/images/:street_id', () => {
       .type('text/plain')
       .send('bar')
       .then((response) => {
-        expect(response.statusCode).toEqual(404)
+        expect(response.statusCode).toEqual(403)
       })
   })
 })
