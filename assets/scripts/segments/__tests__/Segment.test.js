e87b2f2d710a202030f4d41406e905deef86f0d3
Mila Frerichs <mila.frerichs@gmail.com>
2019-04-04 12:48:21 +0200

    Remove old enzyme tests
    port important enzmye tests to react-testing-library
    all others should be tested with the parent component StreetEditable or
    were not important

--- assets/scripts/segments/__tests__/Segment.test.js
+++ assets/scripts/segments/__tests__/Segment.test.js
@@ -1,22 +1,16 @@
 /* eslint-env jest */
 import React from 'react'
-import { shallow } from 'enzyme'
-import { fireEvent, getByTestId } from 'react-testing-library'
+import { fireEvent, getByTestId, cleanup } from 'react-testing-library'
 
 import { renderWithRedux } from '../../../../test/helpers/render'
 
-import ConnectedSegment, { Segment } from '../Segment'
+import ConnectedSegment from '../Segment'
 import { getSpriteDef, getSegmentInfo, getSegmentVariantInfo } from '../info'
 // import { getVariantInfoDimensions } from '../view'
 import { infoBubble } from '../../info_bubble/info_bubble'
-import { SETTINGS_UNITS_IMPERIAL } from '../../users/constants'
 import SEGMENT_INFO from '../info.json'
-import {
-  SEGMENT_WARNING_OUTSIDE,
-  SEGMENT_WARNING_WIDTH_TOO_SMALL,
-  SEGMENT_WARNING_WIDTH_TOO_LARGE
-} from '../constants'
 import { KEYS } from '../../app/keys'
+import { setLastStreet } from '../../streets/data_model'
 
 jest.mock('../../app/load_resources')
 jest.mock('../info')
@@ -40,93 +34,61 @@ jest.mock('react-dnd', () => {
     DropTarget: (d) => (jest.fn((e) => e))
   }
 })
+jest.mock('../../info_bubble/info_bubble')
 
 function connectDropTarget (el) { return el }
 function connectDragSource (el) { return el }
-const connectDragPreview = () => { }
 
 describe('Segment', () => {
-  const updateSegmentData = jest.fn()
-  let segment = {}
+  let variantString, type, variant, currentWidth, increment, activeElement, segment
   beforeEach(() => {
-    const variant = {}
-    const segment = { name: 'Segment', nameKey: 'key', zIndex: 1 }
-    getSegmentInfo.mockImplementation(() => segment)
-    getSegmentVariantInfo.mockImplementation(() => variant)
-    // getVariantInfoDimensions.mockImplementation(() => ({ left: 0, right: 100, center: 50}))
-  })
-  describe('on mount', () => {
-    it('updates the left position for this segment', () => {
-      shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} />)
-      expect(updateSegmentData).toHaveBeenCalledTimes(1)
-    })
-  })
-  describe('if the segment variant is updated', () => {
-    let updatePerspective
-    beforeEach(() => {
-      segment.variantString = 'variant'
-      updatePerspective = jest.fn()
-    })
-    describe('variant is different', () => {
-      it('updates perspective', () => {
-        const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} updatePerspective={updatePerspective} />)
-        wrapper.setProps({ segment: { variantString: 'other' } })
-        expect(updatePerspective).toHaveBeenCalledTimes(4) // should probably only be 2
-      })
-    })
-    it('updates the left position for this segment', () => {
-      const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} updatePerspective={updatePerspective} />)
-      wrapper.setProps({ segment: { variantString: 'other' } })
-      expect(updateSegmentData).toHaveBeenCalledTimes(7) // should probably only be 1
-    })
-  })
-  describe('shows the infobubble', () => {
-    it('when segment removing or dragging action ends', () => {
-      const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} isDragging />)
-      wrapper.setProps({ isDragging: false })
-      expect(infoBubble.considerShowing).toHaveBeenCalledTimes(1)
-    })
-  })
-  it('renders the units correctly', () => {
-    const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} units={SETTINGS_UNITS_IMPERIAL} cssTransform={'transform'} segmentPos={1} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-  it('renders active segment correctly', () => {
-    const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} isDragging={false} dataNo={10} activeSegment={10} cssTransform={'transform'} segmentPos={1} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-  it('renders segment warnings outside correctly', () => {
-    segment.warnings = {}
-    segment.warnings[SEGMENT_WARNING_OUTSIDE] = 'warning'
-    const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} isDragging={false} dataNo={10} activeSegment={10} cssTransform={'transform'} segmentPos={1} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-  it('renders segment warnings correctly', () => {
-    segment.warnings = {}
-    segment.warnings[SEGMENT_WARNING_WIDTH_TOO_SMALL] = 'warning'
-    const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} isDragging={false} dataNo={10} activeSegment={10} cssTransform={'transform'} segmentPos={1} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-  it('renders segment warnings width too large correctly', () => {
-    segment.warnings = {}
-    segment.warnings[SEGMENT_WARNING_WIDTH_TOO_LARGE] = 'warning'
-    const wrapper = shallow(<Segment connectDropTarget={connectDropTarget} connectDragSource={connectDragSource} segment={segment} actualWidth={1} updateSegmentData={updateSegmentData} connectDragPreview={connectDragPreview} isDragging={false} dataNo={10} activeSegment={10} cssTransform={'transform'} segmentPos={1} />)
-    expect(wrapper).toMatchSnapshot()
-  })
-  it.only('renders correctly with deep render', async () => {
-    const variantString = 'inbound|regular'
-    const type = 'streetcar'
-    const variant = SEGMENT_INFO[type].details[variantString]
-    const currentWidth = 400
-    const increment = 1
-    const activeElement = 0
+    variantString = 'inbound|regular'
+    type = 'streetcar'
+    variant = SEGMENT_INFO[type].details[variantString]
+    currentWidth = 400
+    increment = 1
+    activeElement = 0
+    segment = { type, variantString, segmentType: type, id: '1', width: currentWidth, randSeed: 1 }
+    const segmentInfo = { name: 'Segment', nameKey: 'key', zIndex: 1 }
+    getSegmentInfo.mockImplementation(() => segmentInfo)
     getSegmentVariantInfo.mockImplementation(() => variant)
     getSpriteDef.mockImplementation(() => ({ id: 'markings--straight-inbound', width: 4, offsetY: 11.12 }))
-    // getSpriteDef should either be unmocked or be defined
-    segment = { type, variantString, segmentType: type, id: '1', width: currentWidth, randSeed: 1 }
+  })
+  afterEach(cleanup)
+  it('renders correctly', () => {
+    const wrapper = renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={jest.fn()} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
+    expect(wrapper.asFragment()).toMatchSnapshot()
+  })
+  // maybe test this with StreetEditable as well
+  it('updates the left position for this segment', () => {
+    const updateSegmentData = jest.fn()
+    renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={updateSegmentData} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
+    expect(updateSegmentData).toHaveBeenCalledTimes(1)
+  })
+  it('shows the info bubble on mouseover', () => {
     const wrapper = renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={jest.fn()} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
     fireEvent.mouseOver(getByTestId(wrapper.container, 'segment'))
-    fireEvent.keyDown(document, { key: 'Minus', keyCode: KEYS.MINUS, code: KEYS.MINUS, charCode: KEYS.MINUS })
-    expect(wrapper.store.getState().street.segments[activeElement].width).toEqual(currentWidth - increment)
+    expect(infoBubble.considerShowing).toHaveBeenCalledTimes(1)
+  })
+  it('hides the info bubble on mouseleave', () => {
+    const wrapper = renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={jest.fn()} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
+    fireEvent.mouseOver(getByTestId(wrapper.container, 'segment'))
+    fireEvent.mouseLeave(getByTestId(wrapper.container, 'segment'))
+    expect(infoBubble.dontConsiderShowing).toHaveBeenCalledTimes(1)
+  })
+  describe('keyboard events', () => {
+    it('updates the store when reducing the width of the segment via keyboard', () => {
+      const wrapper = renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={jest.fn()} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
+      fireEvent.mouseOver(getByTestId(wrapper.container, 'segment'))
+      fireEvent.keyDown(document, { key: 'Minus', keyCode: KEYS.MINUS, code: KEYS.MINUS, charCode: KEYS.MINUS })
+      expect(wrapper.store.getState().street.segments[activeElement].width).toEqual(currentWidth - increment)
+    })
+    it('removes the store when reducing the width of the segment via keyboard', () => {
+      const wrapper = renderWithRedux(<ConnectedSegment connectDragSource={connectDragSource} connectDropTarget={connectDropTarget} segment={segment} actualWidth={currentWidth} dataNo={activeElement} updateSegmentData={jest.fn()} connectDragPreview={jest.fn()} />, { initialState: { ui: { activeSegment: activeElement, unitSettings: { resolution: 1, clickIncrement: 1 } }, street: { segments: [segment] } } })
+      setLastStreet() // ToDo: needs to be refactored
+      fireEvent.mouseOver(getByTestId(wrapper.container, 'segment'))
+      fireEvent.keyDown(document, { key: 'Delete', keyCode: KEYS.DELETE, code: KEYS.DELETE, charCode: KEYS.DELETE })
+      expect(wrapper.store.getState().street.segments.length).toEqual(0)
+    })
   })
 })
