8156d16acff1af2145eca3b6dae65329071c569c
Mandy Kong <mk5506@nyu.edu>
2019-04-12 18:25:25 -0400

    set _lastSavedTimestamp before async request

--- assets/scripts/streets/image.js
+++ assets/scripts/streets/image.js
@@ -82,7 +82,7 @@ export function initStreetThumbnailSubscriber () {
     _savedThumbnail = false
 
     // Save street thumbnail every 30 minutes if any changes to street.
-    if (timeElapsed >= SAVE_THUMBNAIL_TIME_INTERVAL) {
+    if (timeElapsed && timeElapsed >= SAVE_THUMBNAIL_TIME_INTERVAL) {
       const street = trimStreetData(store.getState().street)
       saveStreetThumbnail(street, SAVE_THUMBNAIL_EVENTS.TIMER)
     }
@@ -95,6 +95,7 @@ export function initStreetThumbnailSubscriber () {
 export async function saveStreetThumbnail (street, event) {
   if (_savedThumbnail) return
 
+  _lastSavedTimestamp = Date.now()
   const thumbnail = getStreetImage(street, false, false, true, 2.0, false)
 
   try {
@@ -127,7 +128,6 @@ export async function saveStreetThumbnail (street, event) {
     }
 
     const response = await window.fetch(url, options)
-    _lastSavedTimestamp = Date.now()
 
     if (response.ok) {
       console.log('Updated street thumbnail.')
