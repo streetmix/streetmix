292cdcfc151504b79e7bfff8cd7b66260acb5153
Lou Huang <lou@louhuang.com>
2019-04-28 18:51:54 -0700

    test(Variants): fix required prop validation warnings

--- assets/scripts/info_bubble/__tests__/Variants.test.js
+++ assets/scripts/info_bubble/__tests__/Variants.test.js
@@ -19,69 +19,93 @@ jest.mock('../../segments/variant_icons.json', () => require('../../segments/__m
 describe('Variants', () => {
   const intlProvider = new IntlProvider({ locale: 'en' }, {})
   const { intl } = intlProvider.getChildContext()
+
+  // Mock required props for component
+  const requiredProps = {
+    changeSegmentVariant: jest.fn(),
+    setBuildingVariant: jest.fn(),
+    flags: {},
+    intl: intl
+  }
+
   it('does not render component', () => {
-    const wrapper = shallow(<Variants intl={intl} />)
+    const wrapper = shallow(<Variants {...requiredProps} />)
     expect(wrapper).toMatchSnapshot()
   })
+
   describe('INFO_BUBBLE_TYPE_SEGMENT', () => {
     let segment
+
     beforeEach(() => {
       segment = { variants: ['direction', 'public-transit-asphalt'] } // coming from info.json
       getSegmentInfo.mockImplementation(() => segment)
     })
+
     const type = INFO_BUBBLE_TYPE_SEGMENT
     const streetSegment = { variantString: 'inbound|regular', segmentType: 'streetcar' }
+
     it('renders segment type correctly', () => {
-      const wrapper = shallow(<Variants intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
+      const wrapper = shallow(<Variants {...requiredProps} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
       expect(wrapper).toMatchSnapshot()
     })
+
     it('onClick calls changeSegmentVariant', () => {
       const changeSegmentVariant = jest.fn()
-      const wrapper = shallow(<Variants changeSegmentVariant={changeSegmentVariant} intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
+      const wrapper = shallow(<Variants {...requiredProps} changeSegmentVariant={changeSegmentVariant} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} />)
       wrapper.find('button').first().simulate('click')
       expect(changeSegmentVariant).toHaveBeenCalledTimes(1)
       expect(changeSegmentVariant).toHaveBeenCalledWith(0, 'direction', 'inbound')
     })
   })
+
   describe('INFO_BUBBLE_TYPE_LEFT_BUILDING', () => {
     const type = INFO_BUBBLE_TYPE_LEFT_BUILDING
+
     it('onClick calls setBuildingVariant', () => {
       const setBuildingVariant = jest.fn()
-      const wrapper = shallow(<Variants setBuildingVariant={setBuildingVariant} intl={intl} type={type} position={'left'} variant={'residential'} />)
+      const wrapper = shallow(<Variants {...requiredProps} setBuildingVariant={setBuildingVariant} type={type} position={'left'} variant={'residential'} />)
       wrapper.find('button').first().simulate('click')
       expect(setBuildingVariant).toHaveBeenCalledTimes(1)
       expect(setBuildingVariant).toHaveBeenCalledWith('left', 'waterfront')
     })
+
     it('renders left building correctly', () => {
-      const wrapper = shallow(<Variants intl={intl} type={type} position={'left'} variant={'residential'} />)
+      const wrapper = shallow(<Variants {...requiredProps} type={type} position={'left'} variant={'residential'} />)
       expect(wrapper).toMatchSnapshot()
     })
   })
+
   describe('INFO_BUBBLE_TYPE_RIGHT_BUILDING', () => {
     const type = INFO_BUBBLE_TYPE_RIGHT_BUILDING
+
     it('onClick calls setBuildingVariant', () => {
       const setBuildingVariant = jest.fn()
-      const wrapper = shallow(<Variants setBuildingVariant={setBuildingVariant} intl={intl} type={type} position={'right'} variant={'residential'} />)
+      const wrapper = shallow(<Variants {...requiredProps} setBuildingVariant={setBuildingVariant} type={type} position={'right'} variant={'residential'} />)
       wrapper.find('button').first().simulate('click')
       expect(setBuildingVariant).toHaveBeenCalledTimes(1)
       expect(setBuildingVariant).toHaveBeenCalledWith('right', 'waterfront')
     })
+
     it('renders left building correctly', () => {
-      const wrapper = shallow(<Variants intl={intl} type={type} position={'right'} variant={'residential'} />)
+      const wrapper = shallow(<Variants {...requiredProps} type={type} position={'right'} variant={'residential'} />)
       expect(wrapper).toMatchSnapshot()
     })
   })
+
   describe('flag', () => {
     let segment
+
     beforeEach(() => {
       segment = { variants: ['building-z', 'waterfront'] } // coming from info.json
       getSegmentInfo.mockImplementation(() => segment)
     })
+
     const type = INFO_BUBBLE_TYPE_SEGMENT
     const streetSegment = { variantString: 'inbound|regular', segmentType: 'streetcar' }
     const flags = { 'flag': { value: 't' } }
+
     it('renders with flag correctly', () => {
-      const wrapper = shallow(<Variants intl={intl} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} flags={flags} />)
+      const wrapper = shallow(<Variants {...requiredProps} type={type} position={0} variant={streetSegment.variantString} segmentType={streetSegment.segmentType} flags={flags} />)
       expect(wrapper).toMatchSnapshot()
     })
   })
