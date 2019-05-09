e8a79adfdc8b4efd49592576ef324a5c78cd61d4
Lou Huang <lou@louhuang.com>
2019-05-09 09:46:11 -0400

    refactor: remove Number polyfills (covered by core-js)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -20,7 +20,6 @@ import './vendor/canvas-toBlob.js'
 import './vendor/Blob.js'
 import './vendor/modernizr-custom'
 import './vendor/polyfills/customevent' // customEvent in IE
-import './vendor/polyfills/Number.isInteger' // for IE
 import './vendor/polyfills/Element.closest'
 import './vendor/polyfills/Element.remove'
 
