1c77c99da5a5a98dd3d3a0e6f28956dcef20466e
Lou Huang <lou@louhuang.com>
2019-05-09 13:56:21 -0400

    refactor(StreetViewDirt): handle height setting with hook

--- assets/scripts/app/StreetViewDirt.jsx
+++ assets/scripts/app/StreetViewDirt.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { getStreetSectionTop } from './window_resize'
 import './StreetViewDirt.scss'
+
+// Legacy height setter w/ DOM methods.
+function handleResize () {
+  const streetSectionTop = getStreetSectionTop()
+  const streetSectionDirtPos = window.innerHeight - streetSectionTop - 400 + 180
+  document.getElementById('street-section-dirt').style.height = streetSectionDirtPos + 'px'
+}
 
 const StreetViewDirt = (props) => {
   const dirtStyle = {
@@ -8,6 +16,19 @@ const StreetViewDirt = (props) => {
     marginRight: (-props.buildingWidth) + 'px'
   }
   const width = `${props.buildingWidth}px`
+
+  // On window resize, figure out what height it should be and apply it
+  // TODO: less stop relying on querying other DOM elements
+  // (this is legacy behavior, ported to this component)
+  useEffect(() => {
+    // Set window listener to do this
+    window.addEventListener('resize', handleResize)
+    // Do it one time
+    handleResize()
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <section id="street-section-dirt" style={dirtStyle}>
