a5dc12a550549be97c9f50d67b109edbe77e039d
Mandy Kong <mk5506@nyu.edu>
2019-05-15 17:43:02 -0400

    handle updating inputValue to highlighted item

--- assets/scripts/dialogs/Geotag/GeoSearch.jsx
+++ assets/scripts/dialogs/Geotag/GeoSearch.jsx
@@ -25,6 +25,10 @@ class GeoSearch extends React.Component {
   constructor (props) {
     super(props)
 
+    this.state = {
+      initialQuery: ''
+    }
+
     this.pelias = new Pelias({ peliasUrl: `https://${PELIAS_HOST_NAME}`, apiKey: PELIAS_API_KEY })
     this.inputEl = React.createRef()
   }
@@ -49,14 +53,20 @@ class GeoSearch extends React.Component {
     this.inputEl.current.focus()
   }
 
-  renderSuggestion = (item, index, inputValue, getItemProps) => {
+  renderSuggestion = (item, index, downshift) => {
     const label = item.properties.label
+    const { inputValue, getItemProps } = downshift
+
+    // In order to handle changing the inputValue when highlighting an item, this.state.initialQuery is either null or
+    // the initial query string before the user pressed the up/down arrow keys to go through the items. If null, the
+    // query string to bold is equal to the inputValue given by downshift. Otherwise, it is equal to this.state.initialQuery.
+    const query = this.state.initialQuery || inputValue
 
     // Highlight the input query
-    const regex = new RegExp(`(${inputValue})`, 'gi')
+    const regex = new RegExp(`(${query})`, 'gi')
     const parts = label.split(regex)
     const highlighted = parts.map((part, index) => {
-      if (part.toLowerCase() === inputValue.toLowerCase()) {
+      if (part.toLowerCase() === query.toLowerCase()) {
         return <strong key={index}>{part}</strong>
       }
       return part
@@ -74,6 +84,27 @@ class GeoSearch extends React.Component {
     )
   }
 
+  stateReducer = (state, changes) => {
+    switch (changes.type) {
+      case '__autocomplete_keydown_arrow_up__':
+      case '__autocomplete_keydown_arrow_down__':
+        const highlightedItem = this.results && this.results.features && this.results.features[changes.highlightedIndex]
+
+        if (!this.state.initialQuery) {
+          this.setState({ initialQuery: state.inputValue })
+        }
+
+        return {
+          ...changes,
+          inputValue: (highlightedItem && highlightedItem.properties.label) || state.inputValue
+        }
+      default:
+        return changes
+    }
+  }
+
+  handleInputValueChange = () => { this.setState({ initialQuery: null }) }
+
   render () {
     const { focus } = this.props
 
@@ -81,7 +112,12 @@ class GeoSearch extends React.Component {
     this.pelias.autocomplete.setFocusPoint({ lat: focus.lat, lon: focus.lng })
 
     return (
-      <DownshiftPelias pelias={this.pelias} onChange={this.handleChange}>
+      <DownshiftPelias
+        pelias={this.pelias}
+        onChange={this.handleChange}
+        stateReducer={this.stateReducer}
+        handleInputValueChange={this.handleInputValueChange}
+      >
         {({
           getInputProps,
           getMenuProps,
@@ -90,32 +126,39 @@ class GeoSearch extends React.Component {
           inputValue,
           isOpen,
           results
-        }) => (
-          <div className="geotag-input-form">
-            <input {...getInputProps({
-              className: 'geotag-input',
-              autoFocus: true,
-              ref: this.inputEl,
-              placeholder: this.props.intl.formatMessage({ id: 'dialogs.geotag.search', defaultMessage: 'Search for a location' })
-            })} />
-            { inputValue && (
-              <span
-                title={this.props.intl.formatMessage({ id: 'dialogs.geotag.clear-search', defaultMessage: 'Clear search' })}
-                className="geotag-input-clear"
-                onClick={() => { this.onClickClearSearch(clearSelection) }}
-              >
-                ×
-              </span>
-            )}
-            { isOpen && results && results.features.length > 0 && (
-              <div className="geotag-suggestions-container">
-                <ul {...getMenuProps({ className: 'geotag-suggestions-list' })}>
-                  { results.features.map((item, index) => this.renderSuggestion(item, index, inputValue, getItemProps)) }
-                </ul>
-              </div>
-            )}
-          </div>
-        )}
+        }) => {
+          this.results = results
+
+          return (
+            <div className="geotag-input-form">
+              <input {...getInputProps({
+                className: 'geotag-input',
+                autoFocus: true,
+                ref: this.inputEl,
+                placeholder: this.props.intl.formatMessage({ id: 'dialogs.geotag.search', defaultMessage: 'Search for a location' })
+              })} />
+              { inputValue && (
+                <span
+                  title={this.props.intl.formatMessage({ id: 'dialogs.geotag.clear-search', defaultMessage: 'Clear search' })}
+                  className="geotag-input-clear"
+                  onClick={() => { this.onClickClearSearch(clearSelection) }}
+                >
+                  ×
+                </span>
+              )}
+              { isOpen && results && results.features.length > 0 && (
+                <div className="geotag-suggestions-container">
+                  <ul {...getMenuProps({ className: 'geotag-suggestions-list' })}>
+                    { results.features.map((item, index) => {
+                      const downshift = { inputValue, getItemProps }
+                      return this.renderSuggestion(item, index, downshift)
+                    })}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )
+        }}
       </DownshiftPelias>
     )
   }
