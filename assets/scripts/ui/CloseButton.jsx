df98c6b089d52ae746233046f06bdd8f805e76b7
Lou Huang <lou@louhuang.com>
2019-05-23 13:32:57 -0400

    refactor(CloseButton): convert to function component & update tests

--- assets/scripts/ui/CloseButton.jsx
+++ assets/scripts/ui/CloseButton.jsx
@@ -5,36 +5,34 @@ import { injectIntl, intlShape } from 'react-intl'
 import { ICON_TIMES } from '../ui/icons'
 import './CloseButton.scss'
 
-class CloseButton extends React.Component {
-  static propTypes = {
-    onClick: PropTypes.func.isRequired,
-    title: PropTypes.string,
-    className: PropTypes.string,
-    intl: intlShape.isRequired
-  }
+const CloseButton = (props) => {
+  const { title, className, onClick, intl, ...restProps } = props
+  const displayTitle = title || intl.formatMessage({
+    id: 'btn.dismiss',
+    defaultMessage: 'Dismiss'
+  })
 
-  static defaultProps = {
-    className: 'close'
-  }
-
-  render () {
-    const { title, className, onClick, intl, ...restProps } = this.props
-    const displayTitle = title || intl.formatMessage({
-      id: 'btn.dismiss',
-      defaultMessage: 'Dismiss'
-    })
-
-    return (
-      <button
-        className={className}
-        onClick={onClick}
-        title={displayTitle}
-        {...restProps}
-      >
-        <FontAwesomeIcon icon={ICON_TIMES} />
-      </button>
-    )
-  }
+  return (
+    <button
+      className={className}
+      onClick={onClick}
+      title={displayTitle}
+      {...restProps}
+    >
+      <FontAwesomeIcon icon={ICON_TIMES} />
+    </button>
+  )
 }
 
-export default injectIntl(CloseButton)
+CloseButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  className: PropTypes.string,
+  intl: intlShape.isRequired
+}
+
+CloseButton.defaultProps = {
+  className: 'close'
+}
+
+export default React.memo(injectIntl(CloseButton))
