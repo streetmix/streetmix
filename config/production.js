d7928627c3e8091bb440c2a36311bd5b8dd9daee
Lou Huang <lou@louhuang.com>
2019-06-09 20:31:20 -0400

    feat(prod): add pinterest domain verification

--- config/production.js
+++ config/production.js
@@ -10,6 +10,7 @@ module.exports = {
   // Temporarily disable mixpanel
   // mixpanel_token: '61e4b1fdd39e00551df8911fe62b8c56',
   luckyorange_enabled: true,
+  pinterest: '0175a0c658a16a45e7c1f6b7cefaa34f',
   db: {
     // MONGOHQ_URL - Compose MongoDB Heroku addon - used in production environments
     // MONGODB_URI - mLab MongoDB Heroku addon - used in free dyno "production-like" environments
