d1950910215405f6c0c6686a4d0cb4c58707369a
Lou Huang <lou@louhuang.com>
2019-08-26 12:12:51 -0400

    test: consolidate react-transition-group and react-dnd mocks

--- test/__mocks__/react-transition-group.js
+++ test/__mocks__/react-transition-group.js
@@ -1,5 +1,15 @@
 /* eslint-env jest */
 
-export const Transition = jest.fn((props) => props.in ? props.children : null)
-export const CSSTransition = jest.fn((props) => props.in ? props.children : null)
+/**
+ * The only functionality we care about from <Transition /> and <CSSTransition /> is the
+ * `in` prop. If it's `true`, return children. If `false`, return `null`. If `in` prop
+ * is not provided, assume it is injected by `TransitionGroup` and use the default
+ * value of `true`.
+ */
+export const Transition = jest.fn(({ children, in: show = true }) => (show ? children : null))
+export const CSSTransition = jest.fn(({ children, in: show = true }) => (show ? children : null))
+
+/**
+ * <TransitionGroup /> just returns its children.
+ */
 export const TransitionGroup = jest.fn(({ children }) => children)
