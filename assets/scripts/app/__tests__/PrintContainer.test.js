54a4fe304319dd9bced2002c7d84add94689dbd8
Lou Huang <lou@louhuang.com>
2019-09-16 12:50:18 -0400

    refactor(PrintContainer): convert to function component

---
+++ assets/scripts/app/__tests__/PrintContainer.test.js
@@ -0,0 +1,32 @@
+/* eslint-env jest */
+import React from 'react'
+import { cleanup } from '@testing-library/react'
+import { renderWithRedux } from '../../../../test/helpers/render'
+import PrintContainer from '../PrintContainer'
+
+// `matchMedia` is not available in test environment, so we mock it
+global.matchMedia = () => ({
+  addListener: () => {},
+  removeListener: () => {}
+})
+
+jest.mock('../../streets/image', () => ({
+  getStreetImage: () => ({
+    toDataURL: () => 'foo'
+  })
+}))
+
+describe('PrintContainer', () => {
+  afterEach(cleanup)
+
+  it('renders', () => {
+    const wrapper = renderWithRedux(<PrintContainer />, { initialState: { app: { printing: false } } })
+    const el = wrapper.container.querySelector('.print-container')
+    expect(el).toBeInTheDocument()
+  })
+
+  it('renders image for printing', () => {
+    const wrapper = renderWithRedux(<PrintContainer />, { initialState: { app: { printing: true } } })
+    expect(wrapper.getByRole('img')).toBeInTheDocument()
+  })
+})
