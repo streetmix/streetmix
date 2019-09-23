d383243c7595a2e841dfcfadbf5da786c32a5639
Lou Huang <lou@louhuang.com>
2019-09-23 14:23:27 -0400

    refactor(AvatarMenu)

--- assets/scripts/menus/AvatarMenu.jsx
+++ assets/scripts/menus/AvatarMenu.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Avatar from '../users/Avatar'
 import USER_ROLES from '../../../app/data/user_roles'
+import Avatar from '../users/Avatar'
 import { ICON_CROWN } from '../ui/icons'
 import './AvatarMenu.scss'
 
+AvatarMenu.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    roles: PropTypes.arrayOf(PropTypes.string)
+  }).isRequired,
+  onClick: PropTypes.func
+}
+
 function AvatarMenu (props) {
-  const { user, onClick } = props
-  const { id, roles } = user
-  const isAdmin = roles && roles.includes(USER_ROLES.ADMIN.value)
+  const { user, onClick = () => {} } = props
+  const { id, roles = [] } = user
+  const isAdmin = roles.includes(USER_ROLES.ADMIN.value)
 
   return (
     <button
@@ -21,18 +29,6 @@ function AvatarMenu (props) {
       <span className="user-id">{id}</span>
     </button>
   )
-}
-
-AvatarMenu.propTypes = {
-  user: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    roles: PropTypes.arrayOf(PropTypes.string)
-  }).isRequired,
-  onClick: PropTypes.func
-}
-
-AvatarMenu.defaultProps = {
-  onClick: () => {}
 }
 
 export default AvatarMenu
