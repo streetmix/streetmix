aeec538c6a3d0628e87dcc57deb60bd433794bb7
Whitman Schorn <wschorn@quartethealth.com>
2019-10-01 02:04:53 -0400

    feature(swagger) add swagger documentation and split routes into 'api' and 'services' add swagger json file
    add yaml file
    
    feature(swagger) add swagger for API documentation serving at /api-docs
    
    mid
    
    no need for new file
    
    no need for new file ii
    
    package cleanup
    
    lock
    
    add api routes file
    
    using shared definition of street
    
    mid
    
    more documentation
    
    more defs
    
    mid finding the way
    
    street update
    
    mid thumbnails
    
    mid
    
    more updates
    
    mid
    
    final routes
    
    add file
    
    review
    
    review
    
    move stuff
    
    lock
    
    fix

---
+++ app/service_routes.js
@@ -0,0 +1,98 @@
+const routes = require('express').Router()
+const cors = require('cors')
+const resources = require('./resources')
+
+// API: all streets
+/**
+ * @swagger
+ *
+ * definitions:
+ *   NewSubscription:
+ *     type: object
+ *     properties:
+ *       userId:
+ *         type: string
+ *       token:
+ *         type: object
+ *         properties:
+ *           email:
+ *             type: string
+ *           id:
+ *             type: string
+ */
+
+/**
+ * @swagger
+ *
+ * /services/pay:
+ *   post:
+ *     description: Creates a payment for a streetmix subscription
+ *     tags:
+ *       - payment
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: street
+ *         description: Street object
+ *         in:  body
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/NewSubscription'
+ *     responses:
+ *       200:
+ *         description: Success
+ */
+routes.post('/services/pay', resources.services.payments.post)
+
+/**
+ * @swagger
+ * /services/geoip:
+ *   get:
+ *     description: Returns geolocation data for the current user
+ *     tags:
+ *       - geolocation
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Geolocation data
+ *         schema:
+ *           type: array
+ *           items:
+ *             $ref: '#/definitions/GeolocationResponse'
+ */
+routes.get('/services/geoip', resources.services.geoip.get)
+
+routes.options('/services/images', cors())
+
+/**
+ * @swagger
+ * /services/images:
+ *   get:
+ *     description: Returns a token to get images for the user
+ *     parameters:
+ *       - in: query
+ *         name: userId
+ *         schema:
+ *           type: string
+ *     tags:
+ *       - images
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: Cloudinary API key and metadata
+ *         schema:
+ *           type: object
+ *           properties:
+ *             signature:
+ *               type: string
+ *             timestamp:
+ *               type: string
+ *             api_key:
+ *               type: string
+ */
+routes.get('/services/images', cors(), resources.services.images.get)
+
+module.exports = routes
