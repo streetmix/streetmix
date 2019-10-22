47b4ed302c62a35d610e94600e716a3edd231b98
Lou Huang <lou@louhuang.com>
2019-10-22 13:46:58 +0100

    refactor(StreetName): port to function component and update test

--- assets/scripts/streets/StreetName.jsx
+++ assets/scripts/streets/StreetName.jsx
@@ -1,85 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import './StreetName.scss'
 
 const MAX_STREET_NAME_WIDTH = 50
 
-export default class StreetName extends React.PureComponent {
-  static propTypes = {
-    id: PropTypes.string,
-    name: PropTypes.string,
-    childRef: PropTypes.func,
-    onClick: PropTypes.func,
-    editable: PropTypes.bool
+/**
+ * Some processing needed to display street name
+ *
+ * @public for main street name ¯\_(ツ)_/¯
+ * @params {string} name - Street name to check
+ */
+function normalizeStreetName (name) {
+  if (!name) return ''
+
+  name = name.trim()
+
+  if (name.length > MAX_STREET_NAME_WIDTH) {
+    name = name.substr(0, MAX_STREET_NAME_WIDTH) + '…'
   }
 
-  static defaultProps = {
-    name: '',
-    editable: false
-  }
-
-  /**
-   * Some processing needed to display street name
-   *
-   * @public for main street name ¯\_(ツ)_/¯
-   * @params {string} name - Street name to check
-   */
-  static normalizeStreetName (name) {
-    if (!name) return ''
-
-    name = name.trim()
-
-    if (name.length > MAX_STREET_NAME_WIDTH) {
-      name = name.substr(0, MAX_STREET_NAME_WIDTH) + '…'
-    }
-
-    return name
-  }
-
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      isHovered: false
-    }
-  }
-
-  handleMouseEnter = () => {
-    this.setState({ isHovered: true })
-  }
-
-  handleMouseLeave = () => {
-    this.setState({ isHovered: false })
-  }
-
-  renderHoverPrompt = () => {
-    if (this.props.editable && this.state.isHovered) {
-      return (
-        <div className="street-name-hover-prompt">
-          <FormattedMessage id="street.rename" defaultMessage="Click to rename" />
-        </div>
-      )
-    }
-
-    return null
-  }
-
-  render () {
-    const displayName = StreetName.normalizeStreetName(this.props.name) || <FormattedMessage id="street.default-name" defaultMessage="Unnamed St" />
-
-    return (
-      <div
-        className="street-name"
-        ref={this.props.childRef}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onClick={this.props.onClick}
-        id={this.props.id}
-      >
-        {this.renderHoverPrompt()}
-        <div className="street-name-text">{displayName}</div>
-      </div>
-    )
-  }
+  return name
 }
+
+StreetName.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  childRef: PropTypes.func,
+  onClick: PropTypes.func,
+  editable: PropTypes.bool
+}
+
+function StreetName (props) {
+  const {
+    id,
+    name = '',
+    childRef,
+    onClick = () => {},
+    editable = false
+  } = props
+  const [isHovered, setHovered] = useState(false)
+
+  function handleMouseEnter () {
+    setHovered(true)
+  }
+
+  function handleMouseLeave () {
+    setHovered(false)
+  }
+
+  return (
+    <div
+      className="street-name"
+      ref={childRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={onClick}
+      id={id}
+    >
+      {editable && isHovered && (
+        <div className="street-name-hover-prompt">
+          <FormattedMessage
+            id="street.rename"
+            defaultMessage="Click to rename"
+          />
+        </div>
+      )}
+      <div className="street-name-text">
+        {normalizeStreetName(name) || (
+          <FormattedMessage
+            id="street.default-name"
+            defaultMessage="Unnamed St"
+          />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(StreetName)
--- assets/scripts/streets/__tests__/StreetName.test.js
+++ assets/scripts/streets/__tests__/StreetName.test.js
@@ -1,33 +1,45 @@
 /* eslint-env jest */
 import React from 'react'
-import { shallow } from 'enzyme'
-import { mountWithIntl } from '../../../../test/helpers/intl-enzyme-test-helper.js'
+import { fireEvent } from '@testing-library/react'
+import { renderWithIntl } from '../../../../test/helpers/render'
 import StreetName from '../StreetName'
 
 describe('StreetName', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<StreetName />)
-    expect(wrapper.exists()).toEqual(true)
-  })
-
   it('renders a name', () => {
-    const wrapper = shallow(<StreetName name="foo" />)
-    expect(wrapper.text()).toEqual('foo')
+    const wrapper = renderWithIntl(<StreetName name="foo" />)
+    expect(wrapper.getByText('foo')).toBeInTheDocument()
   })
 
   it('truncates very long names', () => {
-    const wrapper = shallow(<StreetName name="foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar" />)
-    // We don't care what the actual length of string is, just that it's been truncated
-    expect(wrapper.text().endsWith('…')).toEqual(true)
+    const wrapper = renderWithIntl(
+      <StreetName name="foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar" />
+    )
+    // We don't care what the actual length of string is, just that
+    // it's been truncated and ends with ellipses
+    expect(wrapper.getByText(/…$/)).toBeInTheDocument()
   })
 
   it('renders a placeholder if there is no name', () => {
-    // Requires the react-intl test helper to retrieve the correct content of <FormattedMessage />
-    const wrapper = mountWithIntl(<StreetName />)
-    expect(wrapper.text()).toEqual('Unnamed St')
+    const wrapper = renderWithIntl(<StreetName />)
+    expect(wrapper.getByText('Unnamed St')).toBeInTheDocument()
   })
 
-  it.todo('shows a "Click to edit" message when mouse is hovering over it')
-  it.todo('does not do that if the street name is not editable')
-  it.todo('responds to an onClick handler')
+  it('responds to an onClick handler', () => {
+    const handleClick = jest.fn()
+    const wrapper = renderWithIntl(<StreetName onClick={handleClick} />)
+    fireEvent.click(wrapper.getByText('Unnamed St'))
+    expect(handleClick).toBeCalledTimes(1)
+  })
+
+  it('shows a "Click to edit" message when mouse is hovering over it', () => {
+    const wrapper = renderWithIntl(<StreetName editable />)
+    fireEvent.mouseOver(wrapper.getByText('Unnamed St'))
+    expect(wrapper.getByText('Click to rename')).toBeInTheDocument()
+  })
+
+  it('does not do that if the street name is not editable', () => {
+    const wrapper = renderWithIntl(<StreetName editable={false} />)
+    fireEvent.mouseOver(wrapper.getByText('Unnamed St'))
+    expect(wrapper.queryByText('Click to rename')).not.toBeInTheDocument()
+  })
 })
