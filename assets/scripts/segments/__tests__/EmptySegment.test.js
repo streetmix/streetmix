6252e94a0e7a5db5d20e2d176f789d1b96550110
dwikiarlan <arrlancore@gmail.com>
2019-10-27 10:21:15 +0700

    fix: refactor(EmptySegmentContainer)

--- assets/scripts/segments/__tests__/EmptySegment.test.js
+++ assets/scripts/segments/__tests__/EmptySegment.test.js
@@ -7,25 +7,23 @@ import { renderWithReduxAndIntl } from '../../../../test/helpers/render'
 
 describe('EmptySegment', () => {
   it('renders nothing when the width is 0', () => {
-    const wrapper = renderWithReduxAndIntl(<EmptySegment width={0} />)
-    expect(wrapper.container.firstChild).toBeNull()
+    const { container } = renderWithReduxAndIntl(<EmptySegment width={0} />)
+    expect(container.firstChild).toBeNull()
   })
 
   it('renders a width, and at left position 0 by default', () => {
-    const wrapper = renderWithReduxAndIntl(
+    const { container } = renderWithReduxAndIntl(
       <EmptySegment width={12.5} units={SETTINGS_UNITS_METRIC} locale="en" />
     )
     expect(
-      wrapper.container.firstChild.classList.contains('segment-empty')
+      container.firstChild.classList.contains('segment-empty')
     ).toBeTruthy()
-    expect(wrapper.container.firstChild.style.width).toEqual(
-      `${12.5 * TILE_SIZE}px`
-    )
-    expect(wrapper.container.firstChild.style.left).toEqual('0px')
+    expect(container.firstChild.style.width).toEqual(`${12.5 * TILE_SIZE}px`)
+    expect(container.firstChild.style.left).toEqual('0px')
   })
 
   it('renders at width and left position given', () => {
-    const wrapper = renderWithReduxAndIntl(
+    const { container } = renderWithReduxAndIntl(
       <EmptySegment
         width={15}
         left={33}
@@ -34,21 +32,17 @@ describe('EmptySegment', () => {
       />
     )
     expect(
-      wrapper.container.firstChild.classList.contains('segment-empty')
+      container.firstChild.classList.contains('segment-empty')
     ).toBeTruthy()
-    expect(wrapper.container.firstChild.style.width).toEqual(
-      `${15 * TILE_SIZE}px`
-    )
-    expect(wrapper.container.firstChild.style.left).toEqual(
-      `${33 * TILE_SIZE}px`
-    )
+    expect(container.firstChild.style.width).toEqual(`${15 * TILE_SIZE}px`)
+    expect(container.firstChild.style.left).toEqual(`${33 * TILE_SIZE}px`)
   })
 
   // TODO: unskip these tests when enzyme and react-test-render support memoized components
   it.skip('renders text content', () => {
-    const wrapper = renderWithReduxAndIntl(
+    const { container } = renderWithReduxAndIntl(
       <EmptySegment width={15} units={SETTINGS_UNITS_METRIC} locale="ja" />
     )
-    expect(wrapper.container.firstChild.textContent).toBe('Empty space4.5 m')
+    expect(container.firstChild.textContent).toBe('Empty space4.5 m')
   })
 })
