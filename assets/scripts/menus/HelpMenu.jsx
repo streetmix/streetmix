b6b6d80f6fc65b702932fe35bb625b4585ff9e0d
Lou Huang <lou@louhuang.com>
2019-11-01 09:09:48 -0400

    refactor(HelpMenu): port to react-testing-library

--- assets/scripts/menus/HelpMenu.jsx
+++ assets/scripts/menus/HelpMenu.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
 import Menu from './Menu'
 import KeyboardKey from '../ui/KeyboardKey'
@@ -15,120 +14,144 @@ import { trackEvent } from '../app/event_tracking'
 import { showDialog } from '../store/actions/dialogs'
 import './HelpMenu.scss'
 
-export class HelpMenu extends Component {
-  static propTypes = {
-    showDialog: PropTypes.func
-  }
+function handleShow () {
+  trackEvent('Interaction', 'Open help menu', null, null, false)
+}
 
-  componentDidMount () {
+const shiftKey = (
+  <KeyboardKey>
+    <FormattedMessage id="key.shift" defaultMessage="Shift" />
+  </KeyboardKey>
+)
+
+function HelpMenu (props) {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
     // Set up keyboard shortcuts
-    registerKeypress('?', { shiftKey: 'optional' }, () => this.props.showDialog('ABOUT'))
-  }
-
-  componentWillUnmount () {
-    deregisterKeypress('?', () => this.props.showDialog('ABOUT'))
-  }
-
-  handleShow () {
-    trackEvent('Interaction', 'Open help menu', null, null, false)
-  }
-
-  render () {
-    const shiftKey = (
-      <KeyboardKey>
-        <FormattedMessage id="key.shift" defaultMessage="Shift" />
-      </KeyboardKey>
+    registerKeypress('?', { shiftKey: 'optional' }, () =>
+      dispatch(showDialog('ABOUT'))
     )
 
-    return (
-      <Menu onShow={this.handleShow} {...this.props}>
-        <a href="#" onClick={() => this.props.showDialog('ABOUT')}>
-          <FormattedMessage id="menu.item.about" defaultMessage="About Streetmix…" />
-        </a>
-        <a href="#" onClick={() => this.props.showDialog('WHATS_NEW')}>
-          <FormattedMessage id="dialogs.whatsnew.heading" defaultMessage="What’s new in Streetmix? [en]&lrm;" />
-        </a>
-        <div className="help-menu-shortcuts non-touch-only">
-          <p>
-            <FormattedMessage id="menu.help.keyboard-label" defaultMessage="Keyboard shortcuts:" />
-          </p>
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <KeyboardKey>
-                    <FormattedMessage id="key.backspace" defaultMessage="Backspace" />
-                  </KeyboardKey>
-                </td>
-                <td>
+    return () => {
+      deregisterKeypress('?', () => dispatch(showDialog('ABOUT')))
+    }
+  })
+
+  return (
+    <Menu onShow={handleShow} {...props}>
+      <a href="#" onClick={() => dispatch(showDialog('ABOUT'))}>
+        <FormattedMessage
+          id="menu.item.about"
+          defaultMessage="About Streetmix…"
+        />
+      </a>
+      <a href="#" onClick={() => dispatch(showDialog('WHATS_NEW'))}>
+        <FormattedMessage
+          id="dialogs.whatsnew.heading"
+          defaultMessage="What’s new in Streetmix? [en]&lrm;"
+        />
+      </a>
+      <div className="help-menu-shortcuts non-touch-only">
+        <p>
+          <FormattedMessage
+            id="menu.help.keyboard-label"
+            defaultMessage="Keyboard shortcuts:"
+          />
+        </p>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <KeyboardKey>
                   <FormattedMessage
-                    id="menu.help.remove-instruction"
-                    defaultMessage="Remove a segment you’re pointing at"
+                    id="key.backspace"
+                    defaultMessage="Backspace"
                   />
-                  <br />
-                  <FormattedMessage
-                    id="menu.help.remove-shift-instruction"
-                    defaultMessage="(hold {shiftKey} to remove all)&lrm;"
-                    values={{ shiftKey }}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td dir="ltr">
-                  {/* <FormattedMessage> used with render prop because we pass the translated
-                      text to <KeyboardKey> as a string, not as a component */}
-                  <FormattedMessage id="key.minus" defaultMessage="Minus">
-                    {(label) => <KeyboardKey icon={ICON_MINUS}>{label}</KeyboardKey>}
-                  </FormattedMessage>
-                  <FormattedMessage id="key.plus" defaultMessage="Plus">
-                    {(label) => <KeyboardKey icon={ICON_PLUS}>{label}</KeyboardKey>}
-                  </FormattedMessage>
-                </td>
-                <td>
-                  <FormattedMessage
-                    id="menu.help.change-instruction"
-                    defaultMessage="Change width of a segment you’re pointing at"
-                  />
-                  <br />
-                  <FormattedMessage
-                    id="menu.help.change-shift-instruction"
-                    defaultMessage="(hold {shiftKey} for more precision)&lrm;"
-                    values={{ shiftKey }}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td dir="ltr">
-                  <FormattedMessage id="key.left-arrow" defaultMessage="Left arrow">
-                    {(label) => <KeyboardKey icon={ICON_ARROW_LEFT}>{label}</KeyboardKey>}
-                  </FormattedMessage>
-                  <FormattedMessage id="key.right-arrow" defaultMessage="Right arrow">
-                    {(label) => <KeyboardKey icon={ICON_ARROW_RIGHT}>{label}</KeyboardKey>}
-                  </FormattedMessage>
-                </td>
-                <td>
-                  <FormattedMessage
-                    id="menu.help.move-instruction"
-                    defaultMessage="Move around the street"
-                  />
-                  <br />
-                  <FormattedMessage
-                    id="menu.help.move-shift-instruction"
-                    defaultMessage="(hold {shiftKey} to jump to edges)&lrm;"
-                    values={{ shiftKey }}
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </Menu>
-    )
-  }
+                </KeyboardKey>
+              </td>
+              <td>
+                <FormattedMessage
+                  id="menu.help.remove-instruction"
+                  defaultMessage="Remove a segment you’re pointing at"
+                />
+                <br />
+                <FormattedMessage
+                  id="menu.help.remove-shift-instruction"
+                  defaultMessage="(hold {shiftKey} to remove all)&lrm;"
+                  values={{ shiftKey }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td dir="ltr">
+                {/*
+                  <FormattedMessage> is used with a render prop because we need
+                  to pass a string child to <KeyboardKey /> when the `icon`
+                  prop is used. <KeyboardKey /> will not render correctly if
+                  the child is a React component.
+                */}
+                <FormattedMessage id="key.minus" defaultMessage="Minus">
+                  {(label) => (
+                    <KeyboardKey icon={ICON_MINUS}>{label}</KeyboardKey>
+                  )}
+                </FormattedMessage>
+                <FormattedMessage id="key.plus" defaultMessage="Plus">
+                  {(label) => (
+                    <KeyboardKey icon={ICON_PLUS}>{label}</KeyboardKey>
+                  )}
+                </FormattedMessage>
+              </td>
+              <td>
+                <FormattedMessage
+                  id="menu.help.change-instruction"
+                  defaultMessage="Change width of a segment you’re pointing at"
+                />
+                <br />
+                <FormattedMessage
+                  id="menu.help.change-shift-instruction"
+                  defaultMessage="(hold {shiftKey} for more precision)&lrm;"
+                  values={{ shiftKey }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td dir="ltr">
+                <FormattedMessage
+                  id="key.left-arrow"
+                  defaultMessage="Left arrow"
+                >
+                  {(label) => (
+                    <KeyboardKey icon={ICON_ARROW_LEFT}>{label}</KeyboardKey>
+                  )}
+                </FormattedMessage>
+                <FormattedMessage
+                  id="key.right-arrow"
+                  defaultMessage="Right arrow"
+                >
+                  {(label) => (
+                    <KeyboardKey icon={ICON_ARROW_RIGHT}>{label}</KeyboardKey>
+                  )}
+                </FormattedMessage>
+              </td>
+              <td>
+                <FormattedMessage
+                  id="menu.help.move-instruction"
+                  defaultMessage="Move around the street"
+                />
+                <br />
+                <FormattedMessage
+                  id="menu.help.move-shift-instruction"
+                  defaultMessage="(hold {shiftKey} to jump to edges)&lrm;"
+                  values={{ shiftKey }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Menu>
+  )
 }
 
-const mapDispatchToProps = {
-  showDialog
-}
-
-export default connect(null, mapDispatchToProps)(HelpMenu)
+export default HelpMenu
