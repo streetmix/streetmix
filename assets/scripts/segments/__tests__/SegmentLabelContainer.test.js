8cdbda50bd31c0264e5585a838bc376e482f0073
dwikiarlan <arrlancore@gmail.com>
2019-11-10 14:59:03 +0700

    refactor: segmentLabelContainer

--- assets/scripts/segments/__tests__/SegmentLabelContainer.test.js
+++ assets/scripts/segments/__tests__/SegmentLabelContainer.test.js
@@ -1,8 +1,11 @@
 /* eslint-env jest */
 import React from 'react'
-import { shallow } from 'enzyme'
 import SegmentLabelContainer from '../SegmentLabelContainer'
-import { SETTINGS_UNITS_IMPERIAL, SETTINGS_UNITS_METRIC } from '../../users/constants'
+import {
+  SETTINGS_UNITS_IMPERIAL,
+  SETTINGS_UNITS_METRIC
+} from '../../users/constants'
+import { renderWithReduxAndIntl } from '../../../../test/helpers/render'
 
 const testProps = {
   label: 'foo',
@@ -13,28 +16,42 @@ const testProps = {
 
 describe('SegmentLabelContainer', () => {
   it('renders string label', () => {
-    const wrapper = shallow(<SegmentLabelContainer {...testProps} label="bar" />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = renderWithReduxAndIntl(
+      <SegmentLabelContainer {...testProps} label="bar" />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders React element label', () => {
     const TestComponent = <span>bar</span>
-    const wrapper = shallow(<SegmentLabelContainer {...testProps} label={TestComponent} />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = renderWithReduxAndIntl(
+      <SegmentLabelContainer {...testProps} label={TestComponent} />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders correct grid styling in metric', () => {
-    const wrapper = shallow(<SegmentLabelContainer {...testProps} />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = renderWithReduxAndIntl(
+      <SegmentLabelContainer {...testProps} />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders correct grid styling in imperial', () => {
-    const wrapper = shallow(<SegmentLabelContainer {...testProps} units={SETTINGS_UNITS_IMPERIAL} />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = renderWithReduxAndIntl(
+      <SegmentLabelContainer {...testProps} units={SETTINGS_UNITS_IMPERIAL} />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders editable label', () => {
-    const wrapper = shallow(<SegmentLabelContainer {...testProps} editable editSegmentLabel={() => {}} />)
-    expect(wrapper).toMatchSnapshot()
+    const { asFragment } = renderWithReduxAndIntl(
+      <SegmentLabelContainer
+        {...testProps}
+        editable
+        editSegmentLabel={() => {}}
+      />
+    )
+    expect(asFragment()).toMatchSnapshot()
   })
 })
