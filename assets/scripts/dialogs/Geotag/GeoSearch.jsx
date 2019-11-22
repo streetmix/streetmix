2d438f4f3b3007205e3fd5402406eb0ea69a0d7b
Lou Huang <lou@louhuang.com>
2019-11-22 00:43:22 -0500

    test(GeoSearch): port to react-testing-library

--- assets/scripts/dialogs/Geotag/GeoSearch.jsx
+++ assets/scripts/dialogs/Geotag/GeoSearch.jsx
@@ -1,55 +1,53 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+import { useIntl } from 'react-intl'
 import DownshiftPelias from 'downshift-pelias'
 import Pelias from 'pelias-js'
-import { injectIntl } from 'react-intl'
 import { PELIAS_HOST_NAME, PELIAS_API_KEY } from '../../app/config'
 import { setMapState } from '../../store/actions/map'
 
-class GeoSearch extends React.Component {
-  static propTypes = {
-    intl: PropTypes.object.isRequired,
-    setMapState: PropTypes.func,
-    setSearchResults: PropTypes.func,
-    focus: PropTypes.shape({
-      lat: PropTypes.number,
-      lng: PropTypes.number
-    }).isRequired
-  }
+GeoSearch.propTypes = {
+  setMapState: PropTypes.func,
+  setSearchResults: PropTypes.func,
+  focus: PropTypes.shape({
+    lat: PropTypes.number,
+    lng: PropTypes.number
+  })
+}
 
-  static defaultProps = {
-    focus: { lat: 0, lng: 0 }
-  }
+function GeoSearch ({ setSearchResults, focus = { lat: 0, lng: 0 } }) {
+  const intl = useIntl()
+  const dispatch = useDispatch()
+  const inputEl = useRef()
 
-  constructor (props) {
-    super(props)
-
-    this.pelias = new Pelias({ peliasUrl: `https://${PELIAS_HOST_NAME}`, apiKey: PELIAS_API_KEY })
-    this.inputEl = React.createRef()
-  }
-
-  onClickClearSearch = (clearSelection) => {
+  function handleClickClearSearch (clearSelection) {
     clearSelection()
-    this.inputEl.current.focus()
+    inputEl.current.focus()
   }
 
-  handleChange = (selection) => {
+  function handleChange (selection) {
     if (!selection) return
 
-    this.props.setMapState({
-      addressInformation: selection.properties,
-      markerLocation: {
-        lat: selection.geometry.coordinates[1],
-        lng: selection.geometry.coordinates[0]
-      }
-    })
+    dispatch(
+      setMapState({
+        addressInformation: selection.properties,
+        markerLocation: {
+          lat: selection.geometry.coordinates[1],
+          lng: selection.geometry.coordinates[0]
+        }
+      })
+    )
 
-    this.props.setSearchResults(selection.geometry.coordinates.reverse(), selection.properties.label, selection.bbox)
-    this.inputEl.current.focus()
+    setSearchResults(
+      selection.geometry.coordinates.reverse(),
+      selection.properties.label,
+      selection.bbox
+    )
+    inputEl.current.focus()
   }
 
-  renderSuggestion = (item, index, inputValue, getItemProps) => {
+  function renderSuggestion (item, index, inputValue, getItemProps) {
     const label = item.properties.label
 
     // Highlight the input query
@@ -63,79 +61,81 @@ class GeoSearch extends React.Component {
     })
 
     return (
-      <li {...getItemProps({
-        className: 'geotag-suggestion',
-        key: item.properties.gid,
-        index,
-        item
-      })}
+      <li
+        {...getItemProps({
+          className: 'geotag-suggestion',
+          key: item.properties.gid,
+          index,
+          item
+        })}
       >
         {highlighted}
       </li>
     )
   }
 
-  render () {
-    const { focus } = this.props
+  const pelias = new Pelias({
+    peliasUrl: `https://${PELIAS_HOST_NAME}`,
+    apiKey: PELIAS_API_KEY
+  })
 
-    this.pelias.search.setBoundaryCircle({ lat: focus.lat, lon: focus.lng, radius: 10 })
-    this.pelias.autocomplete.setFocusPoint({ lat: focus.lat, lon: focus.lng })
+  pelias.search.setBoundaryCircle({
+    lat: focus.lat,
+    lon: focus.lng,
+    radius: 10
+  })
+  pelias.autocomplete.setFocusPoint({ lat: focus.lat, lon: focus.lng })
 
-    return (
-      <DownshiftPelias pelias={this.pelias} onChange={this.handleChange}>
-        {({
-          getInputProps,
-          getMenuProps,
-          getItemProps,
-          clearSelection,
-          inputValue,
-          isOpen,
-          results
-        }) => (
-          <div className="geotag-input-form">
-            <input {...getInputProps({
+  return (
+    <DownshiftPelias pelias={pelias} onChange={handleChange}>
+      {({
+        getInputProps,
+        getMenuProps,
+        getItemProps,
+        clearSelection,
+        inputValue,
+        isOpen,
+        results
+      }) => (
+        <div className="geotag-input-form">
+          <input
+            {...getInputProps({
               className: 'geotag-input',
               autoFocus: true,
-              ref: this.inputEl,
-              placeholder: this.props.intl.formatMessage({ id: 'dialogs.geotag.search', defaultMessage: 'Search for a location' })
+              ref: inputEl,
+              placeholder: intl.formatMessage({
+                id: 'dialogs.geotag.search',
+                defaultMessage: 'Search for a location'
+              })
             })}
-            />
-            {inputValue && (
-              <span
-                title={this.props.intl.formatMessage({ id: 'dialogs.geotag.clear-search', defaultMessage: 'Clear search' })}
-                className="geotag-input-clear"
-                onClick={() => { this.onClickClearSearch(clearSelection) }}
-              >
-                ×
-              </span>
-            )}
-            {isOpen && results && results.features.length > 0 && (
-              <div className="geotag-suggestions-container">
-                <ul {...getMenuProps({ className: 'geotag-suggestions-list' })}>
-                  {results.features.map((item, index) => this.renderSuggestion(item, index, inputValue, getItemProps))}
-                </ul>
-              </div>
-            )}
-          </div>
-        )}
-      </DownshiftPelias>
-    )
-  }
+          />
+          {inputValue && (
+            <span
+              title={intl.formatMessage({
+                id: 'dialogs.geotag.clear-search',
+                defaultMessage: 'Clear search'
+              })}
+              className="geotag-input-clear"
+              onClick={() => {
+                handleClickClearSearch(clearSelection)
+              }}
+            >
+              ×
+            </span>
+          )}
+          {isOpen && results && results.features.length > 0 && (
+            <div className="geotag-suggestions-container">
+              <ul {...getMenuProps({ className: 'geotag-suggestions-list' })}>
+                {results.features.map((item, index) =>
+                  renderSuggestion(item, index, inputValue, getItemProps)
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+    </DownshiftPelias>
+  )
 }
 
-// Inject Intl via a higher-order component provided by react-intl.
-// Exported so that this component can be tested.
-export const GeoSearchWithIntl = injectIntl(GeoSearch)
-
-function mapStateToProps (state) {
-  return {
-    markerLocation: state.map.markerLocation,
-    addressInformation: state.map.addressInformation
-  }
-}
-
-const mapDispatchToProps = {
-  setMapState
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(GeoSearchWithIntl)
+export default GeoSearch
--- assets/scripts/dialogs/Geotag/__tests__/GeoSearch.test.js
+++ assets/scripts/dialogs/Geotag/__tests__/GeoSearch.test.js
@@ -1,13 +1,13 @@
 /* eslint-env jest */
 import React from 'react'
-import { mountWithIntl as mount } from '../../../../../test/helpers/intl-enzyme-test-helper.js'
-import { GeoSearchWithIntl as GeoSearch } from '../GeoSearch'
-
+import { fireEvent } from '@testing-library/react'
+import { renderWithReduxAndIntl } from '../../../../../test/helpers/render'
+import GeoSearch from '../GeoSearch'
 import autocompleteResponse from './fixtures/autocomplete.json'
 import searchResponse from './fixtures/search.json'
 
 // Mocks an Mapzen Search request
-const mapzenSearchMock = jest.fn(url => {
+const mapzenSearchMock = jest.fn((url) => {
   let response
   if (url.indexOf('/autocomplete?') > 0) {
     response = autocompleteResponse
@@ -16,16 +16,20 @@ const mapzenSearchMock = jest.fn(url => {
   }
 
   if (response) {
-    return Promise.resolve(new window.Response(JSON.stringify(response), {
-      status: 200,
-      headers: {
-        'Content-type': 'application/json'
-      }
-    }))
+    return Promise.resolve(
+      new window.Response(JSON.stringify(response), {
+        status: 200,
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+    )
   } else {
-    return Promise.resolve(new window.Response(undefined, {
-      status: 500
-    }))
+    return Promise.resolve(
+      new window.Response(undefined, {
+        status: 500
+      })
+    )
   }
 })
 
@@ -33,58 +37,52 @@ describe('GeoSearch', () => {
   window.fetch = mapzenSearchMock
 
   it('focuses the input after mounting', () => {
-    const component = mount(<GeoSearch />)
-
-    // Not referential equality
-    expect(component.find('input').instance().className).toEqual('geotag-input')
+    const { getByPlaceholderText } = renderWithReduxAndIntl(<GeoSearch />)
+    const input = getByPlaceholderText('Search for a location')
+    expect(document.activeElement).toEqual(input)
   })
 
   it('displays a "clear search" button when there is input', () => {
-    const component = mount(<GeoSearch />)
-    const input = component.find('input')
+    const { queryByTitle, getByPlaceholderText } = renderWithReduxAndIntl(
+      <GeoSearch />
+    )
+    const input = getByPlaceholderText('Search for a location')
 
     // The close button should not render when the input is empty
-    const close1 = component.find('.geotag-input-clear')
-    expect(close1.exists()).toEqual(false)
+    expect(queryByTitle('Clear search')).not.toBeInTheDocument()
 
     // Simulates input, which should also trigger events
-    input.simulate('change', { target: { value: 'f' } })
+    fireEvent.change(input, { target: { value: 'f' } })
 
     // The close button should appear after one keystroke
-    const close2 = component.find('.geotag-input-clear')
-    expect(close2.exists()).toEqual(true)
+    expect(queryByTitle('Clear search')).toBeInTheDocument()
 
     // Deletes input
-    input.simulate('change', { target: { value: '' } })
+    fireEvent.change(input, { target: { value: '' } })
 
     // The close button should now disappear
-    const close3 = component.find('.geotag-input-clear')
-    expect(close3.exists()).toEqual(false)
-  })
-
-  it('puts a title attribute on the "clear search" button', () => {
-    const component = mount(<GeoSearch />)
-    component.find('input').simulate('change', { target: { value: 'foo' } })
-
-    const el = component.find('.geotag-input-clear')
-    expect(el.instance().getAttribute('title')).toEqual('Clear search')
+    expect(queryByTitle('Clear search')).not.toBeInTheDocument()
   })
 
   it('clears and focuses input when "clear search" button is clicked', () => {
-    const component = mount(<GeoSearch />)
-    const input = component.find('input')
+    const {
+      getByTitle,
+      queryByTitle,
+      getByPlaceholderText
+    } = renderWithReduxAndIntl(<GeoSearch />)
 
     // Simulates input
-    input.simulate('change', { target: { value: 'foo' } })
+    const input = getByPlaceholderText('Search for a location')
+    fireEvent.change(input, { target: { value: 'foo' } })
 
     // Simulates click on "clear search"
-    component.find('.geotag-input-clear').simulate('click')
+    fireEvent.click(getByTitle('Clear search'))
 
     // The close button should be undefined now
-    expect(component.find('.geotag-input-clear').exists()).toEqual(false)
+    expect(queryByTitle('Clear search')).not.toBeInTheDocument()
 
     // The input should be empty and it should be focused
-    expect(input.instance().value).toEqual('')
-    expect(input.instance()).toEqual(document.activeElement)
+    expect(input.value).toEqual('')
+    expect(input).toEqual(document.activeElement)
   })
 })
