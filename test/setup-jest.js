4fe44fa23803fef6dd3c695b06566747edb4f3ad
Lou Huang <lou@louhuang.com>
2019-11-22 12:41:40 -0500

    chore(tests): remove all enzyme!! #1612

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -1,7 +1,5 @@
 /* eslint-env jest */
 import '@testing-library/jest-dom/extend-expect'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 import fetch from 'jest-fetch-mock'
 import 'jest-canvas-mock'
 import 'jest-date-mock'
@@ -12,5 +10,3 @@ global.fetch = fetch
 global.localStorage = new LocalStorageMock()
 global.Modernizr = Modernizr
 global.print = jest.fn()
-
-Enzyme.configure({ adapter: new Adapter() })
