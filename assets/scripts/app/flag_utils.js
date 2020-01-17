73f2f7798c187cd8509ddcb477795ae576cccb4c
Lou Huang <lou@louhuang.com>
2020-01-17 11:44:24 -0500

    feat: experimental body font is Rubik

--- assets/scripts/app/flag_utils.js
+++ assets/scripts/app/flag_utils.js
@@ -21,7 +21,9 @@ function whatAreTheFlagsWeNeedToSave (flags) {
   // filter out all non-user set flags
   const filter1 = array.filter((item) => item[1].source === 'session')
   // filter out flags that equal default values
-  const filter2 = filter1.filter((item) => item[1].value !== FEATURE_FLAGS[item[0]].defaultValue)
+  const filter2 = filter1.filter(
+    (item) => item[1].value !== FEATURE_FLAGS[item[0]].defaultValue
+  )
   // convert back to obj but simplify it to just the value
   return filter2.reduce((obj, item) => {
     obj[item[0]] = item[1].value
@@ -53,7 +55,8 @@ function initExperimentalBodyFontListener () {
   const onChange = (value) => {
     if (value === true) {
       const el = document.createElement('link')
-      el.href = 'https://fonts.googleapis.com/css?family=Barlow:400,600,400italic'
+      el.href =
+        'https://fonts.googleapis.com/css?family=Rubik:400,600,400italic'
       el.setAttribute('rel', 'stylesheet')
       document.head.appendChild(el)
       document.body.classList.add('experimental-font-family')
