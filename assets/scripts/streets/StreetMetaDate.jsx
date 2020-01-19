1d402d6ee22a18215d9a71987738a7a2429606d1
Lou Huang <lou@louhuang.com>
2020-01-19 09:35:30 -0500

    feat(StreetMetaData): hide recent timestamps when editing, resolves #997

--- assets/scripts/streets/StreetMetaDate.jsx
+++ assets/scripts/streets/StreetMetaDate.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import DateTimeRelative from '../app/DateTimeRelative'
+import { isOwnedByCurrentUser } from './owner'
+
+// Milliseconds that must have elapsed before recent edits display timestamp
+const TIME_LIMIT = 6000
 
 function StreetMetaDate (props) {
-  const updatedAt = useSelector((state) => state.street.updatedAt)
+  const updatedAt = useSelector(
+    (state) => state.street.updatedAt || state.street.clientUpdatedAt
+  )
 
   if (!updatedAt) return null
+
+  // In the user has just edited the street, don't display the "seconds ago" timestamp.
+  if (new Date() - new Date(updatedAt) < TIME_LIMIT && isOwnedByCurrentUser()) {
+    return null
+  }
 
   return (
     <span className="street-metadata-date">
---
+++ assets/scripts/streets/owner.js
@@ -0,0 +1,41 @@
+import { getRemixOnFirstEdit } from './remix'
+import store from '../store'
+
+/**
+ * A helper function that tells us whether the current street is
+ * "owned" by the current user. There are two kinds of ownership:
+ *
+ * (1) the street has a known creator ID, and the user is
+ *     currently signed in with the same creator ID.
+ * (2) the street has no creator ID (it is anonymous), and the
+ *     user is not currently signed in, but is editing the street
+ *     within the same session.
+ *
+ * The second one is the hard one because anonymous streets are
+ * regarded as orphaned the moment user leaves the session. We
+ * currently have no way of tracking anonymous streets across
+ * multiple sessions (or even a single tab reload).
+ *
+ * TODO: Remove dependency on `getRemixOnFirstEdit()`, which is
+ * a variable that's set when a page is loaded to track whether the
+ * current anonymous street belongs to the current session.
+ */
+export function isOwnedByCurrentUser () {
+  const state = store.getState()
+
+  if (!state) return false
+
+  const signedIn = (state.user && state.user.signedIn) || false
+  const creatorId = (state.street && state.street.creatorId) || null
+  const userId =
+    (state.user && state.user.signInData && state.user.signInData.userId) ||
+    null
+
+  if (signedIn && creatorId === userId) {
+    return true
+  } else if (!creatorId && !signedIn && !getRemixOnFirstEdit()) {
+    return true
+  }
+
+  return false
+}
