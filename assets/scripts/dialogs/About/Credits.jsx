6c50a842cb26d2344a8267dda2f997e7b2c74d99
Lou Huang <lou@louhuang.com>
2020-01-19 14:10:41 -0500

    feat(locale): add CC links and credits headers

--- assets/scripts/dialogs/About/Credits.jsx
+++ assets/scripts/dialogs/About/Credits.jsx
@@ -39,13 +39,17 @@ const UNTRANSLATED_LABELS = {
   'additional-contributors': 'Additional contributors',
   'special-thanks': 'Special thanks to',
   ar: 'Arabic',
+  ca: 'Catalan',
   de: 'German',
   en: 'English',
   es: 'Spanish',
   'es-mx': 'Spanish (Mexico)',
+  'es-419': 'Spanish (Latin America)',
   fi: 'Finnish',
   fr: 'French',
+  it: 'Italian',
   ja: 'Japanese',
+  ko: 'Korean',
   pl: 'Polish',
   'pt-br': 'Portuguese (Brazil)',
   ru: 'Russian',
@@ -57,73 +61,101 @@ function Credits (props) {
   return (
     <>
       <h2>
-        <FormattedMessage id="credits.core-team-heading" defaultMessage="Project team" />
+        <FormattedMessage
+          id="credits.core-team-heading"
+          defaultMessage="Project team"
+        />
       </h2>
 
       <div className="credits-team">
-        {
-          CREDITS.team
-            .filter(person => person.active)
-            .sort(alphabetizeNames)
-            .map(person => <TeamMember {...person} key={person.name} />)
-        }
+        {CREDITS.team
+          .filter((person) => person.active)
+          .sort(alphabetizeNames)
+          .map((person) => (
+            <TeamMember {...person} key={person.name} />
+          ))}
       </div>
 
       <h2>
-        <FormattedMessage id="credits.past-team-heading" defaultMessage="Past team members" />
+        <FormattedMessage
+          id="credits.past-team-heading"
+          defaultMessage="Past team members"
+        />
       </h2>
 
       <div className="credits-team credits-team-past">
-        {
-          CREDITS.team
-            .filter(person => !person.active)
-            .sort(alphabetizeNames)
-            .map(person => <TeamMember {...person} key={person.name} />)
-        }
+        {CREDITS.team
+          .filter((person) => !person.active)
+          .sort(alphabetizeNames)
+          .map((person) => (
+            <TeamMember {...person} key={person.name} />
+          ))}
       </div>
 
       <div className="credits-container">
         <div className="credits-credits-left">
-          {
-            Object.entries(CREDITS.contributors)
-              .map(([key, value]) => (
-                <React.Fragment key={key}>
-                  <h3><FormattedMessage id={`credits.${key}`} defaultMessage={UNTRANSLATED_LABELS[key]} /></h3>
-                  <ul>
-                    {value.sort().map(name => Array.isArray(name)
-                      ? <li key={name[0]}>{name[0]}, <i>{name[1]}</i></li>
-                      : (typeof name === 'string')
-                        ? <li key={name}>{name}</li>
-                        : (
-                          <li key={name.label}>
-                            <h4>{name.label}</h4>
-                            <ul>
-                              {name.people.map(person => <li key={person}>{person}</li>)}
-                            </ul>
-                          </li>)
-                    )}
-                  </ul>
-                </React.Fragment>
-              ))
-          }
+          {Object.entries(CREDITS.contributors).map(([key, value]) => (
+            <React.Fragment key={key}>
+              <h3>
+                <FormattedMessage
+                  id={`credits.${key}`}
+                  defaultMessage={UNTRANSLATED_LABELS[key]}
+                />
+              </h3>
+              <ul>
+                {value.sort().map((name) =>
+                  Array.isArray(name) ? (
+                    <li key={name[0]}>
+                      {name[0]}, <i>{name[1]}</i>
+                    </li>
+                  ) : typeof name === 'string' ? (
+                    <li key={name}>{name}</li>
+                  ) : (
+                    <li key={name.label}>
+                      <h4>{name.label}</h4>
+                      <ul>
+                        {name.people.map((person) => (
+                          <li key={person}>{person}</li>
+                        ))}
+                      </ul>
+                    </li>
+                  )
+                )}
+              </ul>
+            </React.Fragment>
+          ))}
         </div>
         <div className="credits-credits-right">
-          <h3><FormattedMessage id="credits.translators" defaultMessage="Translators" /></h3>
+          <h3>
+            <FormattedMessage
+              id="credits.translators"
+              defaultMessage="Translators"
+            />
+          </h3>
 
           <ul>
             {/* TODO: Alphabetize language names for each locale */}
-            {Object.entries(CREDITS.translators)
-              .map(([key, value]) => (
-                <li key={key}>
-                  <h4><FormattedMessage id={`i18n.lang.${key}`} defaultMessage={UNTRANSLATED_LABELS[key]} /></h4>
-                  <ul>
-                    {value.sort().map(name => Array.isArray(name)
-                      ? <li key={name[0]}>{name[0]}, <i>{name[1]}</i></li>
-                      : <li key={name}>{name}</li>
-                    )}
-                  </ul>
-                </li>
-              ))}
+            {Object.entries(CREDITS.translators).map(([key, value]) => (
+              <li key={key}>
+                <h4>
+                  <FormattedMessage
+                    id={`i18n.lang.${key}`}
+                    defaultMessage={UNTRANSLATED_LABELS[key]}
+                  />
+                </h4>
+                <ul>
+                  {value.sort().map((name) =>
+                    Array.isArray(name) ? (
+                      <li key={name[0]}>
+                        {name[0]}, <i>{name[1]}</i>
+                      </li>
+                    ) : (
+                      <li key={name}>{name}</li>
+                    )
+                  )}
+                </ul>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
