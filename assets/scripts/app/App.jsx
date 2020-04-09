4793c393cdce5a953ba9b122984113e52b24063d
Lou Huang <lou@louhuang.com>
2020-04-09 14:02:49 -0400

    refactor(App): port to function component

--- assets/scripts/app/App.jsx
+++ assets/scripts/app/App.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { IntlProvider } from 'react-intl'
 import { DndProvider } from 'react-dnd'
 import MultiBackend from 'react-dnd-multi-backend'
 import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch'
 import NOTIFICATION from '../../../app/data/notification.json'
-
 import NotificationBar from './NotificationBar'
 import MenusContainer from '../menus/MenusContainer'
 import StreetNameplateContainer from '../streets/StreetNameplateContainer'
@@ -28,63 +26,50 @@ import DebugHoverPolygon from '../info_bubble/DebugHoverPolygon'
 import PrintContainer from './PrintContainer'
 import { onResize } from './window_resize'
 
-class App extends React.PureComponent {
-  static propTypes = {
-    locale: PropTypes.object
-  }
+function App () {
+  const locale = useSelector((state) => state.locale)
 
-  componentDidMount () {
+  useEffect(() => {
     onResize()
-  }
+  }, [])
 
-  render () {
-    return (
-      <IntlProvider
-        locale={this.props.locale.locale}
-        key={this.props.locale.locale}
-        messages={this.props.locale.messages}
+  return (
+    <IntlProvider
+      locale={locale.locale}
+      key={locale.locale}
+      messages={locale.messages}
+    >
+      {/* The prop context={window} prevents crash errors with hot-module reloading */}
+      <DndProvider
+        backend={MultiBackend}
+        options={HTML5toTouch}
+        context={window}
       >
-        {/* The prop context={window} prevents crash errors with hot-module reloading */}
-        <DndProvider
-          backend={MultiBackend}
-          options={HTML5toTouch}
-          context={window}
-        >
-          {/* DndProvider allows multiple children; IntlProvider does not */}
-          <NotificationBar
-            locale={this.props.locale.locale}
-            notification={NOTIFICATION}
-          />
-          <BlockingShield />
-          <BlockingError />
-          <Gallery />
-          <Flash />
-          <DebugInfo />
-          <PrintContainer />
-          <div className="main-screen">
-            <MenusContainer />
-            <StreetNameplateContainer />
-            <InfoBubble />
-            <DebugHoverPolygon />
-            <WelcomePanel />
-            <PaletteContainer />
-            <DialogRoot />
-            <StatusMessage />
-            <NoConnectionMessage />
-            <EnvironmentEditor />
-            <SegmentDragLayer />
-            <StreetView />
-          </div>
-        </DndProvider>
-      </IntlProvider>
-    )
-  }
+        {/* DndProvider allows multiple children; IntlProvider does not */}
+        <NotificationBar locale={locale.locale} notification={NOTIFICATION} />
+        <BlockingShield />
+        <BlockingError />
+        <Gallery />
+        <Flash />
+        <DebugInfo />
+        <PrintContainer />
+        <div className="main-screen">
+          <MenusContainer />
+          <StreetNameplateContainer />
+          <InfoBubble />
+          <DebugHoverPolygon />
+          <WelcomePanel />
+          <PaletteContainer />
+          <DialogRoot />
+          <StatusMessage />
+          <NoConnectionMessage />
+          <EnvironmentEditor />
+          <SegmentDragLayer />
+          <StreetView />
+        </div>
+      </DndProvider>
+    </IntlProvider>
+  )
 }
 
-function mapStateToProps (state) {
-  return {
-    locale: state.locale
-  }
-}
-
-export default connect(mapStateToProps)(App)
+export default App
