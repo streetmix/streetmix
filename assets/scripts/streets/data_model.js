4e4856eb297dca8e873cfbca20f02a6e3c08b998
Lou Huang <lou@louhuang.com>
2020-04-30 17:21:25 -0400

    fix(data_model): bug where default segments could be made immutable

--- assets/scripts/streets/data_model.js
+++ assets/scripts/streets/data_model.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
+import cloneDeep from 'lodash/cloneDeep'
 import { DEFAULT_SEGMENTS } from '../segments/default'
 import { getSegmentInfo } from '../segments/info'
 import {
@@ -413,7 +414,7 @@ function fillDefaultSegments (units) {
   const leftHandTraffic = getLeftHandTraffic()
 
   for (const i in DEFAULT_SEGMENTS[leftHandTraffic]) {
-    const segment = DEFAULT_SEGMENTS[leftHandTraffic][i]
+    const segment = cloneDeep(DEFAULT_SEGMENTS[leftHandTraffic][i])
     segment.id = uuidv4()
     segment.warnings = []
     segment.variantString = getVariantString(segment.variant)
