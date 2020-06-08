95eca0b69ea0a0465160612ac46b1b12333753b7
Lou Huang <lou@louhuang.com>
2020-06-08 19:07:37 -0400

    refactor(Checkbox): update to match RangeSlider functionality

--- assets/scripts/ui/Checkbox.jsx
+++ assets/scripts/ui/Checkbox.jsx
@@ -2,14 +2,14 @@
  * Custom stylized checkbox component, so we control the look and
  * feel instead of relying on browser's default styles.
  */
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ICON_CHECK } from '../ui/icons'
 import './Checkbox.scss'
 
 // This stores an incrementing number for unique IDs.
-let idCounter = 0
+let idCounter = 1
 
 Checkbox.propTypes = {
   // Child nodes are wrapped in <label> when rendered.
@@ -29,6 +29,9 @@ Checkbox.propTypes = {
   // The handler function that's called when the value of the input changes.
   onChange: PropTypes.func,
 
+  // Class name applied to containing element
+  className: PropTypes.string,
+
   // An `id` is associates a `label` with an `input` element. If you don't
   // provide one, the component automatically generates a unique ID. IDs
   // are meant "for internal use only".
@@ -42,17 +45,34 @@ function Checkbox (props) {
     disabled = false,
     value,
     onChange = () => {},
+    id,
+    className,
 
-    // An `id` is required to associate a `label` with an `input` element.
-    // You can provide one manually in props, otherwise, this component
-    // will generate a unique ID on each render.
-    id = `checkbox-id-${idCounter++}`
+    // Remainder of props will be applied to the containing <div> element
+    // for instance, `style`, data attributes, etc.
+    ...restProps
   } = props
 
   // This is a controlled component. The `useState` hook maintains
   // this component's internal state, and sets the initial state
   // based on the `checked` prop (which is `false` by default).
   const [isChecked, setChecked] = useState(checked)
+
+  // An `id` is required to associate a `label` with an `input` element.
+  // You can provide one manually in props, otherwise, this component
+  // will generate a unique id value for each instance. Generated ids
+  // are not meant to be accessed by other code or CSS selectors.
+  const elementId = useRef(id)
+  if (!elementId.current) {
+    // This exists in an if statement to check if the ref value is present
+    // to prevent the counter from incrementing on every render
+    elementId.current = `checkbox-id-${idCounter++}`
+  }
+
+  const classNames = ['checkbox-item']
+  if (className) {
+    classNames.push(className)
+  }
 
   // When the value is changed, we update the state, and we also call
   // any `onChange` handler that is provided by the parent via props.
@@ -62,18 +82,16 @@ function Checkbox (props) {
   }
 
   return (
-    <div className="checkbox-item">
+    <div className={classNames.join(' ')} {...restProps}>
       <input
         type="checkbox"
-        id={id}
+        id={elementId.current}
         checked={isChecked}
         value={value}
         disabled={disabled}
         onChange={handleChange}
       />
-      <label htmlFor={id}>
-        {children}
-      </label>
+      <label htmlFor={elementId.current}>{children}</label>
       {/* The visual state of this checkbox is affected by the value of the input, via CSS. */}
       <FontAwesomeIcon icon={ICON_CHECK} />
     </div>
