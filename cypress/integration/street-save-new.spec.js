1980c9053a6dc62c5fa9b2edecf460537b088254
Eric Sherman <ericandrewsherman@gmail.com>
2020-07-08 12:18:50 -0500

    ci(GeotagDialog, cypress): make a street before testing ui
    travis runs tests without a db, so a test might work locally
    but not on travis without having a street POSTed first
    
    ci: consolidate cypress tests
    
    ci: update snapshot

--- cypress/integration/street-save-new.spec.js
+++ cypress/integration/street-save-new.spec.js
@@ -20,6 +20,10 @@ context('User Saving a New Street', () => {
       'fixture:street-image-post-response'
     ).as('streetImagePut')
     cy.route('PUT', '/api/v1/streets/*', 204).as('streetPut')
+    cy.route(
+      ' https://api.geocode.earth/v1/reverse*',
+      'fixture:reverse-location'
+    ).as('locationService')
     cy.visit('', {
       onBeforeLoad (win) {
         cy.stub(win, 'prompt').returns(PROMPT_MESSAGE)
@@ -41,5 +45,20 @@ context('User Saving a New Street', () => {
     cy.contains('Unnamed St').click()
     // prompt autofilled by stub above, thus changing the street name
     cy.wait('@streetPut')
+  })
+
+  // this ideally would be part of its own spec test once we can get that running without a database
+  it('Opens the GeotagDialog & confirms or clears location via the popup button', () => {
+    cy.get('.welcome-panel > .close').click()
+
+    cy.get('.street-metadata-map').click()
+    cy.get('.geotag-dialog').click()
+    cy.get('.leaflet-container').click(625, 300)
+    cy.wait('@locationService')
+    cy.get('.geotag-location-label').should(
+      'have.text',
+      '12572 Fm 586 S, Brookesmith, TX, USA'
+    )
+    cy.get('.button-primary').click({ force: true })
   })
 })
