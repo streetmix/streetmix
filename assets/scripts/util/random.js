ec5e2eb33f9f2f78464ab2b85ab12ccdb0b38fa8
Whitman Schorn <whitman.schorn@gmail.com>
2020-09-13 19:17:35 -0400

    feat: use nanoid to make random seeds

--- assets/scripts/util/random.js
+++ assets/scripts/util/random.js
@@ -1,9 +1,6 @@
-const MAX_RAND_SEED = 999999999
+import { nanoid } from 'nanoid'
 
-/**
- * Returns a random integer between 0 and MAX_RAND_SEED. This value should
- * always be greater than 0.
- */
+// Returns a random string for seeding.
 export function generateRandSeed () {
-  return Math.ceil(Math.random() * MAX_RAND_SEED)
+  return nanoid()
 }
