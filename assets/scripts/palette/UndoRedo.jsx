d05d1d1f88eefe19b1f603f214d76172a71e4995
Lou Huang <lou@louhuang.com>
2020-09-24 22:11:20 -0400

    feat: add tooltips to environment, undo, redo buttons

--- assets/scripts/palette/UndoRedo.jsx
+++ assets/scripts/palette/UndoRedo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useIntl } from 'react-intl'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Tooltip, { useSingleton } from '../ui/Tooltip'
 import { ICON_UNDO, ICON_REDO } from '../ui/icons'
 import { isOwnedByCurrentUser } from '../streets/owner'
 import { handleUndo, handleRedo } from '../store/actions/undo'
@@ -9,6 +10,7 @@ import { handleUndo, handleRedo } from '../store/actions/undo'
 function UndoRedo (props) {
   const undoPosition = useSelector((state) => state.undo.position)
   const undoStack = useSelector((state) => state.undo.stack)
+  const [source, target] = useSingleton()
   const dispatch = useDispatch()
   const intl = useIntl()
 
@@ -27,20 +29,33 @@ function UndoRedo (props) {
 
   return (
     <>
-      <button
-        onClick={() => dispatch(handleUndo())}
-        disabled={!isUndoAvailable()}
-        title={intl.formatMessage({ id: 'btn.undo', defaultMessage: 'Undo' })}
+      <Tooltip source={source} />
+      <Tooltip
+        target={target}
+        label={intl.formatMessage({ id: 'btn.undo', defaultMessage: 'Undo' })}
       >
-        <FontAwesomeIcon icon={ICON_UNDO} />
-      </button>
-      <button
-        onClick={() => dispatch(handleRedo())}
-        disabled={!isRedoAvailable()}
-        title={intl.formatMessage({ id: 'btn.redo', defaultMessage: 'Redo' })}
+        {/* Keep title on button to be queryable by test */}
+        <button
+          onClick={() => dispatch(handleUndo())}
+          disabled={!isUndoAvailable()}
+          title={intl.formatMessage({ id: 'btn.undo', defaultMessage: 'Undo' })}
+        >
+          <FontAwesomeIcon icon={ICON_UNDO} />
+        </button>
+      </Tooltip>
+      <Tooltip
+        target={target}
+        label={intl.formatMessage({ id: 'btn.redo', defaultMessage: 'Redo' })}
       >
-        <FontAwesomeIcon icon={ICON_REDO} />
-      </button>
+        {/* Keep title on button to be queryable by test */}
+        <button
+          onClick={() => dispatch(handleRedo())}
+          disabled={!isRedoAvailable()}
+          title={intl.formatMessage({ id: 'btn.redo', defaultMessage: 'Redo' })}
+        >
+          <FontAwesomeIcon icon={ICON_REDO} />
+        </button>
+      </Tooltip>
     </>
   )
 }
