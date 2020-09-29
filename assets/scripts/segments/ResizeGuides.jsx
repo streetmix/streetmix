ebcce6e16d57c12718e3e07733d3501ca82ef376
Lou Huang <lou@louhuang.com>
2020-09-29 10:47:07 -0400

    refactor(ResizeGuides, SegmentDragHandles): port to function components

--- assets/scripts/segments/ResizeGuides.jsx
+++ assets/scripts/segments/ResizeGuides.jsx
@@ -1,58 +1,81 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import { FormattedMessage } from 'react-intl'
 import { TILE_SIZE, MIN_SEGMENT_WIDTH } from './constants'
 import { getSegmentVariantInfo } from './info'
 import { getSegmentEl } from './view'
 import './ResizeGuides.scss'
 
-ResizeGuides.propTypes = {
-  isVisible: PropTypes.bool,
-  activeSegment: PropTypes.number,
-  segment: PropTypes.object,
-  remainingWidth: PropTypes.number
-}
+function ResizeGuides (props) {
+  const isVisible = useSelector((state) => state.ui.resizeGuidesVisible)
+  const activeSegment = useSelector((state) =>
+    typeof state.ui.activeSegment === 'number' ? state.ui.activeSegment : null
+  )
+  const segment = useSelector(
+    (state) => state.street.segments[state.ui.activeSegment] || null
+  )
+  const remainingWidth = useSelector((state) => state.street.remainingWidth)
+  const [display, setDisplay] = useState({})
 
-function ResizeGuides ({
-  isVisible = false,
-  activeSegment,
-  segment,
-  remainingWidth
-}) {
+  // Calculate render position once, only when the resize guides become visible.
+  // Only updates on a change in value of the `isVisible` prop (which
+  // shows/hides the guides), to prevent continuous re-renders during a
+  // resize drag input. Its appearance should remain the same throughout the
+  // entire drag action.
+  //
+  // The `segment` and `remainingWidth` values may change during the resizing,
+  // but they should NOT update this calculation.
+  useEffect(() => {
+    if (isVisible && segment) {
+      const variantInfo = getSegmentVariantInfo(
+        segment.type,
+        segment.variantString
+      )
+
+      // Maximum-width guides are displayed based on recommended maximum widths
+      // of the segment variant, if provided, but this is also limited by the
+      // remaining space of the street. If no maximum-width recommendations
+      // are provided, the maximum width would be any remaining width of the street.
+      const actualRemainingWidth = remainingWidth + segment.width
+      const shouldUseRemainingWidth =
+        actualRemainingWidth &&
+        ((!variantInfo.minWidth && actualRemainingWidth >= MIN_SEGMENT_WIDTH) ||
+          actualRemainingWidth >= variantInfo.minWidth) &&
+        (!variantInfo.maxWidth || actualRemainingWidth <= variantInfo.maxWidth)
+
+      // Render minimum-width guides if minimum widths are recommended by the
+      // segment variant
+      const shouldRenderMinGuide = Number.isFinite(variantInfo.minWidth)
+      const shouldRenderMaxGuide =
+        shouldUseRemainingWidth || Number.isFinite(variantInfo.maxWidth)
+
+      // Calculate the centerline of the segment (its left offset plus half its width)
+      // Adjusting the centerline by 1px to the left seems to "look" better
+      const el = getSegmentEl(activeSegment)
+      const centerline =
+        el.offsetLeft + (el.cssTransformLeft || 0) + el.offsetWidth / 2 - 1
+
+      setDisplay({
+        style: { left: centerline },
+        minGuide: shouldRenderMinGuide,
+        minGuideStyle: getStyle(variantInfo.minWidth),
+        maxGuide: shouldRenderMaxGuide,
+        maxGuideStyle: getStyle(
+          shouldUseRemainingWidth ? actualRemainingWidth : variantInfo.maxWidth
+        )
+      })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isVisible])
+
   if (!isVisible || !segment) return null
 
-  const variantInfo = getSegmentVariantInfo(segment.type, segment.variantString)
-
-  // Maximum-width guides are displayed based on recommended maximum widths
-  // of the segment variant, if provided, but this is also limited by the
-  // remaining space of the street. If no maximum-width recommendations
-  // are provided, the maximum width would be any remaining width of the street.
-  const actualRemainingWidth = remainingWidth + segment.width
-  const shouldUseRemainingWidth =
-    actualRemainingWidth &&
-    ((!variantInfo.minWidth && actualRemainingWidth >= MIN_SEGMENT_WIDTH) ||
-      actualRemainingWidth >= variantInfo.minWidth) &&
-    (!variantInfo.maxWidth || actualRemainingWidth <= variantInfo.maxWidth)
-
-  // Render minimum-width guides if minimum widths are recommended by the
-  // segment variant
-  const shouldRenderMinGuide = Number.isFinite(variantInfo.minWidth)
-  const shouldRenderMaxGuide =
-    shouldUseRemainingWidth || Number.isFinite(variantInfo.maxWidth)
-
-  // Calculate the centerline of the segment (its left offset plus half its width)
-  // Adjusting the centerline by 1px to the left seems to "look" better
-  const el = getSegmentEl(activeSegment)
-  const centerline =
-    el.offsetLeft + (el.cssTransformLeft || 0) + el.offsetWidth / 2 - 1
-
   return (
-    <div className="resize-guides" style={{ left: centerline }}>
-      {shouldRenderMinGuide && (
+    <div className="resize-guides" style={display.style}>
+      {display.minGuide && (
         <div
           className="resize-guide resize-guide-min"
-          style={getStyle(variantInfo.minWidth)}
+          style={display.minGuideStyle}
         >
           <div className="resize-guide-min-before">
             « <FormattedMessage id="segments.resize.min" defaultMessage="Min" />
@@ -62,14 +85,10 @@ function ResizeGuides ({
           </div>
         </div>
       )}
-      {shouldRenderMaxGuide && (
+      {display.maxGuide && (
         <div
           className="resize-guide resize-guide-max"
-          style={getStyle(
-            shouldUseRemainingWidth
-              ? actualRemainingWidth
-              : variantInfo.maxWidth
-          )}
+          style={display.maxGuideStyle}
         >
           <div className="resize-guide-max-before">
             <FormattedMessage id="segments.resize.max" defaultMessage="Max" /> »
@@ -92,33 +111,4 @@ function getStyle (width) {
   }
 }
 
-function mapStateToProps (state) {
-  return {
-    isVisible: state.ui.resizeGuidesVisible,
-    activeSegment:
-      typeof state.ui.activeSegment === 'number'
-        ? state.ui.activeSegment
-        : null,
-    segment: state.street.segments[state.ui.activeSegment] || null,
-    remainingWidth: state.street.remainingWidth
-  }
-}
-
-export default connect(mapStateToProps)(
-  React.memo(
-    ResizeGuides,
-    /**
-     * Only updates on a change in value of the `isVisible` prop (which
-     * shows/hides the guides), to prevent continuous re-renders during a
-     * resize drag input. Its appearance should remain the same throughout the
-     * entire drag action.
-     *
-     * The `segment` and `remainingWidth` props may change during the resizing,
-     * so we can't rely on normal memoization of all props.
-     *
-     * Note that we cannot use the useSelector hook because that doesn't give
-     * us a way to do this comparison of just the one value.
-     */
-    (prevProps, nextProps) => prevProps.isVisible === nextProps.isVisible
-  )
-)
+export default ResizeGuides
--- assets/scripts/segments/SegmentDragHandles.jsx
+++ assets/scripts/segments/SegmentDragHandles.jsx
@@ -1,46 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { ICON_CHEVRON_LEFT, ICON_CHEVRON_RIGHT } from '../ui/icons'
 import './SegmentDragHandles.scss'
 
-export class SegmentDragHandles extends React.PureComponent {
-  static propTypes = {
-    width: PropTypes.number,
-    infoBubbleHovered: PropTypes.bool,
-    descriptionVisible: PropTypes.bool
-  }
-
-  render () {
-    const display = (this.props.infoBubbleHovered || this.props.descriptionVisible)
-      ? 'none' : null
-
-    // To prevent drag handles from overlapping each other when the segment widths are very small,
-    // we calculate an X-position adjustment when the value of `width` is less than 60px.
-    // The X position adjustment follows the linear equation y = 0.5x - 35 (where `x` is `width`).
-    // For example, (`width = 36`, `adjustX = -11px`), (`width = 12`, `adjustX = -29px`).
-    const adjustX = (this.props.width < 60)
-      ? `${(0.5 * this.props.width) - 35}px` : null
-
-    return (
-      <>
-        <span className="drag-handle drag-handle-left" style={{ display, left: adjustX }}>
-          <FontAwesomeIcon icon={ICON_CHEVRON_LEFT} />
-        </span>
-        <span className="drag-handle drag-handle-right" style={{ display, right: adjustX }}>
-          <FontAwesomeIcon icon={ICON_CHEVRON_RIGHT} />
-        </span>
-      </>
-    )
-  }
+SegmentDragHandles.propTypes = {
+  width: PropTypes.number
 }
 
-function mapStateToProps (state) {
-  return {
-    infoBubbleHovered: state.infoBubble.mouseInside,
-    descriptionVisible: state.infoBubble.descriptionVisible
-  }
+function SegmentDragHandles ({ width }) {
+  const infoBubbleHovered = useSelector((state) => state.infoBubble.mouseInside)
+  const descriptionVisible = useSelector(
+    (state) => state.infoBubble.descriptionVisible
+  )
+
+  const display = infoBubbleHovered || descriptionVisible ? 'none' : null
+
+  // To prevent drag handles from overlapping each other when the segment widths are very small,
+  // we calculate an X-position adjustment when the value of `width` is less than 60px.
+  // The X position adjustment follows the linear equation y = 0.5x - 35 (where `x` is `width`).
+  // For example, (`width = 36`, `adjustX = -11px`), (`width = 12`, `adjustX = -29px`).
+  const adjustX = width < 60 ? `${0.5 * width - 35}px` : null
+
+  return (
+    <>
+      <span
+        className="drag-handle drag-handle-left"
+        style={{ display, left: adjustX }}
+      >
+        <FontAwesomeIcon icon={ICON_CHEVRON_LEFT} />
+      </span>
+      <span
+        className="drag-handle drag-handle-right"
+        style={{ display, right: adjustX }}
+      >
+        <FontAwesomeIcon icon={ICON_CHEVRON_RIGHT} />
+      </span>
+    </>
+  )
 }
 
-export default connect(mapStateToProps)(SegmentDragHandles)
+export default React.memo(SegmentDragHandles)
