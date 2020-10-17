c277c187809379103f6d358f47ea62a9b7c1a1cd
Lou Huang <lou@louhuang.com>
2020-10-17 16:39:00 -0400

    fix(UpDownInput): text input should update proper segment
    There is a bug in the <UpDownInput /> component where updating its value
    in the input element updates the wrong segment. This is caused by the
    `debounceUpdateValue` method caching callback functions set to the position
    of segments from previous renders.
    
    This becomes solved by converting the component to a function component
    (using React Hooks), from a class component.
    
    In addition, the logic has been refactored to try and improve how the
    component reacts / behaves. The next step is to follow up with better
    unit tests.

--- assets/scripts/info_bubble/UpDownInput.jsx
+++ assets/scripts/info_bubble/UpDownInput.jsx
@@ -1,195 +1,189 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { debounce } from 'lodash'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ICON_MINUS, ICON_PLUS } from '../ui/icons'
 import './UpDownInput.scss'
 
-const EDIT_INPUT_DELAY = 200
+const EDIT_INPUT_DELAY = 800
 
-export default class UpDownInput extends React.Component {
-  static propTypes = {
-    // Raw input value must always be a number type which can be
-    // compared with the minValue and maxValue.
-    value: PropTypes.number,
-    minValue: PropTypes.number,
-    maxValue: PropTypes.number,
+UpDownInput.propTypes = {
+  // Raw input value must always be a number type which can be
+  // compared with the minValue and maxValue.
+  value: PropTypes.number,
+  minValue: PropTypes.number,
+  maxValue: PropTypes.number,
 
-    // Formatter functions are used to optionally format raw values
-    // for display. These functions should return a number or a string.
+  // Formatter functions are used to optionally format raw values
+  // for display. These functions should return a number or a string.
 
-    // `inputValueFormatter` formats a value that is displayed when
-    // a user has focused or hovered over the <input> element. If this
-    // function is unspecified, the display value remains the raw
-    // `value` prop.
-    inputValueFormatter: PropTypes.func,
+  // `inputValueFormatter` formats a value that is displayed when
+  // a user has focused or hovered over the <input> element. If this
+  // function is unspecified, the display value remains the raw
+  // `value` prop.
+  inputValueFormatter: PropTypes.func,
 
-    // `displayValueFormatter` formats a value that is displayed inside
-    // the <input> element when it is not being edited. If this
-    // function is unspecified, the display value remains the raw
-    // `value` prop.
-    displayValueFormatter: PropTypes.func,
+  // `displayValueFormatter` formats a value that is displayed inside
+  // the <input> element when it is not being edited. If this
+  // function is unspecified, the display value remains the raw
+  // `value` prop.
+  displayValueFormatter: PropTypes.func,
 
-    // Handler functions are specified by the parent component. These
-    // handlers should be responsible for validating raw inputs and
-    // updating street data.
-    onClickUp: PropTypes.func,
-    onClickDown: PropTypes.func,
-    onUpdatedValue: PropTypes.func,
+  // Handler functions are specified by the parent component. These
+  // handlers should be responsible for validating raw inputs and
+  // updating street data.
+  onClickUp: PropTypes.func,
+  onClickDown: PropTypes.func,
+  onUpdatedValue: PropTypes.func,
 
-    // When `true`, the input box and buttons are disabled
-    disabled: PropTypes.bool,
+  // When `true`, the input box and buttons are disabled
+  disabled: PropTypes.bool,
 
-    // Tooltip text
-    inputTooltip: PropTypes.string,
-    upTooltip: PropTypes.string,
-    downTooltip: PropTypes.string
-  }
+  // Tooltip text
+  inputTooltip: PropTypes.string,
+  upTooltip: PropTypes.string,
+  downTooltip: PropTypes.string
+}
 
-  static defaultProps = {
-    inputValueFormatter: (value) => value,
-    displayValueFormatter: (value) => value,
-    onClickUp: () => {},
-    onClickDown: () => {},
-    onUpdatedValue: () => {},
-    disabled: false,
-    inputTooltip: 'Change value',
-    upTooltip: 'Increment',
-    downTooltip: 'Decrement'
-  }
+function UpDownInput (props) {
+  // Destructure props with default values
+  const {
+    value,
+    minValue,
+    maxValue,
+    inputValueFormatter = (value) => value,
+    displayValueFormatter = (value) => value,
+    onClickUp = () => {},
+    onClickDown = () => {},
+    onUpdatedValue = () => {},
+    disabled = false,
+    inputTooltip = 'Change value',
+    upTooltip = 'Increment',
+    downTooltip = 'Decrement'
+  } = props
 
-  constructor (props) {
-    super(props)
+  const oldValue = useRef(null)
+  const inputEl = useRef()
 
-    this.oldValue = null
-    this.inputEl = React.createRef()
+  const [isEditing, setIsEditing] = useState(false)
+  const [isHovered, setIsHovered] = useState(false)
+  const [displayValue, setDisplayValue] = useState(value)
+  const [userInputValue, setUserInputValue] = useState(null)
 
-    this.state = {
-      isEditing: false,
-      isHovered: false,
-      displayValue: null
+  const debounceUpdateValue = debounce(onUpdatedValue, EDIT_INPUT_DELAY)
+
+  // Depending on what happens, set the display value of the <input> element.
+  useEffect(() => {
+    // If component is disabled, display nothing
+    if (disabled) {
+      setDisplayValue('')
+      return
     }
 
-    // Debounce `props.onUpdatedValue` to prevent rapid input changes from thrashing data
-    this.debounceUpdateValue = debounce(props.onUpdatedValue, EDIT_INPUT_DELAY)
-  }
-
-  /**
-   * If UI is not in user-editing mode, update the display
-   * when the value in store changes
-   *
-   * @param {Object} nextProps
-   */
-  static getDerivedStateFromProps (nextProps, prevState) {
-    if (!prevState.isEditing && !prevState.isHovered) {
-      return {
-        displayValue: nextProps.displayValueFormatter(nextProps.value)
+    // If input is being edited (or hovered, which displays its "raw" value),
+    // display the value without units. The `inputValueFormatter` function is
+    // run, which takes into account the user's preferred units.
+    if (isEditing || isHovered) {
+      if (userInputValue) {
+        // If there is user input, always display that
+        setDisplayValue(userInputValue)
+      } else {
+        // Otherwise, display the value from props
+        setDisplayValue(inputValueFormatter(value))
       }
+      return
     }
 
-    return {
-      value: nextProps.value
-    }
-  }
+    // In all other cases, display the "prettified" value inside the input,
+    // which is the "raw" value formatted using the correct unit conversion
+    // and unit label.
+    setDisplayValue(displayValueFormatter(value))
+  }, [
+    value,
+    userInputValue,
+    disabled,
+    isEditing,
+    isHovered,
+    inputValueFormatter,
+    displayValueFormatter
+  ])
 
   /**
    * If UI is going to enter user-editing mode, immediately
    * save the previous value in case editing is cancelled
-   *
-   * @param {Object} prevProps
-   * @param {Object} prevState
    */
-  componentDidUpdate (prevProps, prevState) {
-    if (!prevState.isEditing && this.state.isEditing) {
-      this.oldValue = prevProps.value
+  useEffect(() => {
+    if (isEditing === true) {
+      oldValue.current = value
+    } else {
+      // reset `userInputValue`
+      setUserInputValue(null)
     }
+    // We only want to save the old value once, not every time it changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEditing])
+
+  function handleClickIncrement (event) {
+    onClickUp(event)
   }
 
-  handleClickIncrement = (event) => {
-    this.props.onClickUp(event)
+  function handleClickDecrement (event) {
+    onClickDown(event)
   }
 
-  handleClickDecrement = (event) => {
-    this.props.onClickDown(event)
+  function handleInputClick (event) {
+    setIsEditing(true)
   }
 
-  handleInputChange = (event) => {
+  function handleInputChange (event) {
     const value = event.target.value
 
     // Update the input element to display user input
-    this.setState({
-      isEditing: true,
-      displayValue: value
-    })
+    setUserInputValue(value)
 
     // Send the value to the parent's handler function
-    // using the debounced version of `props.onUpdatedValue`
-    this.debounceUpdateValue(value)
+    // using the debounced version of `onUpdatedValue`
+    debounceUpdateValue(value)
   }
 
-  handleInputClick = (event) => {
-    const el = event.target
-
-    this.setState({
-      isEditing: true,
-      displayValue: this.props.inputValueFormatter(this.props.value)
-    })
-
-    if (document.activeElement !== el) {
-      el.select()
+  function handleInputFocus (event) {
+    // Automatically select the value on focus so that it's easy to start
+    // typing new values. In React, this only works if the .select() is called
+    // at the end of the execution stack, so we put it inside a setTimeout()
+    // with a timeout of zero. We also must store the reference to the event
+    // target because the React synthetic event will not persist into the
+    // `setTimeout` function.
+    const target = event.target
+    if (document.activeElement !== target) {
+      window.setTimeout(() => {
+        target.select()
+      }, 0)
     }
   }
 
-  handleInputDoubleClick = (event) => {
-    const el = event.target
-    el.select()
+  function handleInputBlur (event) {
+    setIsEditing(false)
   }
 
-  handleInputFocus = (event) => {
-    const el = event.target
+  function handleInputMouseDown (event) {
+    // Bail if already in editing mode.
+    if (isEditing) return
 
-    if (document.activeElement !== el) {
-      el.select()
-    }
-  }
-
-  /**
-   * On blur, input shows prettified value.
-   */
-  handleInputBlur = (event) => {
-    this.setState({
-      isEditing: false,
-      displayValue: this.props.displayValueFormatter(this.props.value)
-    })
-  }
-
-  /**
-   * Prevent mousedown event from resetting the input view.
-   */
-  handleInputMouseDown = (event) => {
-    this.setState({
-      isEditing: true,
-      displayValue: this.props.inputValueFormatter(this.props.value)
-    })
+    setIsEditing(true)
   }
 
   /**
    * On mouse over, UI assumes user is ready to edit.
    */
-  handleInputMouseOver = (event) => {
+  function handleInputMouseOver (event) {
     // Bail if already in editing mode.
-    if (this.state.isEditing) return
+    if (isEditing) return
 
-    this.setState({
-      isHovered: true,
-      displayValue: this.props.inputValueFormatter(this.props.value)
-    })
+    setIsHovered(true)
 
-    // Automatically select the value on hover so that it's easy to start typing new values.
-    // In React, this only works if the .select() is called at the end of the execution
-    // stack, so we put it inside a setTimeout() with a timeout of zero. We also must
-    // store the reference to the event target because the React synthetic event will
-    // not persist into the setTimeout function.
+    // Automatically select the value on hover so that it's easy to start
+    // typing new values. See comment in `handleInputFocus` about why we use
+    // a `setTimeout` with a 0 time value here.
     const target = event.target
     window.setTimeout(() => {
       target.focus()
@@ -200,100 +194,79 @@ export default class UpDownInput extends React.Component {
   /**
    * On mouse out, if user is not editing, UI returns to default view.
    */
-  handleInputMouseOut = (event) => {
+  function handleInputMouseOut (event) {
     // Bail if already in editing mode.
-    if (this.state.isEditing) return
+    if (isEditing) return
 
-    this.setState({
-      isHovered: false,
-      displayValue: this.props.displayValueFormatter(this.props.value)
-    })
+    setIsHovered(false)
 
     event.target.blur()
   }
 
-  handleInputKeyDown = (event) => {
+  function handleInputKeyDown (event) {
     switch (event.key) {
       case 'Enter':
-        this.props.onUpdatedValue(event.target.value)
+        onUpdatedValue(event.target.value)
 
-        this.setState({
-          isEditing: false
-        })
+        setIsEditing(false)
 
-        this.inputEl.current.focus()
-        this.inputEl.current.select()
+        inputEl.current.focus()
+        inputEl.current.select()
 
         break
       case 'Esc': // IE/Edge specific value
       case 'Escape':
         // Lose focus from input but place focus on body
-        this.inputEl.current.blur()
+        inputEl.current.blur()
         document.body.focus()
 
         // Reset editing or hover state
-        this.setState({
-          isEditing: false,
-          isHovered: false
-        })
+        setIsEditing(false)
+        setIsHovered(false)
 
-        this.props.onUpdatedValue(this.oldValue)
+        onUpdatedValue(oldValue.current)
         break
     }
   }
 
-  renderInputEl = () => (
-    <input
-      type="text"
-      className="up-down-input-element"
-      title={this.props.inputTooltip}
-      disabled={this.props.disabled}
-      value={this.props.disabled ? '' : this.state.displayValue}
-      onChange={this.handleInputChange}
-      onClick={this.handleInputClick}
-      onDoubleClick={this.handleInputDoubleClick}
-      onFocus={this.handleInputFocus}
-      onBlur={this.handleInputBlur}
-      onMouseDown={this.handleInputMouseDown}
-      onMouseOver={this.handleInputMouseOver}
-      onMouseOut={this.handleInputMouseOut}
-      onKeyDown={this.handleInputKeyDown}
-      ref={this.inputEl}
-    />
+  return (
+    <>
+      <button
+        className="up-down-input-decrement"
+        title={downTooltip}
+        tabIndex={-1}
+        onClick={handleClickDecrement}
+        disabled={disabled || (minValue ? value <= minValue : false)}
+      >
+        <FontAwesomeIcon icon={ICON_MINUS} />
+      </button>
+      <input
+        type="text"
+        className="up-down-input-element"
+        title={inputTooltip}
+        disabled={disabled}
+        value={displayValue}
+        onChange={handleInputChange}
+        onClick={handleInputClick}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        onMouseDown={handleInputMouseDown}
+        onMouseOver={handleInputMouseOver}
+        onMouseOut={handleInputMouseOut}
+        onKeyDown={handleInputKeyDown}
+        ref={inputEl}
+      />
+      <button
+        className="up-down-input-increment"
+        title={upTooltip}
+        tabIndex={-1}
+        onClick={handleClickIncrement}
+        disabled={disabled || (maxValue ? value >= maxValue : false)}
+      >
+        <FontAwesomeIcon icon={ICON_PLUS} />
+      </button>
+    </>
   )
-
-  /**
-   * @todo make minValue and maxValue optional
-   */
-  render () {
-    return (
-      <>
-        <button
-          className="up-down-input-decrement"
-          title={this.props.downTooltip}
-          tabIndex={-1}
-          onClick={this.handleClickDecrement}
-          disabled={
-            this.props.disabled || this.props.value <= this.props.minValue
-          }
-        >
-          <FontAwesomeIcon icon={ICON_MINUS} />
-        </button>
-
-        {this.renderInputEl()}
-
-        <button
-          className="up-down-input-increment"
-          title={this.props.upTooltip}
-          tabIndex={-1}
-          onClick={this.handleClickIncrement}
-          disabled={
-            this.props.disabled || this.props.value >= this.props.maxValue
-          }
-        >
-          <FontAwesomeIcon icon={ICON_PLUS} />
-        </button>
-      </>
-    )
-  }
 }
+
+export default UpDownInput
