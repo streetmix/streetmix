2c7abf25b2c049ed77620f3b6fca1bfc6f4e7e0f
Lou Huang <lou@louhuang.com>
2020-11-11 09:33:41 -0500

    fix(sequelize): correct import model syntax

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -36,7 +36,10 @@ fs.readdirSync(__dirname)
     )
   })
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
     if (!model) {
       throw new Error(`missing model for file: ${file}`)
     }
