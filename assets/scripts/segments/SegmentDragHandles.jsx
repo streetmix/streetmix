3f7781aae4cb48cc250a328f9d3560e62729081b
Lou Huang <lou@louhuang.com>
2020-11-20 12:19:30 -0500

    style(SegmentDragHandles): format comment length

--- assets/scripts/segments/SegmentDragHandles.jsx
+++ assets/scripts/segments/SegmentDragHandles.jsx
@@ -17,10 +17,13 @@ function SegmentDragHandles ({ width }) {
 
   const display = infoBubbleHovered || descriptionVisible ? 'none' : null
 
-  // To prevent drag handles from overlapping each other when the segment widths are very small,
-  // we calculate an X-position adjustment when the value of `width` is less than 60px.
-  // The X position adjustment follows the linear equation y = 0.5x - 35 (where `x` is `width`).
-  // For example, (`width = 36`, `adjustX = -11px`), (`width = 12`, `adjustX = -29px`).
+  // To prevent drag handles from overlapping each other when the segmen
+  // widths are very small, we calculate an X-position adjustment when the
+  // value of `width` is less than 60px. The X position adjustment follows
+  // the linear equation y = 0.5x - 35 (where `x` is `width`).
+  // For example:
+  //    width = 36 ==> adjustX = -11px
+  //    width = 12 ==> adjustX = -29px
   const adjustX = width < 60 ? `${0.5 * width - 35}px` : null
 
   return (
