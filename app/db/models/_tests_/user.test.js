b333284dfea23d92cfdff3b56232a012ba276b73
Eric Sherman <ericandrewsherman@gmail.com>
2021-01-05 18:06:27 -0600

    feat(testing): adds database tests for user model

---
+++ app/db/models/_tests_/user.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+import userFactory from '../../factories/user.js'
+const { User } = require('../../models')
+
+describe('creates a user', () => {
+  let user
+
+  // Before any tests run, clear the DB and run migrations with Sequelize sync()
+  // note: we may not need the pretest hooks if we do it this way?
+  beforeAll(async () => {
+    await User.sequelize.sync({ force: true })
+  })
+
+  beforeEach(async () => {
+    user = await userFactory()
+  })
+  it('created user to match', async () => {
+    user = await userFactory({ id: 'thefirstuser' })
+    expect(user.id).toBe('thefirstuser')
+  })
+  it('has a default role of USER', async () => {
+    expect(user.roles[0]).toBe('USER')
+    console.log(user.roles)
+  })
+  it('adds admin to role', async () => {
+    user.addRole('ADMIN')
+    console.log(user.roles)
+    expect(user.roles[1]).toBe('ADMIN')
+  })
+
+  // After all tests have finished, close the DB connection
+  afterAll(async () => {
+    await User.sequelize.close()
+  })
+})
