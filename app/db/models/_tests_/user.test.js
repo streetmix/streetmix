9b9cbb751b88948ecca001dca63b2e1758543710
Eric <ericandrewsherman@gmail.com>
2021-01-08 13:39:36 -0600

    feat(testing): testing for error seems to throw error

--- app/db/models/_tests_/user.test.js
+++ app/db/models/_tests_/user.test.js
@@ -25,16 +25,11 @@ describe('creates a user', () => {
     user.roles.push('ADMIN')
     expect(user.roles[1]).toBe('ADMIN')
   })
-  it('adds admin to role', async () => {
+  it('throws an error when role is invalid', async () => {
     user.roles.push('SPACE')
-    // should fail validation, but im not certain how to check this
-    // const validatedItem = await user.validate()
-    expect(user.roles[1]).toBe('SPACE')
-  })
-  it('should throw an error', () => {
-    expect(() => user.roles.push('"SPACE"')).toThrowError(
-      'Role does not match list of valid roles'
-    )
+    expect(() => {
+      user.validate()
+    }).toThrow('Role does not match list of valid roles')
   })
 
   // After all tests have finished, close the DB connection
