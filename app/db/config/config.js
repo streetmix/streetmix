9f24c0204a49854626542d4125b1b3598f37e973
Lou Huang <lou@louhuang.com>
2021-02-01 20:37:26 -0500

    refactor: put heroku-specific sequelize options into config

--- app/db/config/config.js
+++ app/db/config/config.js
@@ -6,16 +6,11 @@ const config = require('config')
 // Export environment-specific Sequelize configuration
 // imported from app-level configuration
 // required by sequelize-cli utility
-const data = {
+module.exports = {
   // Property needs to match the environment sequelize is used in
   [config.env]: {
     // Dialect needs to be explicitly supplied as of sequelize v4.0.0
     dialect: 'postgres',
-    // Heroku explicitly requires ssl: true, otherwise connections may
-    // fail intermittently. `dialectOptions` is not well documented by
-    // the sequelize ORM, and this issue seems to be only guidance there is,
-    // even for Heroku support staff:
-    // https://github.com/sequelize/sequelize/issues/956#issuecomment-713296042
     dialectOptions: {
       ssl: {
         // Ref.: https://github.com/brianc/node-postgres/issues/2009
@@ -25,7 +20,3 @@ const data = {
     ...config.db.sequelize
   }
 }
-
-console.log(data)
-
-module.exports = data
--- config/production.js
+++ config/production.js
@@ -20,6 +20,18 @@ module.exports = {
         min: 0,
         acquire: 30000,
         idle: 10000
+      },
+      // Heroku requires SSL connections to Postgres. As of the date of this
+      // commit, hobby-tier and some some older database instances do not yet
+      // enforce SSL. As a result, we also set the `rejectUnauthorized`
+      // property to `false` so we don't fail on the error message
+      // "self signed certificate".
+      // Additional info:
+      // https://help.heroku.com/tickets/955239#event_5b8da58d-e65f-4f18-8744-e96c0b7507f6
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false
+        }
       }
     }
   },
