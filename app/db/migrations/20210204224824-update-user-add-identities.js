7be07b466b216a21a93d5f86e47d8fbcd02ada00
Eric Sherman <ericandrewsherman@gmail.com>
2021-02-05 12:30:21 -0600

    feat(patreon): add field to user db, comments

---
+++ app/db/migrations/20210204224824-update-user-add-identities.js
@@ -0,0 +1,11 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'identities', Sequelize.JSON)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'identities', Sequelize.JSON)
+  }
+}
--- app/resources/services/integrations/patreon.js
+++ app/resources/services/integrations/patreon.js
@@ -1,8 +1,33 @@
 const config = require('config')
+const logger = require('../../../../lib/logger.js')()
+
 const { User } = require('../../../db/models')
 const passport = require('passport')
 const PatreonStrategy = require('passport-patreon').Strategy
 
+/*
+our use case makes this a little complicated,
+we basically have three user instances to check:
+
+ * Auth0 data on a user who is authenticated (aka 'user)
+ * Data on this user that we store in our database (aka 'databaseUser')
+ * Data from a user's third party account (aka profile)
+
+Our existing code handles login using the auth0 service,
+which if valid we store a JWT.
+
+We pass this to the initial get route,
+which passes along the account id/name (and errors if no one is signed in)
+
+We then pass this to our passport strategy, which grabs third party data and
+looks up the user in _our_ database, which then gets handled by the callback function
+an subsequently links all this info together
+
+*/
+
+/**
+ finds the database record for the given user
+ */
 const findUser = async function (userId) {
   const user = await User.findByPk(userId)
   if (user === null) {
@@ -21,16 +46,20 @@ passport.use(
       passReqToCallback: true
     },
     async function (req, accessToken, refreshToken, profile, done) {
-      // this could find by patreon id, but we also need the existing user
-      // im not sure if this is the best place to handle checking the DB or not
-      const DBuser = await findUser(req.query.state)
-      // passing profile along the request, probably another way to do this
+      /*
+      this is what passport calls the 'verify callback'
+      our case is a little different since we already have a logged in user
+      not sure if this is the best place to handle checking the DB or not but it works
+       */
+      const databaseUser = await findUser(req.query.state)
+      // passing the profile data along the request, probably another way to do this
       req.profile = profile
-      return done(null, DBuser)
+      return done(null, databaseUser)
     }
   )
 )
 
+// these aren't used yet, but would be if we start using persistent user sessions
 passport.serializeUser(function (user, done) {
   done(null, user)
 })
@@ -43,11 +72,14 @@ passport.deserializeUser(function (id, done) {
 })
 
 exports.get = (req, res, next) => {
-  // at this point the request has user info from auth0
-  // auth0 nickname == user.id in our internal db that we'll need later
-  // this might not be available if the user isn't signed in
-  // we could add error handiling for this, but also they _should_ only ever be
-  // getting here from a button that you only see when you're signed in..
+  /*
+  at this point,the request has user info from auth0 that we passed to this route
+  auth0 nickname == user.id in our internal db that we'll need later for lookup
+
+  this might not be available if the user isn't signed in
+    we could add error handiling for this, but also they _should_ only ever be
+    getting here from a button that you only see when you're signed in..
+  */
   passport.authorize('patreon', {
     state: req.user.nickname,
     failureRedirect: '/error'
@@ -60,12 +92,29 @@ exports.callback = (req, res, next) => {
   })(req, res, next)
 }
 
-exports.connectUser = (req, res) => {
-  // pass profile here, construct an object to save to user DB
-  const account = req.account
-  const identities = {
+/**
+ * connects the third party profile with the database user record
+ * pass third party profile data here, construct an object to save to user DB
+ */
+exports.connectUser = async (req, res) => {
+  // in passport, using 'authorize' attaches user data to 'account'
+  // instead of overriding the user session data
+  const databaseUser = req.account
+  const identity = {
     provider: req.profile.provider,
     user_id: req.profile.id
   }
-  res.redirect('/')
+  try {
+    await User.update(
+      {
+        identities: [identity]
+      },
+      { where: { id: databaseUser.id }, returning: true }
+    )
+    res.redirect('/')
+  } catch (err) {
+    // what would we want to do here?
+    logger.error(err)
+    res.redirect('/error')
+  }
 }
