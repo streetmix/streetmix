f9d2c4da78ab1cf2523b13c6abf12f23ff48761b
Lou Huang <lou@louhuang.com>
2021-02-18 16:18:40 -0500

    test(react-dnd): refactor test implementation
    This commit began as an attempt to remove the `React.createFactory() will
    be deprecated` warnings that would occur during unit tests. The warning came
    from the `recompose` package, which is no longer being updated. This meant
    that any solution would require replacing the `recompose` package, either
    with a similar one, or with different functionality.
    
    We only use this package to set up a mock version of the react-dnd library
    for testing.
    
    So, instead of replacing `recompose`, I refocused my attention on writing
    the tests differently. The solution (based on react-dnd's documentation)
    would be to wrap components under test, rather than to stub out the library
    entirely.
    
    Looking at react-testing-library docs, we also see some patterns for
    testing components that are wrapped with providers that we could adopt to
    improve and streamline our own codebase.
    
    This commit is targeted only for react-dnd implementation, but also
    provides the initial step in putting all of our provider wrappers into
    one single method (instead of `renderWithIntl` or `renderWithRedux`
    separately).

--- test/helpers/render.js
+++ test/helpers/render.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { render } from '@testing-library/react'
 import { IntlProvider } from 'react-intl'
 import { Provider } from 'react-redux'
-import { render } from '@testing-library/react'
+import { DndProvider } from 'react-dnd'
+import { TestBackend } from 'react-dnd-test-backend'
 import { createStore } from './store'
+
+// Define a wrapper component that includes all of our global context
+// providers from Redux, react-intl, react-dnd. The nesting order of these
+// components should match the actual order in the application.
+/* eslint-disable react/prop-types */
+const AllTheProviders = ({ store = {}, children }) => {
+  return (
+    <Provider store={store}>
+      <IntlProvider locale="en">
+        <DndProvider backend={TestBackend}>{children}</DndProvider>
+      </IntlProvider>
+    </Provider>
+  )
+}
+
+// Define a custom render method which wraps our UI elements with the global
+// context providers, defined above as `AllTheProviders`. For more information:
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
+//
+// We adopt a pattern to pass props to the wrapper, as described here:
+// https://github.com/testing-library/react-testing-library/issues/780
+export const renderWithProviders = (ui, options = {}) => {
+  const {
+    initialState,
+    store = createStore(initialState),
+    ...restOpts
+  } = options
+  const wrapper = (props) => <AllTheProviders {...props} store={store} />
+  return {
+    ...render(ui, { wrapper, ...restOpts }),
+    store
+  }
+}
 
 export const renderWithRedux = function (
   ui,
@@ -33,3 +68,13 @@ export const renderWithReduxAndIntl = function (
     store
   }
 }
+
+// Re-export everything
+// This is a pattern suggested by React Testing Library (see
+// https://testing-library.com/docs/react-testing-library/setup/)
+// so you can import the render and all other methods from just one
+// helper module (this one!)
+export * from '@testing-library/react'
+
+// Override render method so that test syntax can remain simple
+export { renderWithProviders as render }
