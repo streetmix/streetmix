fffa8b9f422c695f6f8e76cd7d321f9048e340e7
Lou Huang <lou@louhuang.com>
2021-06-17 15:17:41 -0400

    test(EnvironmentEditor): add and update tests

--- assets/scripts/streets/__tests__/EnvironmentEditor.test.js
+++ assets/scripts/streets/__tests__/EnvironmentEditor.test.js
@@ -1,7 +1,14 @@
 /* eslint-env jest */
 import React from 'react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { render } from '../../../../test/helpers/render'
+import * as uiSlice from '../../store/slices/ui'
 import EnvironmentEditor from '../EnvironmentEditor'
+
+// jest.mock('../../store/slices/ui', () => ({
+//   toggleToolbox: jest.fn(() => ({ type: 'MOCK_ACTION' }))
+// }))
 
 describe('EnvironmentEditor', () => {
   const initialState = {
@@ -10,10 +17,17 @@ describe('EnvironmentEditor', () => {
     },
     ui: {
       toolboxVisible: true
+    },
+    user: {
+      signInData: {
+        details: {
+          roles: ['SUBSCRIBER_1']
+        }
+      }
     }
   }
 
-  it('renders', () => {
+  it('renders for subscribers', () => {
     const { asFragment } = render(<EnvironmentEditor />, {
       initialState
     })
@@ -21,5 +35,36 @@ describe('EnvironmentEditor', () => {
   })
 
   it.todo('selects an environment')
-  it.todo('closes when close button is clicked')
+
+  it('closes when close button is clicked', () => {
+    render(<EnvironmentEditor />, { initialState })
+
+    // Mock the single action creator to test if it's called
+    // We cannot mock the entire module because it prevents the test store
+    // from being created (the slice is responsible for both store and actions)
+    // ESLint does not like that we are assigning to the imported function
+    // so disable it so that we don't see that warning
+    // The action creator itself doesn't do anything but it must still return
+    // a plain object or the dispatch() throws an error
+    // eslint-disable-next-line
+    uiSlice.toggleToolbox = jest.fn().mockReturnValue({ type: 'MOCK_ACTION' })
+
+    waitFor(() => {
+      userEvent.click(screen.getByTitle('Dismiss'))
+    })
+
+    expect(uiSlice.toggleToolbox).toBeCalled()
+  })
+
+  it('shows upgrade prompt for unsubscribed users', () => {
+    render(<EnvironmentEditor />, {
+      initialState: {
+        ...initialState,
+        user: {}
+      }
+    })
+    expect(screen.queryByText('Upgrade to unlock')).toBeInTheDocument()
+  })
+
+  it.todo('does not select an environment for unsubscribed users')
 })
