27466e939259fd2c12028462d37ee58bc4899c8b
Lou Huang <lou@louhuang.com>
2021-07-11 10:27:03 -0400

    feat(csp_report): silent treatment on known blocks

--- app/resources/services/csp_report.js
+++ app/resources/services/csp_report.js
@@ -2,6 +2,22 @@ const chalk = require('chalk')
 const logger = require('../../../lib/logger.js')()
 
 exports.post = (req, res) => {
+  const cspReport = req.body.cspReport
+
+  // Early exit if a POST did not contain the report body
+  if (!cspReport) {
+    res.status(400).end()
+    return
+  }
+
+  // Some scripts are intentionally blocked. When that's the case we silence
+  // the report instead of logging it. This might be expanded to handle
+  // multiple use cases abstractly, but for now, we handle cases specifically
+  if (cspReport['blocked-uri'] === 'https://platform.twitter.com') {
+    res.status(204).end()
+    return
+  }
+
   logger.warn(
     '[csp-report] 🚨 ' +
       chalk.yellowBright.bold(
