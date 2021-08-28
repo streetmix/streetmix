73a1f140abd650e3e40e62c7f5efa04ce06ee64e
Lou Huang <lou@louhuang.com>
2021-08-28 13:48:45 -0400

    test: jest should use stock test .env

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -1,9 +1,14 @@
 /* eslint-env jest */
+import path from 'path'
 import '@testing-library/jest-dom/extend-expect'
 import fetch from 'jest-fetch-mock'
 import 'jest-canvas-mock'
 import 'jest-date-mock'
 import LocalStorageMock from './__mocks__/LocalStorageMock'
+
+require('dotenv').config({
+  path: path.resolve(process.cwd(), '.env.test')
+})
 
 // Add mocks for global methods
 fetch.enableMocks()
@@ -16,6 +21,3 @@ global.confirm = jest.fn(() => true)
 // Add HTML methods not implemented in jsdom
 // https://github.com/jsdom/jsdom/issues/1695
 global.HTMLElement.prototype.scrollIntoView = jest.fn()
-
-process.env.PELIAS_API_KEY = process.env.PELIAS_API_KEY || 'ge-iampelias'
-process.env.PELIAS_HOST_NAME = 'dummy.pelias.com'
