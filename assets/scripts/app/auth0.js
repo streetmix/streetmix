12f7d5bcdd9b253b1d0195fbc5e6fc0529f445fe
Lou Huang <lou@louhuang.com>
2022-07-03 12:08:40 -0400

    chore: try deploying with auth0 removed

--- assets/scripts/app/auth0.js
+++ assets/scripts/app/auth0.js
@@ -1,10 +1,13 @@
-import { WebAuth } from 'auth0-js'
-import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './config'
+// import { WebAuth } from 'auth0-js'
+// import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './config'
 
 export default function () {
-  return new WebAuth({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
-    scope: 'openid profile screen_name offline_access email'
-  })
+  return {
+    authorize: () => {}
+  }
+  // return new WebAuth({
+  //   domain: AUTH0_DOMAIN,
+  //   clientID: AUTH0_CLIENT_ID,
+  //   scope: 'openid profile screen_name offline_access email'
+  // })
 }
