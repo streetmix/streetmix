86518a7ef46ece6b2e61bb947a431cdc312f2efd
Lou Huang <lou@louhuang.com>
2022-08-05 22:00:12 -0400

    refactor: move sign out to axios api

--- app/resources/v1/user_session.js
+++ app/resources/v1/user_session.js
@@ -1,6 +1,8 @@
 const logger = require('../../../lib/logger.js')
 
 exports.delete = async function (req, res) {
+  // In order to sign out, make sure the user's session cookies
+  // are part of the request.
   if (!req.user) {
     res.status(400).json({ status: 400, msg: 'Must have user to logout.' })
     return
