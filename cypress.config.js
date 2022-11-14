cf7956804a17840ad75531da43ae755a833c6e6a
Lou Huang <lou@louhuang.com>
2022-11-13 21:02:21 -0500

    chore(cypress): upgrade to v10

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents (on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8000'
+  }
+})
