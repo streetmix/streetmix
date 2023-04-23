aa417d4b74cef60e1b56e43e567c7d31e2ccd789
Lou Huang <lou@streetmix.net>
2023-04-22 22:46:40 -0400

    Fix environment editor tooltip translations + updates tests (#2523)
    * fix(EnvironmentSelector): translate tooltips
    
    * feat(EnvironmentEditor): improve accessibility
    
    * test(EnvironmentEditor): update tests and un-todo a couple

--- assets/scripts/streets/EnvironmentSelector.jsx
+++ assets/scripts/streets/EnvironmentSelector.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useIntl } from 'react-intl'
+import { IntlProvider, useIntl } from 'react-intl'
+import { useSelector } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Tooltip from '../ui/Tooltip'
 import { ICON_LOCK } from '../ui/icons'
@@ -16,6 +17,7 @@ EnvironmentSelector.propTypes = {
 }
 
 function EnvironmentSelector ({ enabled, selected, handleSelect }) {
+  const locale = useSelector((state) => state.locale)
   const intl = useIntl()
 
   function handleClick (event, env) {
@@ -27,56 +29,59 @@ function EnvironmentSelector ({ enabled, selected, handleSelect }) {
   const envs = getAllEnvirons()
 
   return (
-    <div className="environment-selector">
-      {envs.map((env) => {
-        const { id, name, iconStyle } = env
-        const classNames = ['environment-item']
-        const label = intl.formatMessage({
-          id: `environs.${name}`,
-          defaultMessage: name
-        })
+    <IntlProvider locale={locale.locale} messages={locale.segmentInfo}>
+      <div className="environment-selector">
+        {envs.map((env) => {
+          const { id, name, iconStyle } = env
+          const classNames = ['environment-item']
+          const label = intl.formatMessage({
+            id: `environs.${id}`,
+            defaultMessage: name
+          })
 
-        if (selected === id) {
-          classNames.push('environment-active')
-        } else if (!selected && id === DEFAULT_ENVIRONS) {
-          classNames.push('environment-active')
-        }
+          if (selected === id) {
+            classNames.push('environment-active')
+          } else if (!selected && id === DEFAULT_ENVIRONS) {
+            classNames.push('environment-active')
+          }
 
-        if (!enabled && selected !== id) {
-          classNames.push('environment-disabled')
-        }
+          if (!enabled && selected !== id) {
+            classNames.push('environment-disabled')
+          }
 
-        return (
-          <Tooltip label={label} key={id} placement="bottom">
-            <div
-              className={classNames.join(' ')}
-              style={iconStyle}
-              onClick={(event) => handleClick(event, env)}
-            >
-              {!enabled && selected !== id && (
-                <>
-                  <div className="environment-disabled-overlay" />
-                  <FontAwesomeIcon icon={ICON_LOCK} />
-                </>
-              )}
-              {env.iconImage && (
-                <img
-                  src={images.get(env.iconImage)?.src}
-                  alt=""
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    pointerEvents: 'none',
-                    userSelect: 'none'
-                  }}
-                  draggable={false}
-                />
-              )}
-            </div>
-          </Tooltip>
-        )
-      })}
-    </div>
+          return (
+            <Tooltip label={label} key={id} placement="bottom">
+              <button
+                aria-label={label}
+                className={classNames.join(' ')}
+                style={iconStyle}
+                onClick={(event) => handleClick(event, env)}
+              >
+                {!enabled && selected !== id && (
+                  <>
+                    <div className="environment-disabled-overlay" />
+                    <FontAwesomeIcon icon={ICON_LOCK} />
+                  </>
+                )}
+                {env.iconImage && (
+                  <img
+                    src={images.get(env.iconImage)?.src}
+                    alt=""
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      pointerEvents: 'none',
+                      userSelect: 'none'
+                    }}
+                    draggable={false}
+                  />
+                )}
+              </button>
+            </Tooltip>
+          )
+        })}
+      </div>
+    </IntlProvider>
   )
 }
 
