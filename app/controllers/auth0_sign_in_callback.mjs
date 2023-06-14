9eda78fc6a3fcb629cd61bddc67037d4ff990856
Lou Huang <lou@streetmix.net>
2023-06-14 09:29:47 -0400

    chore(auth0): debug logging for login requests

--- app/controllers/auth0_sign_in_callback.mjs
+++ app/controllers/auth0_sign_in_callback.mjs
@@ -6,10 +6,12 @@ import appURL from '../lib/url.mjs'
 const AccessTokenHandler = function (req, res) {
   return async (response) => {
     const body = response.data
+
     if (body.error && body.error === 'access_denied') {
       res.redirect('/error/access-denied')
       return
     }
+
     const auth0 = Authentication()
 
     try {
@@ -17,7 +19,6 @@ const AccessTokenHandler = function (req, res) {
       const idToken = body.id_token
       const accessToken = body.access_token
       const user = await auth0.getProfile(accessToken)
-
       const apiRequestBody = getUserInfo(user)
       const endpoint = `${appURL.origin}/api/v1/users`
       const apiRequestOptions = {
@@ -33,19 +34,24 @@ const AccessTokenHandler = function (req, res) {
           const userAuthData = apiRequestBody.auth0_twitter
             ? apiRequestBody.auth0_twitter.screenName
             : apiRequestBody.auth0.nickname
+          const cookieOptions = {
+            maxAge: 9000000000,
+            sameSite: 'strict'
+          }
 
-          const cookieOptions = { maxAge: 9000000000, sameSite: 'strict' }
           res.cookie('user_id', user.id || userAuthData, cookieOptions)
           res.cookie('refresh_token', refreshToken, cookieOptions)
           res.cookie('login_token', idToken, cookieOptions)
           res.redirect('/services/auth/just-signed-in')
         })
         .catch((error) => {
-          logger.error('Error from auth0 API when signing in: ' + error)
+          logger.error(
+            '[auth0] Error from auth0 API when signing in: ' + error
+          )
           res.redirect('/error/authentication-api-problem')
         })
     } catch (error) {
-      logger.error('Error obtaining user info from Auth0: ' + error)
+      logger.error('[auth0] Error obtaining user info from Auth0: ' + error)
       res.redirect('/error/no-access-token')
     }
   }
@@ -86,35 +92,32 @@ const getUserTwitterAuth0Info = function (user) {
 }
 
 export function get (req, res) {
+  logger.info('[auth0] Logging in user with data:', req.query)
+
   if (req.query.error) {
-    logger.error('Auth0 encountered an error: ' + req.query.error)
+    logger.error('[auth0] Auth0 encountered an error: ' + req.query.error)
     res.redirect('/error/access-denied')
     return
   }
 
-  const code = req.query.code
-
-  const redirectUri = `${appURL.origin}/services/auth/sign-in-callback`
-
   const tokenUrl = `https://${process.env.AUTH0_DOMAIN}/oauth/token`
-  const options = {
-    headers: { 'content-type': 'application/json' },
-    json: true
-  }
-
   const body = {
     grant_type: 'authorization_code',
     client_id: process.env.AUTH0_CLIENT_ID,
     client_secret: process.env.AUTH0_CLIENT_SECRET,
-    code,
-    redirect_uri: redirectUri
+    code: req.query.code,
+    redirect_uri: `${appURL.origin}/services/auth/sign-in-callback`
+  }
+  const options = {
+    headers: { 'content-type': 'application/json' },
+    json: true
   }
 
   axios
     .post(tokenUrl, body, options)
     .then(AccessTokenHandler(req, res))
     .catch((err) => {
-      logger.error('Error obtaining access token from Auth0: ' + err)
+      logger.error('[auth0] Error obtaining access token from Auth0: ' + err)
       res.redirect('/error/no-access-token')
     })
 }
