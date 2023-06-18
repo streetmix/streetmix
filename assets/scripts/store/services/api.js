568fd1d5a2b6297064f5411eea63030ad54b61f0
Lou Huang <lou@streetmix.net>
2023-06-17 20:55:11 -0400

    chore(rtk-query): update comment

--- assets/scripts/store/services/api.js
+++ assets/scripts/store/services/api.js
@@ -1,7 +1,9 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 // Normally, `fetchBaseQuery` can be exported from '@reduxjs/toolkit/query/react'
 // but it must be exported from this package path for Parcel's production
-// bundle. See https://github.com/reduxjs/redux-toolkit/issues/3533 and others.
+// bundle. See https://github.com/reduxjs/redux-toolkit/issues/3533.
+// This may be a Parcel issue, filed here:
+// https://github.com/parcel-bundler/parcel/issues/7622#issuecomment-1027569976
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 export const streetmixApi = createApi({
