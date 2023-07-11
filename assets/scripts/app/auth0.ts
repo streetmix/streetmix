64b894734f1c0e537c75693ec13bad77a6902fc1
Lou Huang <lou@streetmix.net>
2023-07-10 22:07:30 -0400

    chore: add types for auth0

--- assets/scripts/app/auth0.ts
+++ assets/scripts/app/auth0.ts
@@ -3,8 +3,8 @@ import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from './config'
 
 export default function () {
   return new WebAuth({
-    domain: AUTH0_DOMAIN,
-    clientID: AUTH0_CLIENT_ID,
+    domain: AUTH0_DOMAIN ?? '',
+    clientID: AUTH0_CLIENT_ID ?? '',
     scope: 'openid profile screen_name offline_access email'
   })
 }
