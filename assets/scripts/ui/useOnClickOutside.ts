6d4cde839d82fb5d3f8c824d2010eeeb33135a92
Lou Huang <lou@streetmix.net>
2023-09-01 17:26:44 -0400

    refactor(useClickOnOutside): convert to ts

--- assets/scripts/ui/useOnClickOutside.ts
+++ assets/scripts/ui/useOnClickOutside.ts
@@ -10,12 +10,15 @@
  */
 import { useEffect } from 'react'
 
-export function useOnClickOutside (ref, handler) {
+export function useOnClickOutside (
+  ref: React.RefObject<HTMLElement> | React.MutableRefObject<HTMLElement>,
+  handler: (event: MouseEvent | TouchEvent) => void
+): void {
   useEffect(
     () => {
-      const listener = (event) => {
+      const listener = (event: MouseEvent | TouchEvent): void => {
         // Do nothing if clicking ref's element or descendent elements
-        if (!ref.current || ref.current.contains(event.target)) {
+        if (!ref.current || ref.current.contains(event.target as HTMLElement)) {
           return
         }
 
@@ -30,14 +33,12 @@ export function useOnClickOutside (ref, handler) {
         document.removeEventListener('touchstart', listener)
       }
     },
-
     // Add ref and handler to effect dependencies
-    // It's worth noting that because passed in handler is a new ...
-    // ... function on every render that will cause this effect ...
-    // ... callback/cleanup to run every render. It's not a big deal ...
-    // ... but to optimize you can wrap handler in useCallback before ...
-    // ... passing it into this hook.
-
+    // It's worth noting that because passed in handler is a new
+    // function on every render that will cause this effect
+    // callback/cleanup to run every render. It's not a big deal
+    // but to optimize you can wrap handler in useCallback before
+    // passing it into this hook.
     [ref, handler]
   )
 }
