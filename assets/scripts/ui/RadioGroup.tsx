a347106b5fe454b6cb7c2c742586277554120a70
Lou Huang <lou@streetmix.net>
2023-09-14 23:08:07 -0400

    refactor(RadioGroup): port to ts

--- assets/scripts/ui/RadioGroup.tsx
+++ assets/scripts/ui/RadioGroup.tsx
@@ -1,31 +1,28 @@
 import React, { useRef } from 'react'
-import PropTypes from 'prop-types'
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
 import './RadioGroup.scss'
 
 // This stores an incrementing number for unique IDs.
 let idCounter = 1
 
-RadioGroup.propTypes = {
+interface RadioGroupProps {
   // Class name applied to root switch element. Will be combined with
   // the default `switch-root` class name.
-  className: PropTypes.string,
+  className?: string
 
   // An `id` is associates a `label` with an `input` element. If you don't
   // provide one, the component automatically generates a unique ID. IDs
   // are "for internal use only".
-  id: PropTypes.string,
+  id?: string
 
   // Values are array of objects that specify what the options should be
-  values: PropTypes.arrayOf(
-    PropTypes.shape({
-      value: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      sublabel: PropTypes.string,
-      disabled: PropTypes.bool,
-      required: PropTypes.bool
-    })
-  )
+  values: Array<{
+    value: string
+    label: string
+    sublabel?: string
+    disabled?: boolean
+    required?: boolean
+  }>
 
   // Note:
   // For all other accepted props, see the Radix UI documentation at
@@ -33,15 +30,25 @@ RadioGroup.propTypes = {
   // Use Radix root API to get/set values and handle changes, for example.
 }
 
-function RadioGroup (props) {
-  const { id, className, values, ...restProps } = props
+interface RadioGroupItemProps {
+  id: string
+  value: string
+  label: string
+  sublabel?: string
+  disabled?: boolean
+  required?: boolean
+}
+
+function RadioGroup (props: RadioGroupProps): React.ReactElement {
+  const { id, className = '', values, ...restProps } = props
 
   // An `id` associates a `label` with the Radix UI RadioGroup component.
   // You can provide one manually in props, otherwise, this component
   // will generate a unique id value for each instance. Generated ids
   // are not meant to be accessed by other code or CSS selectors.
   const elementId = useRef(id)
-  if (!elementId.current) {
+  const idValue = elementId.current ?? `radio-group-id-${idCounter++}`
+  if (elementId.current === undefined) {
     // This exists in an if statement to check if the ref value is present
     // to prevent the counter from incrementing on every render
     elementId.current = `radio-group-id-${idCounter++}`
@@ -56,8 +63,8 @@ function RadioGroup (props) {
     <RadioGroupPrimitive.Root className={classNames.join(' ')} {...restProps}>
       {values.map((props, i) => (
         <RadioGroupItem
-          key={`${elementId.current}_${i}`}
-          id={`${elementId.current}_${i}`}
+          key={`${idValue}_${i}`}
+          id={`${idValue}_${i}`}
           {...props}
         />
       ))}
@@ -65,23 +72,14 @@ function RadioGroup (props) {
   )
 }
 
-RadioGroupItem.propTypes = {
-  id: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  sublabel: PropTypes.string,
-  disabled: PropTypes.bool,
-  required: PropTypes.bool
-}
-
 function RadioGroupItem ({
   id,
   value,
   label,
-  sublabel,
+  sublabel = '',
   disabled = false,
   required = false
-}) {
+}: RadioGroupItemProps): React.ReactElement {
   return (
     <div className="radio-group-item">
       <RadioGroupPrimitive.Item
