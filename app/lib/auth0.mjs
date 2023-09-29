5d09abd0b689f79ff413ceb2310ecc790dd1bb78
Lou Huang <lou@streetmix.net>
2023-09-29 17:04:02 -0400

    refactor(auth): update for v4

--- app/lib/auth0.mjs
+++ app/lib/auth0.mjs
@@ -1,9 +1,11 @@
-import auth0 from 'auth0'
+import { AuthenticationClient, UserInfoClient } from 'auth0'
 
-export const Authentication = function (scope) {
-  return new auth0.AuthenticationClient({
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET
-  })
-}
+export const authClient = new AuthenticationClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET
+})
+
+export const userClient = new UserInfoClient({
+  domain: process.env.AUTH0_DOMAIN
+})
