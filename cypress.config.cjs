3f996effaf9591b4bd964b107c4144c116f2a2ce
Lou Huang <lou@streetmix.net>
2024-02-20 15:16:09 -0500

    Fix ESM requirements and commitlint package version diffs (#2902)
    * fix(package): explicitly declare this is esm now
    
    * chore(package): bump @commitlint/cli, group updates
    
    * chore(package): update browserslist
    
    * chore(cypress): remove deprecated plugin config
    
    * chore(package): bump cypress
    
    * chore(cypress): remove outdatd information from README
    
    * fix(cypress): change config back to cjs because of esm incompatibility

---
+++ cypress.config.cjs
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+const dotenv = require('dotenv')
+
+// eslint-disable-next-line import/no-named-as-default-member
+dotenv.config()
+
+module.exports = defineConfig({
+  projectId: '2bmjk3',
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: false,
+  e2e: {
+    setupNodeEvents (on, config) {
+      config.env.PELIAS_HOST_NAME = process.env.PELIAS_HOST_NAME
+
+      return config
+    },
+    baseUrl: 'http://localhost:8000'
+  }
+})
