8f0159f540a4a5b8cd4f3e820d766ca543bd29c4
Lou Huang <lou@streetmix.net>
2024-03-01 10:47:49 -0500

    refactor(StreetMetaDate): port to ts

--- assets/scripts/streets/StreetMetaDate.tsx
+++ assets/scripts/streets/StreetMetaDate.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { IoTimeOutline } from 'react-icons/io5'
+import { useSelector } from '../store/hooks'
 import DateTimeRelative from '../app/DateTimeRelative'
 import { isOwnedByCurrentUser } from './owner'
 
 // Milliseconds that must have elapsed before recent edits display timestamp
 const TIME_LIMIT = 6000
 
-function StreetMetaDate (props) {
+function StreetMetaDate (): React.ReactElement | null {
   const updatedAt = useSelector(
-    (state) => state.street.updatedAt || state.street.clientUpdatedAt
+    (state) => state.street.updatedAt ?? state.street.clientUpdatedAt
   )
 
-  if (!updatedAt) return null
+  if (updatedAt === undefined) return null
 
   // In the user has just edited the street, don't display the "seconds ago" timestamp.
-  if (new Date() - new Date(updatedAt) < TIME_LIMIT && isOwnedByCurrentUser()) {
-    return null
+  if (isOwnedByCurrentUser()) {
+    const now = new Date().getTime()
+    const updated = new Date(updatedAt).getTime()
+    if (now - updated < TIME_LIMIT) return null
   }
 
   return (
