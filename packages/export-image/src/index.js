56ed2f4e9f293a6f89b45b6df1b848293e7e3387
Lou Huang <lou@streetmix.net>
2024-04-12 17:31:32 -0400

    feat(canvas): test svg rendering to canvas

--- packages/export-image/src/index.js
+++ packages/export-image/src/index.js
@@ -1,7 +1,13 @@
+import path from 'node:path'
+import url from 'node:url'
 import Canvas from '@napi-rs/canvas'
 
+// Set up some legacy Node.js globals for convenience
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export async function runTestCanvas () {
-  const canvas = Canvas.createCanvas(200, 200)
+  const canvas = Canvas.createCanvas(800, 800)
   const ctx = canvas.getContext('2d')
 
   // Write "Awesome!"
@@ -17,16 +23,28 @@ export async function runTestCanvas () {
   ctx.lineTo(50 + text.width, 102)
   ctx.stroke()
 
-  // Draw cat with lime helmet
-  // loadImage('examples/images/lime-cat.jpg').then((image) => {
-  //   ctx.drawImage(image, 50, 0, 70, 70)
+  // Reset context rotation from previous
+  ctx.rotate(-0.1)
 
-  //   console.log('<img src="' + canvas.toDataURL() + '" />')
-  // })
+  // Test wordmark
+  try {
+    // Must assemble path with __dirname to read from filesystem
+    const wordmark = await Canvas.loadImage(
+      path.join(__dirname, './wordmark_black.svg')
+    )
+
+    // Set the width and height here to scale properly
+    wordmark.width = wordmark.width * 4
+    wordmark.height = wordmark.height * 4
+
+    // Draw the image; don't use the dw and dh arguments for scaling, it will
+    // be pixelated. That's why we set the width/height properties earlier.
+    ctx.drawImage(wordmark, 30, 30)
+  } catch (err) {
+    console.error(err)
+  }
 
   const buffer = await canvas.encode('png')
-
-  // TODO: insert metadata
 
   return buffer
 }
