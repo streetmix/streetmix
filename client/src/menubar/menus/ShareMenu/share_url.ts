171bd6bd3f453e67007e3b753f29bab56b0fea92
Lou Huang <lou@streetmix.net>
2024-05-06 16:01:14 -0400

    chore(ShareMenu): move share_url and convert to ts

--- client/src/menubar/menus/ShareMenu/share_url.ts
+++ client/src/menubar/menus/ShareMenu/share_url.ts
@@ -2,6 +2,6 @@
  * Returns the current page URL.
  * This may eventually change to a short URL generator - strt.mx
  */
-export function getSharingUrl () {
+export function getSharingUrl (): string {
   return window.location.href
 }
