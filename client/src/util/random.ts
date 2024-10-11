705c8ab309bbc195dd0b316e3a0bddb73ef0c854
Lou Huang <lou@streetmix.net>
2024-10-11 10:26:25 -0400

    fix(nanoid): workaround for Parcel tree-shaking

--- client/src/util/random.ts
+++ client/src/util/random.ts
@@ -1,6 +1,8 @@
-import { nanoid } from 'nanoid'
+// Importing * instead of `import { nanoid }` is a workaround to force Parcel
+// not to tree-shake this library, which can lead to build failures
+import * as nanoid from 'nanoid'
 
 // Returns a random string for seeding.
 export function generateRandSeed (): string {
-  return nanoid()
+  return nanoid.nanoid()
 }
