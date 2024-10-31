0f474118b2737325b4330f29c8412fe958e28486
Lou Huang <lou@streetmix.net>
2024-10-31 16:49:12 -0400

    feat(db): update street id to UUID datatype

---
+++ app/db/migrations/20241031205836-update-street-id-uuid-datatype.cjs
@@ -0,0 +1,30 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(`
+      ALTER TABLE "Streets"
+      ALTER COLUMN id
+      TYPE uuid USING id::uuid
+    `)
+    // Sequelize will refuse to run the `changeColumn` command because, and
+    // I quote, `ERROR: column "id" is in a primary key`. So we just run the
+    // bare query above instead.
+    // return queryInterface.changeColumn('Streets', 'id', {
+    //   type: Sequelize.DataTypes.UUID
+    // })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.sequelize.query(`
+      ALTER TABLE "Streets"
+      ALTER COLUMN id
+      TYPE varchar(255)
+    `)
+    // Sequelize will refuse to run the `changeColumn` command -- see above.
+    // return queryInterface.changeColumn('Streets', 'id', {
+    //   type: Sequelize.DataTypes.STRING
+    // })
+  }
+}
