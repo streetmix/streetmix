5e8ca21d1049fdd5a92aac9fe95dc728f7f6abc9
Lou Huang <lou@streetmix.net>
2025-03-05 13:47:58 -0500

    refactor(types): add monetization api to types

--- client/src/types/global.d.ts
+++ client/src/types/global.d.ts
@@ -7,3 +7,19 @@ declare module '*.png' {
   const value: string
   export default value
 }
+
+// Add an optional `monetization` property on Document when Web Monetization
+// API is supported or present.
+declare global {
+  interface Document {
+    monetization?: Monetization
+  }
+}
+
+// This is bare-bones.
+// See also: https://github.com/dacioromero/types-wm
+// and https://webmonetization.org/
+// `document.monetization` has been implemented as an unmounted `<div>` element
+export interface Monetization extends HTMLDivElement {
+  state: 'stopped' | 'pending' | 'started'
+}
