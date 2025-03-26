8aac3adce35a6fff059362edfac1a59bae4d0325
Lou Huang <lou@streetmix.net>
2025-03-26 13:35:44 -0400

    refactor(SegmentDragLayer): function component + hooks

--- client/src/segments/SegmentDragLayer.jsx
+++ client/src/segments/SegmentDragLayer.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { DragLayer } from 'react-dnd'
+import React, { useEffect, useRef } from 'react'
+import { useDragLayer } from 'react-dnd'
+
+import { usePrevious } from '~/src/util/usePrevious'
 import SegmentCanvas from './SegmentCanvas'
 import { Types } from './drag_and_drop'
 import './SegmentDragLayer.css'
@@ -8,26 +9,27 @@ import './SegmentDragLayer.css'
 const DRAG_OFFSET_Y_PALETTE = -340 - 150
 const MAX_DRAG_DEGREE = 20
 
-class SegmentDragLayer extends React.PureComponent {
-  static propTypes = {
-    isDragging: PropTypes.bool.isRequired,
-    currentOffset: PropTypes.shape({
-      x: PropTypes.number,
-      y: PropTypes.number
-    }),
-    item: PropTypes.object,
-    type: PropTypes.string
-  }
+function SegmentDragLayer () {
+  const floatingEl = useRef(null)
+  const collectedProps = useDragLayer((monitor) => ({
+    item: monitor.getItem(),
+    type: monitor.getItemType(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging()
+  }))
+  const { item, type, currentOffset, isDragging } = collectedProps
+  const prevProps = usePrevious(collectedProps)
 
-  floatingEl = React.createRef()
+  function getDegree (currentOffset) {
+    const prevOffset = prevProps.currentOffset
 
-  getSnapshotBeforeUpdate (prevProps) {
-    const { currentOffset } = this.props
-    const prevMouseX = prevProps.currentOffset && prevProps.currentOffset.x
+    let deg
+    if (currentOffset === null || prevOffset === null) {
+      deg = 0
+    } else {
+      deg = currentOffset.x - prevOffset.x
+    }
 
-    if (!currentOffset || !prevMouseX) return null
-
-    let deg = currentOffset.x - prevMouseX
     if (deg > MAX_DRAG_DEGREE) {
       deg = MAX_DRAG_DEGREE
     } else if (deg < -MAX_DRAG_DEGREE) {
@@ -37,53 +39,35 @@ class SegmentDragLayer extends React.PureComponent {
     return deg
   }
 
-  componentDidUpdate (prevProps, prevState, snapshot) {
-    if (this.props.isDragging) {
-      this.getSegmentStyle(snapshot)
-    }
-  }
-
-  getSegmentStyle = (deg = 0) => {
-    const { currentOffset, item } = this.props
-
-    if (!currentOffset || !this.floatingEl.current) return
+  function applySegmentStyle (deg = 0) {
+    if (currentOffset === null || floatingEl === null) return
 
     let { x, y } = currentOffset
-
-    if (this.props.type === Types.PALETTE_SEGMENT) {
+    if (type === Types.PALETTE_SEGMENT) {
       x -= item.actualWidth * 4 // TODO: document magic number, probably TILE_SIZE * PALETTE_MULTIPLIER
       y += DRAG_OFFSET_Y_PALETTE
     }
 
     const transform = `translate(${x}px, ${y}px) rotateZ(${deg}deg)`
-    this.floatingEl.current.style.transform = transform
-    this.floatingEl.current.style['-webkit-transform'] = transform
+    floatingEl.current.style.transform = transform
+    floatingEl.current.style['-webkit-transform'] = transform
   }
 
-  render () {
-    const { isDragging, item, type } = this.props
+  useEffect(() => {
+    if (isDragging) {
+      applySegmentStyle(getDegree(currentOffset))
+    }
+  }, [currentOffset, isDragging])
 
-    return (
-      <div className="segment-drag-layer">
-        {/* Ignore the drag state unless it's a Streetmix segment */}
-        {isDragging &&
-          (type === Types.SEGMENT || type === Types.PALETTE_SEGMENT) && (
-            <div className="floating segment" ref={this.floatingEl}>
-              <SegmentCanvas randSeed={item.id} {...item} />
-            </div>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div className="segment-drag-layer">
+      {isDragging && (
+        <div className="floating segment" ref={floatingEl}>
+          <SegmentCanvas randSeed={item.id} {...item} />
+        </div>
+      )}
+    </div>
+  )
 }
 
-function collect (monitor) {
-  return {
-    item: monitor.getItem(),
-    type: monitor.getItemType(),
-    currentOffset: monitor.getSourceClientOffset(),
-    isDragging: monitor.isDragging()
-  }
-}
-
-export default DragLayer(collect)(SegmentDragLayer)
+export default SegmentDragLayer
