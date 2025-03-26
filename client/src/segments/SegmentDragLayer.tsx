38bf176cc9ed8bbcaf63a1df31364f84e9a4e67f
Lou Huang <lou@streetmix.net>
2025-03-26 15:55:28 -0400

    refactor SegmentDragLayer component (#3381)
    * refactor(SegmentDragLayer): function component + hooks
    
    * refactor(SegmentDragLayer): port to typescript
    
    * refactor(SegmentDragLayer): resolve todos in CSS

---
+++ client/src/segments/SegmentDragLayer.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from 'react'
+import { useDragLayer } from 'react-dnd'
+
+import { usePrevious } from '~/src/util/usePrevious'
+import SegmentCanvas from './SegmentCanvas'
+import { Types } from './drag_and_drop'
+import './SegmentDragLayer.css'
+
+const DRAG_OFFSET_Y_PALETTE = -340 - 150 // TODO: Document magic numbers
+const MAX_DRAG_DEGREE = 20
+
+function SegmentDragLayer (): React.ReactElement {
+  const floatingEl = useRef<HTMLDivElement>(null)
+  const collectedProps = useDragLayer((monitor) => ({
+    item: monitor.getItem(),
+    type: monitor.getItemType(),
+    currentOffset: monitor.getSourceClientOffset(),
+    isDragging: monitor.isDragging()
+  }))
+  const { item, type, currentOffset, isDragging } = collectedProps
+  const prevProps = usePrevious(collectedProps)
+
+  function getDegree (
+    currentOffset: typeof collectedProps.currentOffset
+  ): number {
+    const prevOffset = prevProps?.currentOffset ?? null
+
+    let deg
+    if (currentOffset === null || prevOffset === null) {
+      deg = 0
+    } else {
+      deg = currentOffset.x - prevOffset.x
+    }
+
+    if (deg > MAX_DRAG_DEGREE) {
+      deg = MAX_DRAG_DEGREE
+    } else if (deg < -MAX_DRAG_DEGREE) {
+      deg = -MAX_DRAG_DEGREE
+    }
+
+    return deg
+  }
+
+  function applySegmentStyle (deg = 0): void {
+    if (currentOffset === null || floatingEl.current === null) return
+
+    let { x, y } = currentOffset
+    if (type === Types.PALETTE_SEGMENT) {
+      x -= item.actualWidth * 4 // TODO: document magic number, probably TILE_SIZE * PALETTE_MULTIPLIER
+      y += DRAG_OFFSET_Y_PALETTE
+    }
+
+    floatingEl.current.style.transform = `translate(${x}px, ${y}px) rotateZ(${deg}deg)`
+  }
+
+  useEffect(() => {
+    if (isDragging) {
+      applySegmentStyle(getDegree(currentOffset))
+    }
+  }, [currentOffset, isDragging])
+
+  return (
+    <div className="segment-drag-layer">
+      {isDragging && (
+        <div className="floating segment" ref={floatingEl}>
+          <SegmentCanvas randSeed={item.id} {...item} />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default SegmentDragLayer
