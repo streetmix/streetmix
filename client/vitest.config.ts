6370f71bde7bcb7fbbd0948125c20f2a41c3d912
Lou Huang <lou@streetmix.net>
2025-04-19 09:33:18 -0400

    test: support yaml import in vitest

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import ViteYaml from '@modyfi/vite-plugin-yaml'
 
 export default defineConfig({
+  plugins: [ViteYaml()],
   test: {
     name: 'client',
     globals: true,
