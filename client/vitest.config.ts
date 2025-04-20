f7c491f513dd4dae415082ef316db1bb128214a9
Lou Huang <lou@streetmix.net>
2025-04-19 21:06:42 -0400

    chore(vitest): incremental analysis of breaking changes

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -15,12 +15,16 @@ export default defineConfig({
       },
       { find: '~', replacement: '/' }
     ],
+    include: [
+      '../client/src/app/**/*.test.?(c|m)[jt]s?(x)',
+      '../client/src/dialogs/**/*.test.?(c|m)[jt]s?(x)'
+    ]
     // Vitest v2 changes the default pool from `threads` to `forks`,
     // which feels like it will fix situations where test suite hangs
     // and runs forever in background.
     // https://github.com/vitest-dev/vitest/pull/5047
     // We can't update to Vitest v2 right now, but we can pull this
     // change forward
-    pool: 'forks'
+    // pool: 'forks'
   }
 })
