920c8eb53704fd0c4e5078bbc644542272e76480
Lou Huang <lou@streetmix.net>
2025-04-20 01:11:49 -0400

    chore(vitest): clean up config

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -20,23 +20,6 @@ export default defineConfig({
         find: '~',
         replacement: path.resolve(__dirname, './')
       }
-    ],
-    include: [
-      './src/app/**/*.test.?(c|m)[jt]s?(x)',
-      // './src/dialogs/**/*.test.?(c|m)[jt]s?(x)',
-      './src/sky/**/*.test.?(c|m)[jt]s?(x)',
-      './src/store/**/*.test.?(c|m)[jt]s?(x)',
-      './src/streets/**/*.test.?(c|m)[jt]s?(x)',
-      './src/ui/**/*.test.?(c|m)[jt]s?(x)',
-      './src/users/**/*.test.?(c|m)[jt]s?(x)',
-      './src/util/**/*.test.?(c|m)[jt]s?(x)'
     ]
-    // Vitest v2 changes the default pool from `threads` to `forks`,
-    // which feels like it will fix situations where test suite hangs
-    // and runs forever in background.
-    // https://github.com/vitest-dev/vitest/pull/5047
-    // We can't update to Vitest v2 right now, but we can pull this
-    // change forward
-    // pool: 'forks'
   }
 })
