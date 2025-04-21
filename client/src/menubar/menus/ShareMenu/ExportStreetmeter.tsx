4a5d98de5f9e550c10e3d7b871aad86950bc067e
Lou Huang <lou@streetmix.net>
2025-04-21 10:21:36 -0400

    feat(Streetmeter): add menu icon

--- client/src/menubar/menus/ShareMenu/ExportStreetmeter.tsx
+++ client/src/menubar/menus/ShareMenu/ExportStreetmeter.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import Icon from '~/src/ui/Icon'
 import MenuItem from '../MenuItem'
 import BetaTag from '../BetaTag'
 
 function ExportStreetmeter (): React.ReactElement {
   return (
     <MenuItem href={`https://streetmeter.net/#${window.location.href}`}>
+      <Icon name="graph" className="menu-item-icon" />
       Open in Streetmeter <BetaTag />
     </MenuItem>
   )
