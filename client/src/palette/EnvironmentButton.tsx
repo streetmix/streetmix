8ebd3c403786f62cb99651d8d5056e94f13d60c2
Lou Huang <lou@streetmix.net>
2025-05-05 15:31:58 -0400

    refactor(EnvironmentButton): upgrade tooltip

--- client/src/palette/EnvironmentButton.tsx
+++ client/src/palette/EnvironmentButton.tsx
@@ -5,7 +5,7 @@ import { useIntl } from 'react-intl'
 import { toggleToolbox } from '../store/slices/ui'
 import Button from '../ui/Button'
 import Icon from '../ui/Icon'
-import Tooltip from '../ui/TooltipLegacy'
+import { Tooltip } from '../ui/Tooltip'
 
 function EnvironmentButton (): React.ReactElement {
   const dispatch = useDispatch()
