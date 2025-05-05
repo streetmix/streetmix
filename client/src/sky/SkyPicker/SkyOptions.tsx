8a34539c7f0e05788a1d922e46c0edd5d94cdb10
Lou Huang <lou@streetmix.net>
2025-05-05 16:07:07 -0400

    refactor: update some tooltips

--- client/src/sky/SkyPicker/SkyOptions.tsx
+++ client/src/sky/SkyPicker/SkyOptions.tsx
@@ -3,7 +3,7 @@ import { IntlProvider, useIntl } from 'react-intl'
 
 import { useSelector } from '~/src/store/hooks'
 import Icon from '~/src/ui/Icon'
-import Tooltip from '~/src/ui/TooltipLegacy'
+import { Tooltip, TooltipGroup } from '~/src/ui/Tooltip'
 import { images } from '~/src/app/load_resources'
 import { DEFAULT_SKYBOX } from '../constants'
 import { getAllSkyboxDefs } from '..'
@@ -39,57 +39,59 @@ function SkyOptions ({
   return (
     <IntlProvider locale={locale.locale} messages={locale.segmentInfo}>
       <div className="sky-options">
-        {envs.map((env) => {
-          const { id, name, iconStyle } = env
-          const classNames = ['sky-option-item']
-          const label = intl.formatMessage({
-            id: `skybox.${id}`,
-            defaultMessage: name
-          })
+        <TooltipGroup>
+          {envs.map((env) => {
+            const { id, name, iconStyle } = env
+            const classNames = ['sky-option-item']
+            const label = intl.formatMessage({
+              id: `skybox.${id}`,
+              defaultMessage: name
+            })
 
-          if (selected === id) {
-            classNames.push('sky-selected')
-          } else if (!selected && id === DEFAULT_SKYBOX) {
-            classNames.push('sky-selected')
-          }
+            if (selected === id) {
+              classNames.push('sky-selected')
+            } else if (!selected && id === DEFAULT_SKYBOX) {
+              classNames.push('sky-selected')
+            }
 
-          if (!enabled && selected !== id) {
-            classNames.push('sky-disabled')
-          }
+            if (!enabled && selected !== id) {
+              classNames.push('sky-disabled')
+            }
 
-          return (
-            <Tooltip label={label} key={id} placement="bottom">
-              <button
-                aria-label={label}
-                className={classNames.join(' ')}
-                style={iconStyle}
-                onClick={(event) => {
-                  handleClick(event, env)
-                }}
-              >
-                {!enabled && selected !== id && (
-                  <>
-                    <div className="sky-disabled-overlay" />
-                    <Icon name="lock" />
-                  </>
-                )}
-                {env.iconImage !== undefined && (
-                  <img
-                    src={images.get(env.iconImage)?.src}
-                    alt=""
-                    style={{
-                      width: '100%',
-                      height: '100%',
-                      pointerEvents: 'none',
-                      userSelect: 'none'
-                    }}
-                    draggable={false}
-                  />
-                )}
-              </button>
-            </Tooltip>
-          )
-        })}
+            return (
+              <Tooltip label={label} key={id} placement="bottom">
+                <button
+                  aria-label={label}
+                  className={classNames.join(' ')}
+                  style={iconStyle}
+                  onClick={(event) => {
+                    handleClick(event, env)
+                  }}
+                >
+                  {!enabled && selected !== id && (
+                    <>
+                      <div className="sky-disabled-overlay" />
+                      <Icon name="lock" />
+                    </>
+                  )}
+                  {env.iconImage !== undefined && (
+                    <img
+                      src={images.get(env.iconImage)?.src}
+                      alt=""
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        pointerEvents: 'none',
+                        userSelect: 'none'
+                      }}
+                      draggable={false}
+                    />
+                  )}
+                </button>
+              </Tooltip>
+            )
+          })}
+        </TooltipGroup>
       </div>
     </IntlProvider>
   )
