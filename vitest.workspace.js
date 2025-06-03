5069a8e8ead5604e702cf2a12c1056f6b4c576d2
Lou Huang <lou@streetmix.net>
2025-06-03 13:54:24 -0400

    chore(vitest): replace project config

--- vitest.workspace.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="vitest" />
-import { defineWorkspace } from 'vitest/config'
-
-export default defineWorkspace([
-  'client/vitest.config.ts',
-  'packages/utils/vitest.config.ts'
-])
