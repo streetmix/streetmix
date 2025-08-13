664300fee7f9e92b233cf64fb826917c434ebaa5
Lou Huang <lou@streetmix.net>
2025-08-13 16:54:07 -0400

    feat(VariantButton): extract component

---
+++ client/src/info_bubble/PopupControls/VariantButton.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { useIntl } from 'react-intl'
+
+import { useSelector } from '~/src/store/hooks'
+import VARIANT_ICONS from '~/src/segments/variant_icons.yaml'
+import Button from '~/src/ui/Button'
+import Icon from '~/src/ui/Icon'
+import { Tooltip } from '~/src/ui/Tooltip'
+
+interface VariantButtonProps {
+  set: string
+  selection: string
+  isSelected: boolean
+  onClick: () => void
+}
+
+export function VariantButton (
+  props: VariantButtonProps
+): React.ReactElement | null {
+  const { set, selection, isSelected, onClick } = props
+  const flags = useSelector((state) => state.flags)
+  const isSignedIn = useSelector((state) => state.user.signedIn)
+  const isSubscriber = useSelector((state) => state.user.isSubscriber)
+  const intl = useIntl()
+
+  const icon = VARIANT_ICONS[set][selection]
+
+  if (icon === undefined) return null
+
+  // If a variant is disabled by feature flag, skip it
+  if (icon.enableWithFlag !== undefined) {
+    const flag = flags[icon.enableWithFlag]
+    if (!flag?.value) return null
+  }
+
+  const label = intl.formatMessage({
+    id: `variant-icons.${set}|${selection}`,
+    defaultMessage: icon.title
+  })
+
+  let isLocked = false
+  let sublabel
+
+  // If there is an enable condition, add a note to the tooltip if it
+  // is locked for a reason (e.g. must sign in, must be a subscriber)
+  // If an "unlock flag" is set, enable the thing
+  if (
+    icon.unlockCondition !== undefined &&
+    !(icon.unlockWithFlag !== undefined && flags[icon.unlockWithFlag]?.value)
+  ) {
+    switch (icon.unlockCondition) {
+      case 'SUBSCRIBE':
+        if (!isSubscriber) {
+          isLocked = true
+          sublabel = intl.formatMessage({
+            id: 'plus.locked.sub',
+            // Default message ends with a Unicode-only left-right order mark
+            // to allow for proper punctuation in `rtl` text direction
+            // This character is hidden from editors by default!
+            defaultMessage: 'Upgrade to Streetmix+ to use!‎'
+          })
+        }
+        break
+      case 'SIGN_IN':
+      default:
+        if (!isSignedIn) {
+          isLocked = true
+          sublabel = intl.formatMessage({
+            id: 'plus.locked.user',
+            // Default message ends with a Unicode-only left-right order mark
+            // to allow for proper punctuation in `rtl` text direction
+            // This character is hidden from editors by default!
+            defaultMessage: 'Sign in to use!‎'
+          })
+        }
+        break
+    }
+  }
+
+  return (
+    <Tooltip
+      label={label}
+      sublabel={sublabel}
+      placement="bottom"
+      key={set + '.' + selection}
+    >
+      <Button
+        data-testid={icon.title}
+        className={isSelected ? 'variant-selected' : undefined}
+        disabled={isSelected || isLocked}
+        onClick={onClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/1999/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          className="icon"
+          style={icon.color !== undefined ? { fill: icon.color } : undefined}
+        >
+          <use href={`#icon-${icon.id}`} />
+        </svg>
+        {isLocked && <Icon name="lock" />}
+      </Button>
+    </Tooltip>
+  )
+}
--- client/src/info_bubble/PopupControls/Variants.tsx
+++ client/src/info_bubble/PopupControls/Variants.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useIntl } from 'react-intl'
 
 import { useSelector, useDispatch } from '~/src/store/hooks'
 import {
@@ -10,9 +9,7 @@ import { segmentsChanged } from '~/src/segments/view'
 import { getSegmentInfo } from '~/src/segments/info'
 import VARIANT_ICONS from '~/src/segments/variant_icons.yaml'
 import { getVariantInfo } from '~/src/segments/variant_utils'
-import Button from '~/src/ui/Button'
-import Icon from '~/src/ui/Icon'
-import { Tooltip } from '~/src/ui/Tooltip'
+import { VariantButton } from './VariantButton'
 
 import type { SectionElementTypeAndPosition } from '@streetmix/types'
 
@@ -36,24 +33,14 @@ function Variants (
 
     return null
   })
-  const flags = useSelector((state) => state.flags)
-  const isSignedIn = useSelector((state) => state.user.signedIn)
-  const isSubscriber = useSelector((state) => state.user.isSubscriber)
   const dispatch = useDispatch()
-  const intl = useIntl()
 
   let variantSets: string[] = []
-  switch (type) {
-    case 'slice': {
-      const { variants } = getSegmentInfo(segment.type)
-      variantSets = variants
-      break
-    }
-    case 'boundary':
-      variantSets = Object.keys(VARIANT_ICONS.building)
-      break
-    default:
-      break
+  if (type === 'boundary') {
+    variantSets = Object.keys(VARIANT_ICONS.building)
+  } else {
+    const { variants } = getSegmentInfo(segment.type)
+    variantSets = variants
   }
 
   // Remove any empty entries
@@ -62,20 +49,13 @@ function Variants (
   function isVariantCurrentlySelected (set: string, selection: string): boolean {
     let bool = false
 
-    switch (type) {
-      case 'slice': {
-        if (segment) {
-          const obj = getVariantInfo(segment.type, variant)
-          bool = selection === obj[set as keyof typeof obj]
-        }
-        break
+    if (type === 'boundary') {
+      bool = selection === variant
+    } else {
+      if (segment) {
+        const obj = getVariantInfo(segment.type, variant)
+        bool = selection === obj[set as keyof typeof obj]
       }
-      case 'boundary':
-        bool = selection === variant
-        break
-      default:
-        bool = false
-        break
     }
 
     return bool
@@ -84,117 +64,32 @@ function Variants (
   function getButtonOnClickHandler (set: string, selection: string): () => void {
     let handler
 
-    switch (type) {
-      case 'slice':
-        handler = () => {
-          dispatch(changeSegmentVariant(position, set, selection))
-          segmentsChanged()
-        }
-        break
-      case 'boundary':
-        handler = () => {
-          dispatch(setBuildingVariant(position, selection))
-        }
-        break
-      default:
-        handler = () => {}
-        break
+    if (type === 'boundary') {
+      handler = () => {
+        dispatch(setBuildingVariant(position, selection))
+      }
+    } else {
+      handler = () => {
+        dispatch(changeSegmentVariant(position, set, selection))
+        segmentsChanged()
+      }
     }
 
     return handler
   }
 
-  function renderButton (
-    set: string,
-    selection: string
-  ): React.ReactElement | null {
-    const icon = VARIANT_ICONS[set][selection]
-
-    if (icon === undefined) return null
-
-    // If a variant is disabled by feature flag, skip it
-    if (icon.enableWithFlag !== undefined) {
-      const flag = flags[icon.enableWithFlag]
-      if (!flag?.value) return null
-    }
-
-    const label = intl.formatMessage({
-      id: `variant-icons.${set}|${selection}`,
-      defaultMessage: icon.title
-    })
-
-    let isLocked = false
-    let sublabel
-
-    // If there is an enable condition, add a note to the tooltip if it
-    // is locked for a reason (e.g. must sign in, must be a subscriber)
-    // If an "unlock flag" is set, enable the thing
-    if (
-      icon.unlockCondition !== undefined &&
-      !(icon.unlockWithFlag !== undefined && flags[icon.unlockWithFlag]?.value)
-    ) {
-      switch (icon.unlockCondition) {
-        case 'SUBSCRIBE':
-          if (!isSubscriber) {
-            isLocked = true
-            sublabel = intl.formatMessage({
-              id: 'plus.locked.sub',
-              // Default message ends with a Unicode-only left-right order mark
-              // to allow for proper punctuation in `rtl` text direction
-              // This character is hidden from editors by default!
-              defaultMessage: 'Upgrade to Streetmix+ to use!‎'
-            })
-          }
-          break
-        case 'SIGN_IN':
-        default:
-          if (!isSignedIn) {
-            isLocked = true
-            sublabel = intl.formatMessage({
-              id: 'plus.locked.user',
-              // Default message ends with a Unicode-only left-right order mark
-              // to allow for proper punctuation in `rtl` text direction
-              // This character is hidden from editors by default!
-              defaultMessage: 'Sign in to use!‎'
-            })
-          }
-          break
-      }
-    }
-
-    const isSelected = isVariantCurrentlySelected(set, selection)
-
-    return (
-      <Tooltip
-        label={label}
-        sublabel={sublabel}
-        placement="bottom"
-        key={set + '.' + selection}
-      >
-        <Button
-          data-testid={icon.title}
-          className={isSelected ? 'variant-selected' : undefined}
-          disabled={isSelected || isLocked}
-          onClick={getButtonOnClickHandler(set, selection)}
-        >
-          <svg
-            xmlns="http://www.w3.org/1999/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            className="icon"
-            style={icon.color !== undefined ? { fill: icon.color } : undefined}
-          >
-            <use href={`#icon-${icon.id}`} />
-          </svg>
-          {isLocked && <Icon name="lock" />}
-        </Button>
-      </Tooltip>
-    )
-  }
-
-  function renderVariantSet (set: string, items: string[]): React.ReactElement {
+  function renderButtonGroup (set: string, items: string[]): React.ReactElement {
     return (
       <div className="popup-control-button-group">
-        {items.map((selection) => renderButton(set, selection))}
+        {items.map((selection) => (
+          <VariantButton
+            set={set}
+            selection={selection}
+            isSelected={isVariantCurrentlySelected(set, selection)}
+            onClick={getButtonOnClickHandler(set, selection)}
+            key={set + '.' + selection}
+          />
+        ))}
       </div>
     )
   }
@@ -204,10 +99,10 @@ function Variants (
     | React.ReactElement
     | null {
     if (type === 'boundary') {
-      return renderVariantSet('building', variantSets)
+      return renderButtonGroup('building', variantSets)
     } else {
       return variantSets.map((set) =>
-        renderVariantSet(set, Object.keys(VARIANT_ICONS[set]))
+        renderButtonGroup(set, Object.keys(VARIANT_ICONS[set]))
       )
     }
   }
