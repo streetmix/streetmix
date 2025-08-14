566f79bee17453a9be2c1a62e9639e02d14aafc7
Lou Huang <lou@streetmix.net>
2025-08-14 15:38:41 -0400

    refactor(VariantButton): actually don't need that css

--- client/src/info_bubble/PopupControls/VariantButton.tsx
+++ client/src/info_bubble/PopupControls/VariantButton.tsx
@@ -6,7 +6,6 @@ import VARIANT_ICONS from '~/src/segments/variant_icons.yaml'
 import Button from '~/src/ui/Button'
 import Icon from '~/src/ui/Icon'
 import { Tooltip } from '~/src/ui/Tooltip'
-import './VariantButton.css'
 
 interface VariantButtonProps {
   set: string
