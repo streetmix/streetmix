b2ebbf6a21701e06072a8eee4f20482911dab83f
Lou Huang <lou@streetmix.net>
2025-10-30 05:05:59 -0400

    refactor(TestSlope): ground drawing implementation

--- client/src/segments/TestSlope.tsx
+++ client/src/segments/TestSlope.tsx
@@ -1,30 +1,21 @@
 import React, { useRef, useEffect } from 'react'
 
+import { images } from '~/src/app/load_resources'
 import { useSelector } from '~/src/store/hooks'
 import { CANVAS_HEIGHT, GROUND_BASELINE_HEIGHT, TILE_SIZE } from './constants'
 import './TestSlope.css'
 
 import { calculateSlope } from './slope'
 import { getElevation } from './view'
-import { getSegmentVariantInfo } from './info'
+import { getSegmentVariantInfo, getSpriteDef } from './info'
 import type { Segment } from '@streetmix/types'
 
 interface Props {
   slice: Segment
 }
 
-// TODO: maybe we don't hard-code these here, but it's enough to get started
-const GROUND_COLORS = {
-  sand: '#ECDBB1',
-  'asphalt-red': '#992025',
-  'asphalt-green': '#2E6550',
-  asphalt: '#292B29',
-  'asphalt-gray': '#5C5E5F',
-  concrete: '#D8D3CB'
-}
-
-function estimateCoord (elev: number, scale: number): number {
-  return getElevation(elev) * scale
+function getCanvasElevation (elev: number, scale: number): number {
+  return getElevation(elev) * scale + GROUND_BASELINE_HEIGHT * scale
 }
 
 function drawSegment (
@@ -37,46 +28,53 @@ function drawSegment (
   const ctx = canvas.getContext('2d')
   if (!ctx) return
 
-  // TODO: redefine magic number in a less hacky way
-  const magicNumber = dpi * GROUND_BASELINE_HEIGHT
-  const groundLevel =
-    magicNumber + estimateCoord(Math.min(leftElevation, rightElevation), dpi)
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
   const variantInfo = getSegmentVariantInfo(slice.type, slice.variantString)
   const graphics = variantInfo.graphics
   let ground
   if (Array.isArray(graphics.repeat)) {
     const found = graphics.repeat.find((id) => id.startsWith('ground--'))
-    console.log(found)
     ground = found
   } else {
     ground = graphics.repeat
   }
-  const texture = ground.split('--')[1]
+  const spriteDef = getSpriteDef(ground)
+  const spriteImage = images.get(spriteDef.id)
+  const pattern = ctx.createPattern(spriteImage.img, 'repeat')
 
-  // These rectangles are telling us that we're drawing at the right places.
-  ctx.fillStyle = GROUND_COLORS[texture]
-  // ctx.fillStyle = GROUND_COLORS.asphalt
-  ctx.strokeStyle = 'transparent'
-  ctx.lineWidth = 0
-  // Start at the bottom left corner and use a negative number to draw upwards
-  ctx.fillRect(0, canvas.height, canvas.width, groundLevel * -1)
-  ctx.strokeRect(0, canvas.height, canvas.width, groundLevel * -1)
+  // TODO: scale the pattern according to image scale
+  // This will only be important if we have patterns that aren't solid colors
+  // pattern.setTransform(new DOMMatrix().scale(1))
 
-  // Draw a slope
-  // TODO: move this to the function that draws other stuff
+  // Save context state before drawing ground pattern
+  ctx.save()
+
+  // Clear previous context
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  // Draw a shape representing the ground
   ctx.beginPath()
-  ctx.moveTo(0, canvas.height - groundLevel)
-  ctx.lineTo(0, canvas.height - estimateCoord(leftElevation, dpi) - magicNumber)
+  // Bottom left
+  ctx.moveTo(0, canvas.height)
+  // Top left
+  ctx.lineTo(0, canvas.height - getCanvasElevation(leftElevation, dpi))
+  // Top right
   ctx.lineTo(
     canvas.width,
-    canvas.height - estimateCoord(rightElevation, dpi) - magicNumber
+    canvas.height - getCanvasElevation(rightElevation, dpi)
   )
-  ctx.lineTo(canvas.width, canvas.height - groundLevel)
-  ctx.fill()
-  ctx.stroke()
+  // Bottom right
+  ctx.lineTo(canvas.width, canvas.height)
+  ctx.closePath()
+
+  // Clip our fill to this shape
+  ctx.clip()
+
+  // Then fill the clipped shape
+  ctx.fillStyle = pattern
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  // Restore context state
+  ctx.restore()
 }
 
 function TestSlope ({ slice }: Props): React.ReactNode | null {
@@ -93,11 +91,20 @@ function TestSlope ({ slice }: Props): React.ReactNode | null {
     if (slice.slope) {
       const { leftElevation, rightElevation } = slopeData
       drawSegment(canvasEl.current, slice, leftElevation, rightElevation, dpi)
+    } else {
+      // If not sloped, draw slice's inherent flat elevation
+      drawSegment(
+        canvasEl.current,
+        slice,
+        slice.elevation,
+        slice.elevation,
+        dpi
+      )
     }
   }, [slice, slopeData, dpi])
 
-  // Bail if slice is not sloped, or it has been removed
-  if (slice.slope !== true || slopeData === null) return null
+  // Bail if slice has been removed
+  if (slopeData === null) return null
 
   const { slope, ratio, warnings } = slopeData
 
