9bf2594b78365fa43838b6cb6ead23f4811e5c80
Lou Huang <lou@streetmix.net>
2025-11-03 10:28:39 -0500

    refactor(thumbnail): complete typescript refactor

--- client/src/streets/thumbnail.ts
+++ client/src/streets/thumbnail.ts
@@ -1,6 +1,3 @@
-/**
- * temporary file where the typescript porting happens
- */
 import { drawLine } from '@streetmix/export-image/src/labels'
 
 import { images } from '../app/load_resources'
@@ -21,7 +18,7 @@ import type {
   CSSGradientDeclaration,
   SkyboxDefWithStyles,
   SkyboxObject,
-  StreetJson
+  StreetState
 } from '@streetmix/types'
 
 const BOTTOM_BACKGROUND = 'rgb(216, 211, 203)'
@@ -49,9 +46,9 @@ const WORDMARK_MARGIN = 4
 /**
  * Draws sky.
  */
-export function drawSky (
+function drawSky (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
-  street: StreetJson, // street data
+  street: StreetState, // street data
   width: number, // width of area to draw
   height: number, // height of area to draw
   dpi: number, // pixel density of canvas
@@ -275,9 +272,9 @@ function drawClouds (
 /**
  * Draws ground.
  */
-export function drawGround (
+function drawGround (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
-  street: StreetJson, // street data
+  street: StreetState, // street data
   width: number, // width of area to draw
   dpi: number, // pixel density of canvas
   multiplier: number, // scale factor of image
@@ -322,9 +319,9 @@ export function drawGround (
 /**
  * Draws buildings.
  */
-export function drawBoundaries (
+function drawBoundaries (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
-  street: StreetJson, // street data
+  street: StreetState, // street data
   width: number, // width of area to draw
   dpi: number, // pixel density of canvas
   multiplier: number, // scale factor of image
@@ -390,9 +387,9 @@ export function drawBoundaries (
 /**
  * Draws segments.
  */
-export function drawSegments (
+function drawSegments (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
-  street: StreetJson, // street data
+  street: StreetState, // street data
   dpi: number, // pixel density of canvas
   multiplier: number, // scale factor of image
   groundLevel: number, // vertical height of ground
@@ -446,7 +443,7 @@ export function drawSegments (
 /**
  * Draws the segment names background.
  */
-export function drawSegmentNamesBackground (
+function drawSegmentNamesBackground (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
   width: number, // width of area to draw
   height: number, // height of area to draw
@@ -465,24 +462,16 @@ export function drawSegmentNamesBackground (
 
 /**
  * Draws segment names and widths.
- *
- * @param {CanvasRenderingContext2D} ctx -
- * @param {Number} dpi -
- * @param {Number} multiplier -
- * @param {Number} groundLevel -
- * @param {Number} offsetLeft -
- * @param {Number} locale -
- * @modifies {CanvasRenderingContext2D} ctx
  */
-export function drawSegmentNamesAndWidths (
+function drawSegmentNamesAndWidths (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
-  street: StreetJson,
+  street: StreetState,
   dpi: number, // pixel density of canvas
   multiplier: number, // scale factor of image
   groundLevel: number, // vertical height of ground
   offsetLeft: number, // left position to start from
   locale: string // locale to render labels in
-) {
+): void {
   ctx.save()
 
   ctx.strokeStyle = 'black'
@@ -548,7 +537,7 @@ export function drawSegmentNamesAndWidths (
 /**
  * Turns drawn objects on canvas into a single-colour silhouette
  */
-export function drawSilhouette (
+function drawSilhouette (
   ctx: CanvasRenderingContext2D, // the canvas context to draw on
   width: number, // width of area to draw
   height: number, // height of area to draw
@@ -559,4 +548,254 @@ export function drawSilhouette (
   ctx.fillStyle = SILHOUETTE_FILL_COLOUR
   ctx.fillRect(0, 0, width * dpi, height * dpi)
   ctx.restore()
+}
+
+/**
+ * Draws street nameplate
+ */
+function drawStreetNameplate (
+  ctx: CanvasRenderingContext2D, // the canvas context to draw on
+  street: StreetState, // street data
+  width: number, // width of area to draw
+  dpi: number // pixel density of canvas
+): void {
+  let text = street.name || formatMessage('street.default-name', 'Unnamed St')
+
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
+  ctx.letterSpacing = '-.025em'
+  ctx.font = `normal ${STREET_NAME_FONT_WEIGHT} ${
+    STREET_NAME_FONT_SIZE * dpi
+  }px ${STREET_NAME_FONT},sans-serif`
+
+  // Handles long names
+  let measurement = ctx.measureText(text)
+
+  let needToBeElided = false
+  while (measurement.width > (width - 200) * dpi) {
+    text = text.substring(0, text.length - 1)
+    measurement = ctx.measureText(text)
+    needToBeElided = true
+  }
+  if (needToBeElided) {
+    // Append ellipsis, then re-measure the text
+    text += '…'
+    measurement = ctx.measureText(text)
+  }
+
+  // Street nameplate
+  ctx.fillStyle = 'white'
+  const x1 = (width * dpi) / 2 - (measurement.width / 2 + 45 * dpi)
+  const x2 = (width * dpi) / 2 + (measurement.width / 2 + 45 * dpi)
+  const y1 = (75 - 60) * dpi
+  const y2 = (75 + 60) * dpi
+  ctx.fillRect(x1, y1, x2 - x1, y2 - y1)
+
+  // Street nameplate border
+  ctx.strokeStyle = 'black'
+  ctx.lineWidth = 5 * dpi
+  ctx.strokeRect(
+    x1 + 5 * dpi * 2,
+    y1 + 5 * dpi * 2,
+    x2 - x1 - 5 * dpi * 4,
+    y2 - y1 - 5 * dpi * 4
+  )
+
+  const x = (width * dpi) / 2
+
+  const baselineCorrection = 12
+  const y = (75 + baselineCorrection) * dpi
+
+  ctx.strokeStyle = 'transparent'
+  ctx.fillStyle = 'black'
+  ctx.fillText(text, x, y)
+}
+
+/**
+ * Draws a "made with Streetmix" watermark on the lower right of the image.
+ */
+function drawWatermark (
+  ctx: CanvasRenderingContext2D, // the canvas context to draw on
+  dpi: number, // pixel density of canvas
+  invert = false // if `true`, render light text for dark background
+): void {
+  const text = formatMessage(
+    'export.watermark',
+    'Made with {streetmixWordmark}',
+    {
+      // Replace the {placeholder} with itself. Later, this is used to
+      // render the logo image in place of the text.
+      streetmixWordmark: '{streetmixWordmark}'
+    }
+  )
+  const wordmarkImage = invert
+    ? images.get('/images/wordmark_white.svg')
+    : images.get('/images/wordmark_black.svg')
+
+  // Separate string so that we can render a wordmark with an image
+  const strings = text.replace(/{/g, '||{').replace(/}/g, '}||').split('||')
+
+  // Set text render options
+  ctx.textAlign = 'right'
+  ctx.textBaseline = 'alphabetic'
+  ctx.font = `normal ${WATERMARK_FONT_WEIGHT} ${
+    WATERMARK_FONT_SIZE * dpi
+  }px ${WATERMARK_FONT},sans-serif`
+  ctx.fillStyle = invert ? WATERMARK_LIGHT_COLOR : WATERMARK_DARK_COLOR
+
+  // Set starting X/Y positions so that watermark is aligned right and bottom of image
+  const startRightX = ctx.canvas.width - WATERMARK_RIGHT_MARGIN * dpi
+  const startBottomY = ctx.canvas.height - WATERMARK_BOTTOM_MARGIN * dpi
+
+  // Set wordmark width and height based on image scale (dpi)
+  const logoWidth = wordmarkImage.width * dpi
+  const logoHeight = wordmarkImage.height * dpi
+
+  // Keep track of where we are on the X-position.
+  let currentRightX = startRightX
+
+  // Render each part of the string.
+  for (let i = strings.length - 1; i >= 0; i--) {
+    const string = strings[i]
+
+    // If we see the wordmark placeholder, render the image.
+    if (string === '{streetmixWordmark}') {
+      const margin = WORDMARK_MARGIN * dpi
+      const logoLeftX = currentRightX - logoWidth - margin
+      const logoTopY = startBottomY - logoHeight + dpi // Additional adjustment for visual alignment
+
+      ctx.drawImage(
+        wordmarkImage.img,
+        logoLeftX,
+        logoTopY,
+        logoWidth,
+        logoHeight
+      )
+
+      // Update X position.
+      currentRightX = logoLeftX - margin
+    } else {
+      ctx.fillText(string, currentRightX, startBottomY)
+
+      // Update X position.
+      currentRightX = currentRightX - ctx.measureText(string).width
+    }
+  }
+}
+
+interface ThumbnailOptions {
+  width: number
+  height: number
+  dpi: number
+  multiplier: number
+  silhouette: boolean
+  transparentSky: boolean
+  segmentNamesAndWidths: boolean
+  streetName: boolean
+  watermark: boolean
+  locale: string
+}
+
+/**
+ * Draws street image to 2D canvas element.
+ */
+export function drawStreetThumbnail (
+  ctx: CanvasRenderingContext2D, // 2D canvas context to draw on
+  street: StreetState, // Street data
+  {
+    width, // Width of resulting image
+    height, // Height of resulting image
+    dpi, // Pixel density of canvas
+    multiplier, // Scale factor of image
+    silhouette,
+    transparentSky, // If `true`, image is a silhouette
+    segmentNamesAndWidths, // If `true`, include segment names and widths
+    streetName, // If `true`, include street nameplate
+    watermark = true, // If `true`, include Streetmix watermark
+    locale = 'en'
+  }: ThumbnailOptions
+): void {
+  // Calculations
+
+  // Determine how wide the street is
+  let occupiedWidth = 0
+  for (const segment of street.segments) {
+    occupiedWidth += segment.width
+  }
+
+  // Align things to bottom edge of image
+  let offsetTop = height - 180 * multiplier
+  if (segmentNamesAndWidths) {
+    offsetTop -= SAVE_AS_IMAGE_NAMES_WIDTHS_PADDING * multiplier
+  }
+
+  const offsetLeft = (width - occupiedWidth * TILE_SIZE * multiplier) / 2
+  const buildingOffsetLeft = (width - street.width * TILE_SIZE * multiplier) / 2
+
+  const groundLevel = offsetTop + 135 * multiplier
+  const horizonLine = groundLevel + 20 * multiplier
+
+  // Sky
+  if (!transparentSky) {
+    drawSky(
+      ctx,
+      street,
+      width,
+      height,
+      dpi,
+      multiplier,
+      horizonLine,
+      groundLevel
+    )
+  }
+
+  // Ground
+  drawGround(ctx, street, width, dpi, multiplier, horizonLine, groundLevel)
+
+  // Buildings
+  drawBoundaries(
+    ctx,
+    street,
+    width,
+    dpi,
+    multiplier,
+    groundLevel,
+    buildingOffsetLeft
+  )
+
+  // Segments
+  drawSegments(ctx, street, dpi, multiplier, groundLevel, offsetLeft)
+
+  // Segment names background
+  if (segmentNamesAndWidths || silhouette) {
+    drawSegmentNamesBackground(ctx, width, height, dpi, multiplier, groundLevel)
+  }
+
+  // Segment names
+  if (segmentNamesAndWidths) {
+    drawSegmentNamesAndWidths(
+      ctx,
+      street,
+      dpi,
+      multiplier,
+      groundLevel,
+      offsetLeft,
+      locale
+    )
+  }
+
+  // Silhouette
+  if (silhouette) {
+    drawSilhouette(ctx, width, height, dpi)
+  }
+
+  // Street nameplate
+  if (streetName) {
+    drawStreetNameplate(ctx, street, width, dpi)
+  }
+
+  // Watermark
+  if (watermark) {
+    drawWatermark(ctx, dpi, !segmentNamesAndWidths)
+  }
 }
