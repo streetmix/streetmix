e61b8c3edd929582a147917117934203248bfa1d
Lou Huang <lou@streetmix.net>
2025-11-18 12:12:35 -0500

    Bump dependencies (#3492)
    * chore(packages): bump depedencies
    
    * chore(vitest): no more triple slashes -- vitest globals are imported in tsconfig

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,5 +1,3 @@
-// Vitest recommends triple-slash reference up here, but this error is thrown:
-// Do not use a triple slash reference for vitest, use `import` style instead  @typescript-eslint/triple-slash-reference
 import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
