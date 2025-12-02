2cf72e52fc9501bacc8fa33c1347ff568def306a
Lou Huang <lou@streetmix.net>
2025-12-02 15:52:44 -0500

    test(KeyboardKey): remove unused

--- client/src/ui/KeyboardKey.test.tsx
+++ client/src/ui/KeyboardKey.test.tsx
@@ -20,18 +20,4 @@ describe('KeyboardKey', () => {
     expect(screen.getByText('foo').title).toBe('')
     expect(asFragment()).toMatchSnapshot()
   })
-
-  it('renders a <kbd> element with icon and title', async () => {
-    const { container, asFragment } = render(
-      <KeyboardKey icon={{ prefix: 'fas', iconName: 'minus' }}>foo</KeyboardKey>
-    )
-
-    const elementWithTitle = screen.getByTitle('foo')
-    const iconElement = container.querySelector('svg')
-
-    expect(elementWithTitle).toBeDefined()
-    expect(elementWithTitle.children).toHaveLength(1)
-    expect(iconElement?.children).toBeDefined()
-    expect(asFragment()).toMatchSnapshot()
-  })
 })
