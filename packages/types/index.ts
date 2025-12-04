0256ddf368859a21a70ea128bc470b6d9e0d0bbb
Lou Huang <lou@louhuang.com>
2025-12-04 18:15:55 -0500

    fix(types): make it a regular module not a definition file

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,7 +1,9 @@
-import type {
+import {
   SETTINGS_UNITS_IMPERIAL,
   SETTINGS_UNITS_METRIC,
-} from '@streetmix/client/src/users/constants'
+} from '@streetmix/client/src/users/constants.js'
+
+import type React from 'react'
 
 // Utility type for making a single property (K) optional
 // when a type (T) has defined K as required.
