2e4b33d152c2093a20b95c0bb96ec8ab143f1dd8
Lou Huang <lou@streetmix.net>
2025-12-14 00:14:53 -0500

    refactor(streets_export): use papaparse to build csv
    the main reason to do this is to outsource some responsibility, eg
    being able to handle escaping characters, etc. this does change some
    existing functionality (it now returns an empty file if a street has
    no remixes, as opposed to a file with just the header row) but we've
    never guaranteed this api was stable (and it has only been used a
    few times, and it isn't public)

--- app/lib/streets_export.js
+++ app/lib/streets_export.js
@@ -1,120 +1,67 @@
 import { round } from '@streetmix/utils'
+import Papa from 'papaparse'
 
 import appURL from './url.js'
 
 const IMPERIAL_CONVERSION_RATE = 0.3048
 const IMPERIAL_PRECISION = 3
 
-const camelToSnakeCase = (str) =>
-  str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
-
-function convertMetricMeasurementToImperial (value) {
+function convertMetricMeasurementToImperial(value) {
   if (value === undefined) return
   return round(value * IMPERIAL_CONVERSION_RATE, IMPERIAL_PRECISION)
 }
 
-export function streetsToCSV (json) {
-  const SEGMENT_REPLACE_HEADER = '<segments>'
+export function streetsToCSV(json) {
   const streets = json.streets
 
-  const maxSegmentCount = streets.reduce((value, street) => {
+  const maxSliceCount = streets.reduce((value, street) => {
     const length = street.data.street.segments.length
     if (length > value) return length
     else return value
   }, 1)
 
-  const headers = [
-    'id',
-    'name',
-    'creatorId',
-    'width',
-    'widthImperial',
-    'leftBoundaryVariant',
-    'leftBoundaryFloors',
-    SEGMENT_REPLACE_HEADER, // WILL BE REPLACED
-    'rightBoundaryVariant',
-    'rightBoundaryFloors',
-    'editCount',
-    'createdAt',
-    'url'
-  ]
-
-  let csv = ''
-
-  const segmentHeaders = []
-  for (let i = 0; i < maxSegmentCount; i++) {
-    segmentHeaders.push(`segment_${i}_type`)
-    segmentHeaders.push(`segment_${i}_variant`)
-    segmentHeaders.push(`segment_${i}_width`)
-    segmentHeaders.push(`segment_${i}_width_imperial`)
-    segmentHeaders.push(`segment_${i}_elevation`)
-  }
-
-  const segmentHeaderIndex = headers.indexOf(SEGMENT_REPLACE_HEADER)
-  const segmentHeaderString = segmentHeaders.join(',')
-  if (segmentHeaderIndex !== -1) {
-    headers[segmentHeaderIndex] = segmentHeaderString
-  }
-
-  csv += headers.map(camelToSnakeCase).join(',') + '\n'
-
-  const rows = streets.map((street, i) => {
-    const values = headers.map((header) => {
-      switch (header) {
-        case 'id':
-        case 'name':
-        case 'creatorId':
-        case 'createdAt':
-          return street[header] ?? ''
-        case 'leftBoundaryVariant':
-          return street.data.boundary.left.variant
-        case 'leftBoundaryFloors':
-          return street.data.boundary.left.floors
-        case 'rightBoundaryVariant':
-          return street.data.boundary.right.variant
-        case 'rightBoundaryFloors':
-          return street.data.boundary.left.floors
-        case 'editCount':
-          return street.data.street[header] ?? ''
-        case 'width':
-          return street.data.street.width
-        case 'widthImperial':
-          return convertMetricMeasurementToImperial(street.data.street.width)
-        case segmentHeaderString:
-          return getSegmentData(street.data.street.segments, maxSegmentCount)
-        case 'url':
-          return getStreetUrl(street)
-        default:
-          return ''
-      }
-    })
-    return values.join(',')
+  const rows = streets.map((street) => {
+    const slices = getSliceData(street.data.street.segments, maxSliceCount)
+    return {
+      id: street['id'] ?? '',
+      name: street['name'] ?? '',
+      creator_id: street['creatorId'] ?? '',
+      width: street.data.street.width,
+      widthImperial: convertMetricMeasurementToImperial(
+        street.data.street.width
+      ),
+      left_boundary_variant: street.data.street.boundary.left.variant,
+      left_boundary_floors: street.data.street.boundary.left.floors,
+      ...slices,
+      right_boundary_variant: street.data.street.boundary.right.variant,
+      right_boundary_floors: street.data.street.boundary.left.floors,
+      edit_count: street.data.street['editCount'] ?? '',
+      created_at: street['createdAt'] ?? '',
+      url: getStreetUrl(street),
+    }
   })
 
-  csv += rows.join('\n')
-
-  return csv
+  return Papa.unparse(rows)
 }
 
-function getSegmentData (segments = [], maxSegmentCount) {
-  const values = []
-  for (let i = 0; i < maxSegmentCount; i++) {
-    const segment = segments[i] ?? {}
-    values.push(
-      [
-        segment.type ?? '',
-        segment.variantString ?? '',
-        segment.width ?? '',
-        convertMetricMeasurementToImperial(segment.width) ?? '',
-        typeof segment.elevation !== 'undefined' ? segment.elevation : ''
-      ].join(',')
-    )
+function getSliceData(slices = [], maxSliceCount) {
+  const object = {}
+
+  for (let i = 0; i < maxSliceCount; i++) {
+    const slice = slices[i] ?? {}
+    object[`slice_${i}_type`] = slice.type ?? ''
+    object[`slice_${i}_variant`] = slice.variantString ?? ''
+    object[`slice_${i}_width`] = slice.width ?? ''
+    object[`slice_${i}_width_imperial`] =
+      convertMetricMeasurementToImperial(slice.width) ?? ''
+    object[`slice_${i}_elevation`] =
+      typeof slice.elevation !== 'undefined' ? slice.elevation : ''
   }
 
-  return values.join(',')
+  return object
 }
 
-function getStreetUrl (street) {
+function getStreetUrl(street) {
   let url = appURL.href
   if (street.creatorId) {
     url += street.creatorId
