2cad32d325a0fa2d3ea5e1a1f48fc2bf4134aa16
Lou Huang <lou@streetmix.net>
2026-02-25 11:53:48 -0500

    fix(templates): guard on null sign in data

--- client/src/streets/templates.ts
+++ client/src/streets/templates.ts
@@ -146,7 +146,7 @@ function createStreetData(data: StreetTemplate, units: UnitsSetting) {
   const currentDate = new Date().toISOString()
   const slices = processTemplateSlices(data.slices, units)
   const boundary = processTemplateBoundaries(data.boundary, units)
-  const creatorId = (isSignedIn() && getSignInData().userId) || null
+  const creatorId = (isSignedIn() && getSignInData()?.userId) || null
 
   // Remove `slices` from the existing data because it is being stored
   // as `segments` for backwards compatibility
