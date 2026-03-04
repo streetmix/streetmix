db9fce0ae50e16195e73e8b343dbbca6440f101d
Lou Huang <lou@streetmix.net>
2026-03-04 12:50:27 -0500

    feat(WelcomeCoastix): message tweak

--- client/src/app/WelcomePanel/WelcomeCoastmix.tsx
+++ client/src/app/WelcomePanel/WelcomeCoastmix.tsx
@@ -14,13 +14,13 @@ export function WelcomeCoastmix() {
       <h1>Welcome to Coastmix!</h1>
 
       <p style={{ textAlign: 'left' }}>
-        Welcome to Coastmix! This interactive tool lets you design coastal
-        waterfronts and explore ways to prevent flooding. Climate change and
-        global warming are causing sea level rise and more intense and frequent
-        coastal storms. Cities around the world must adapt their waterfronts to
-        prevent flooding to protect communities, infrastructure, buildings, and
-        more. With Coastmix, learn about coastal resilience planning and design
-        your own adaptation strategies.
+        This interactive tool lets you design coastal waterfronts and explore
+        ways to prevent flooding. Climate change and global warming are causing
+        sea level rise and more intense and frequent coastal storms. Cities
+        around the world must adapt their waterfronts to prevent flooding to
+        protect communities, infrastructure, buildings, and more. With Coastmix,
+        learn about coastal resilience planning and design your own adaptation
+        strategies.
       </p>
       <p style={{ textAlign: 'left' }}>Let’s get started!</p>
       <p style={{ textAlign: 'left' }}>
