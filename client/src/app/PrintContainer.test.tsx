fb2a0693632dd3eb23acdb3ac033e282a10bfffc
Lou Huang <lou@streetmix.net>
2026-03-26 18:13:24 -0400

    refactor(PrintContainer): modern syntax

--- client/src/app/PrintContainer.test.tsx
+++ client/src/app/PrintContainer.test.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
 import { vi } from 'vitest'
 import { screen } from '@testing-library/react'
 
-import { render } from '~/test/helpers/render'
-import PrintContainer from './PrintContainer'
+import { render } from '~/test/helpers/render.js'
+import { PrintContainer } from './PrintContainer.js'
 
-vi.mock('../streets/image', () => ({
+vi.mock('../streets/image.js', () => ({
   getStreetImage: () => ({
-    toDataURL: () => 'foo'
-  })
+    toDataURL: () => 'foo',
+  }),
 }))
 
 describe('PrintContainer', () => {
   it('renders', () => {
     const { asFragment } = render(<PrintContainer />, {
-      initialState: { app: { printing: false } }
+      initialState: { app: { printing: false } },
     })
     expect(asFragment()).toMatchSnapshot()
   })
 
   it('renders image for printing', () => {
     render(<PrintContainer />, {
-      initialState: { app: { printing: true } }
+      initialState: { app: { printing: true } },
     })
 
     expect(screen.getByRole('img')).toBeInTheDocument()
