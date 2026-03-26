d7f072c0eb5008fac963f87e4da19d24f2537bfa
Lou Huang <lou@streetmix.net>
2026-03-26 19:27:34 -0400

    refactor(UpgradeDialog): named export

--- client/src/dialogs/Upgrade/UpgradeDialog.tsx
+++ client/src/dialogs/Upgrade/UpgradeDialog.tsx
@@ -10,7 +10,7 @@ import './UpgradeDialog.css'
 const DEFAULT_BODY =
   'Thank you for using Streetmix! For only $5/month, the Enthusiast Plan lets users support Streetmix while also gaining access to new experimental features. Plus your avatar gets a neat badge!'
 
-function UpgradeDialog() {
+export function UpgradeDialog() {
   const userId = useSelector((state) => state.user.signInData?.userId)
   const roles: string[] = useSelector(
     (state) => state.user.signInData?.details?.roles ?? []
@@ -97,5 +97,3 @@ function UpgradeDialog() {
     </Dialog>
   )
 }
-
-export default UpgradeDialog
--- client/src/dialogs/Upgrade/index.ts
+++ client/src/dialogs/Upgrade/index.ts
@@ -1,1 +1,1 @@
-export { default } from './UpgradeDialog'
+export * from './UpgradeDialog.js'
