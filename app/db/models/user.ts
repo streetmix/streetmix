d09863dd6fddfb2712e76a8d123fa364ef3b2d7b
Lou Huang <lou@streetmix.net>
2026-03-31 15:59:36 -0400

    refactor(models/user): update how to type it

--- app/db/models/user.ts
+++ app/db/models/user.ts
@@ -1,3 +1,12 @@
+import {
+  DataTypes,
+  Model,
+  Sequelize,
+  type InferAttributes,
+  type InferCreationAttributes,
+  type CreationOptional,
+} from 'sequelize'
+
 import roles from '../../data/user_roles.json' with { type: 'json' }
 
 const validUserRoles = Object.keys(roles)
@@ -6,38 +15,66 @@ const validUserRoles = Object.keys(roles)
 a little atypical setup here...'id' is usually a unique primary key value
 but in this app it is actually the username of the user
 */
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id: string
+  declare auth0Id: CreationOptional<string>
+  declare displayName: CreationOptional<string>
+  declare email: CreationOptional<string>
+  declare identities: CreationOptional<unknown>
+  declare roles: CreationOptional<string[]>
+  declare profileImageUrl: CreationOptional<string>
+  declare flags: CreationOptional<unknown>
+  declare data: CreationOptional<unknown>
+  declare lastStreetId: CreationOptional<number>
+  declare createdAt: CreationOptional<Date>
+  declare updatedAt: CreationOptional<Date>
+
+  async addRole(newRole: string) {
+    if (!this.roles.includes(newRole)) {
+      const updatedRoles = [...this.roles, newRole]
+      this.roles = updatedRoles
+      await this.update({ roles: updatedRoles })
+    }
+  }
+
+  async removeRole(roleToRemove: string) {
+    const newRoles = this.roles.filter((item: string) => item !== roleToRemove)
+    this.roles = newRoles
+    await this.update({ roles: newRoles })
+  }
+
+  static associate(_models: unknown) {
+    // associations can be defined here
+  }
+}
+
+export default (sequelize: Sequelize, dataTypes: typeof DataTypes) => {
+  User.init(
     {
       id: {
         allowNull: false,
         primaryKey: true,
         unique: true,
-        type: DataTypes.STRING,
+        type: dataTypes.STRING,
       },
       auth0Id: {
-        type: DataTypes.STRING,
+        type: dataTypes.STRING,
         field: 'auth0_id',
       },
       displayName: {
-        type: DataTypes.STRING,
+        type: dataTypes.STRING,
         field: 'display_name',
         validate: {
           len: [0, 30],
         },
       },
-      email: { type: DataTypes.STRING, unique: true },
-      identities: DataTypes.JSON,
+      email: { type: dataTypes.STRING, unique: true },
+      identities: dataTypes.JSON,
       roles: {
-        type: DataTypes.ARRAY(DataTypes.TEXT),
+        type: dataTypes.ARRAY(dataTypes.TEXT),
         defaultValue: ['USER'],
         validate: {
-          /*
-          sequelize validator passes the userRoles value which is an array of what roles they have
-          for every value in the array, check if its valid against the set of valid roles
-          to do this we have a custom validator
-          */
+          // Check if user role is a valid one against a list of defined roles
           arrayIsValid(userRoles: string[]) {
             if (!userRoles.every((value) => validUserRoles.includes(value))) {
               throw new Error('Role does not match list of valid roles')
@@ -52,22 +89,22 @@ export default (sequelize, DataTypes) => {
         // characters, but a rare handful of googleusercontent.com URLs are
         // just slightly longer. Bumping to 2048 characters should prevent
         // SQL insert errors that block people from signing in.
-        type: DataTypes.STRING(2048),
+        type: dataTypes.STRING(2048),
         field: 'profile_image_url',
       },
-      flags: DataTypes.JSON,
-      data: DataTypes.JSON,
+      flags: dataTypes.JSON,
+      data: dataTypes.JSON,
       lastStreetId: {
-        type: DataTypes.INTEGER,
+        type: dataTypes.INTEGER,
         field: 'last_street_id',
       },
-      createdAt: { type: DataTypes.DATE, field: 'created_at' },
-      updatedAt: { type: DataTypes.DATE, field: 'updated_at' },
+      createdAt: { type: dataTypes.DATE, field: 'created_at' },
+      updatedAt: { type: dataTypes.DATE, field: 'updated_at' },
     },
     {
-      timestamp: true,
-    },
-    {
+      sequelize,
+      modelName: 'User',
+      timestamps: true,
       indexes: [
         {
           unique: true,
@@ -83,46 +120,6 @@ export default (sequelize, DataTypes) => {
       ],
     }
   )
-
-  /*
-  different users of streetmix can have different roles,
-  which can enable or disable certain features (see '../../data/user_roles.json')
-
-  We need to be able to:
-  * add roles
-
-  * remove roles
-  * check if a role _should_ be updated(added/removed) or not based on criteria(like a matching auth token from a given provider)
-*/
-
-  /*
-  https://sequelize.org/master/manual/model-basics.html#taking-advantage-of-models-being-classes
-  https://github.com/sequelize/sequelize/blob/4478d74a3e5dc8cd30837d8a193754867d06ccf5/docs/upgrade-to-v4.md#config--options
-
-  code examples:
-  // Class Method
-  Model.associate = function (models) {
-      ...associate the models
-  };
-
-  // Instance Method
-  Model.prototype.someMethod = function () {..}
-*/
-
-  User.prototype.addRole = function (newRole: string) {
-    if (!this.roles.includes(newRole)) {
-      this.update(this.roles.push(newRole))
-    }
-  }
-
-  User.prototype.removeRole = function (roleToRemove: string) {
-    const newRoles = this.roles.filter((item: string) => item !== roleToRemove)
-    this.update((this.roles = newRoles))
-  }
-
-  User.associate = function (_models) {
-    // associations can be defined here
-  }
 
   return User
 }
