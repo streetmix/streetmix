d35a9fdbd96aaf70f7a2d3d96c568a7caa3e671c
Lou Huang <lou@streetmix.net>
2026-04-22 15:29:09 +0200

    feat(CopyShareLink): show a tooltip before/after copying

--- client/src/menubar/menus/ShareMenu/CopyShareLink.tsx
+++ client/src/menubar/menus/ShareMenu/CopyShareLink.tsx
@@ -1,8 +1,10 @@
+import React, { useState } from 'react'
 import { FormattedMessage, useIntl } from 'react-intl'
 import copy from 'copy-to-clipboard'
 
 import { Button } from '~/src/ui/Button.js'
 import { Icon } from '~/src/ui/Icon.js'
+import { Tooltip } from '~/src/ui/Tooltip.js'
 
 interface CopyShareLinkProps {
   shareUrl: string
@@ -10,7 +12,38 @@ interface CopyShareLinkProps {
 }
 
 export function CopyShareLink({ shareUrl, ref }: CopyShareLinkProps) {
+  const [hasCopied, setHasCopied] = useState(false)
   const intl = useIntl()
+
+  const label = hasCopied
+    ? intl.formatMessage({
+        id: 'menu.share.copy-success',
+        defaultMessage: 'Copied!',
+      })
+    : intl.formatMessage({
+        id: 'menu.share.copy-to-clipboard',
+        defaultMessage: 'Copy to clipboard',
+      })
+
+  async function handleClick(_event: React.MouseEvent) {
+    try {
+      await copy(shareUrl)
+      setHasCopied(true)
+    } catch (err) {
+      console.error('clipboard copy error')
+    }
+  }
+
+  // Resets copied state after copying, and element loses focus.
+  // Do it after the tooltip animation timing so that text doesn't change when
+  // animating out
+  function resetState() {
+    if (hasCopied) {
+      window.setTimeout(() => {
+        setHasCopied(false)
+      }, 150)
+    }
+  }
 
   return (
     <div className="share-via-link-container">
@@ -28,18 +61,15 @@ export function CopyShareLink({ shareUrl, ref }: CopyShareLinkProps) {
           ref={ref}
           readOnly
         />
-        <Button
-          title={intl.formatMessage({
-            id: 'menu.share.copy-to-clipboard',
-            defaultMessage: 'Copy to clipboard',
-          })}
-          onClick={async (event) => {
-            event.preventDefault()
-            await copy(shareUrl)
-          }}
-        >
-          <Icon name="clipboard" size="16" />
-        </Button>
+        <Tooltip label={label}>
+          <Button
+            onClick={handleClick}
+            onBlur={resetState}
+            onMouseLeave={resetState}
+          >
+            <Icon name="clipboard" size="16" />
+          </Button>
+        </Tooltip>
       </div>
     </div>
   )
