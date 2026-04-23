75d3ece49a87b1cf5b91aa12c134cc19db51ff76
Lou Huang <lou@streetmix.net>
2026-04-23 08:55:17 +0200

    chore(coastmix): remove old button

--- client/src/plugins/coastmix/CoastmixUI.tsx
+++ client/src/plugins/coastmix/CoastmixUI.tsx
@@ -1,17 +1,10 @@
 import { CoastalFloodingPanel } from '~/src/plugins/coastmix'
 import { useSelector } from '~/src/store/hooks'
-// import { ShepherdTest } from '~src/ui/Tours/ShepherdTest.js'
 
 export function CoastmixUI() {
   const coastmixMode = useSelector((state) => state.flags.COASTMIX_MODE.value)
 
   if (!coastmixMode) return null
 
-  return (
-    <>
-      {/* <CoastalFloodingButton /> */}
-      <CoastalFloodingPanel />
-      {/* <ShepherdTest /> */}
-    </>
-  )
+  return <CoastalFloodingPanel />
 }
