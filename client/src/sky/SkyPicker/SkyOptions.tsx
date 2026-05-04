a65009220a56bf022cc75a30371f31b7fcddb91e
Lou Huang <lou@streetmix.net>
2026-05-04 15:53:50 -0400

    feat(SkyPicker): add heading for original sky picker

--- client/src/sky/SkyPicker/SkyOptions.tsx
+++ client/src/sky/SkyPicker/SkyOptions.tsx
@@ -1,11 +1,10 @@
-import { IntlProvider, useIntl } from 'react-intl'
+import { FormattedMessage, IntlProvider, useIntl } from 'react-intl'
 
 import { useSelector, useDispatch } from '~/src/store/hooks.js'
 import { setSkybox } from '~/src/store/slices/street.js'
 import { DEFAULT_SKYBOX } from '../constants.js'
 import { getAllSkyboxDefs } from '..'
 import { SkyOptionItem } from './SkyOptionItem.js'
-import './SkyOptions.css'
 
 interface SkyOptionsProps {
   enabled: boolean
@@ -14,6 +13,12 @@ interface SkyOptionsProps {
 export function SkyOptions({ enabled }: SkyOptionsProps) {
   const locale = useSelector((state) => state.locale)
   const selected = useSelector((state) => state.street.skybox ?? DEFAULT_SKYBOX)
+  const weatherEnabled = useSelector(
+    (state) => state.flags.WEATHER_EFFECTS?.value ?? false
+  )
+  const coastmixMode = useSelector(
+    (state) => state.flags.COASTMIX_MODE?.value ?? false
+  )
   const dispatch = useDispatch()
   const intl = useIntl()
   const envs = getAllSkyboxDefs()
@@ -24,33 +29,48 @@ export function SkyOptions({ enabled }: SkyOptionsProps) {
     }
   }
 
+  const showHeading = weatherEnabled || coastmixMode
+
   return (
     <IntlProvider locale={locale.locale} messages={locale.segmentInfo}>
-      <div className="sky-options">
-        {envs.map((env) => {
-          const { id, name, iconImage, iconStyle } = env
-          const label = intl.formatMessage({
-            id: `skybox.${id}`,
-            defaultMessage: name,
-          })
-
-          const isSelected =
-            selected === id || (!selected && id === DEFAULT_SKYBOX)
-
-          return (
-            <SkyOptionItem
-              key={id}
-              label={label}
-              iconImage={iconImage}
-              iconStyle={iconStyle}
-              isSelected={isSelected}
-              isUnlocked={enabled}
-              onClick={(_event) => {
-                handleSelect(id)
-              }}
+      <div
+        className="sky-options-group"
+        style={{ marginTop: showHeading ? '0' : undefined }}
+      >
+        {showHeading && (
+          <h4>
+            <FormattedMessage
+              id="tools.skybox.sky.heading"
+              defaultMessage="Sky"
             />
-          )
-        })}
+          </h4>
+        )}
+        <div className="sky-options">
+          {envs.map((env) => {
+            const { id, name, iconImage, iconStyle } = env
+            const label = intl.formatMessage({
+              id: `skybox.${id}`,
+              defaultMessage: name,
+            })
+
+            const isSelected =
+              selected === id || (!selected && id === DEFAULT_SKYBOX)
+
+            return (
+              <SkyOptionItem
+                key={id}
+                label={label}
+                iconImage={iconImage}
+                iconStyle={iconStyle}
+                isSelected={isSelected}
+                isUnlocked={enabled}
+                onClick={(_event) => {
+                  handleSelect(id)
+                }}
+              />
+            )
+          })}
+        </div>
       </div>
     </IntlProvider>
   )
