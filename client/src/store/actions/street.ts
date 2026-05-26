9bded2da64550fea579461efa80adf7129fb8b73
Lou Huang <lou@streetmix.net>
2026-05-26 15:54:00 -0400

    fix(undo): use updateStreetDataAction for undo/redo

--- client/src/store/actions/street.ts
+++ client/src/store/actions/street.ts
@@ -46,15 +46,24 @@ import type {
 } from '@streetmix/types'
 
 /**
- * updateStreetWidth as a thunk action that automatically
+ * `updateStreetWidth` as a thunk action that automatically
  * dispatches segmentChanged
- *
- * @param width
  */
 export function updateStreetWidthAction(width: number) {
   return async (dispatch: Dispatch) => {
     await dispatch(updateStreetWidth(width))
     await dispatch(segmentsChanged())
+  }
+}
+
+/**
+ * `updateStreetData` as a thunk action that also makes sure that
+ * `immediateRemoval` is true
+ */
+export function updateStreetDataAction(data: Partial<StreetState>) {
+  return async (dispatch: Dispatch) => {
+    await dispatch(setImmediateRemoval(true))
+    await dispatch(updateStreetData(data))
   }
 }
 
