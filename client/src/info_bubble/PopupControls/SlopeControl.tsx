9a5725a2075e713729dbbb14052441b3c380982e
Lou Huang <lou@streetmix.net>
2026-06-10 13:52:26 -0400

    Update Coastmix tour part 2 (#3688)
    * feat(tour): update practice tour + slope icons
    
    * refactor(tours): update practice tour

--- client/src/info_bubble/PopupControls/SlopeControl.tsx
+++ client/src/info_bubble/PopupControls/SlopeControl.tsx
@@ -1,6 +1,10 @@
+import { useIntl } from 'react-intl'
+
 import { useSelector } from '~/src/store/hooks.js'
+import { Icon } from '~/src/ui/Icon.js'
+import { Tooltip } from '~/src/ui/Tooltip.js'
 import { SlopeControlTemp } from './SlopeControlTemp.js'
-import { SlopeControlPin } from './SlopeControlPin.js'
+// import { SlopeControlPin } from './SlopeControlPin.js'
 
 interface SlopeControlProps {
   position: number
@@ -11,13 +15,26 @@ export function SlopeControl({ position }: SlopeControlProps) {
   const slope = useSelector((state) => {
     return state.street.segments[position].slope
   })
+  const intl = useIntl()
+
+  const leftLabel = intl.formatMessage({
+    id: 'segments.controls.slope.left-label',
+    defaultMessage: 'Left ground height',
+  })
+
+  const rightLabel = intl.formatMessage({
+    id: 'segments.controls.slope.right-label',
+    defaultMessage: 'Right ground height',
+  })
 
   return (
-    <>
-      <div className="popup-control-row" data-tour-id="elevation-control">
-        <div className="popup-control-label" style={{ marginRight: '0.25em' }}>
-          left
-        </div>
+    <div className="non-variant" data-tour-id="slope-control-group">
+      <div className="popup-control-row">
+        <Tooltip label={leftLabel} placement="left" role="label">
+          <span className="popup-control-icon">
+            <Icon name="slope-left" size="30" stroke="1.5" />
+          </span>
+        </Tooltip>
         <SlopeControlTemp
           key={position}
           anchor={0}
@@ -25,12 +42,14 @@ export function SlopeControl({ position }: SlopeControlProps) {
           elevation={slope.values[0]}
           units={units}
         />
-        <SlopeControlPin position={position} anchor={0} />
+        {/* <SlopeControlPin position={position} anchor={0} /> */}
       </div>
-      <div className="popup-control-row" data-tour-id="elevation-control">
-        <div className="popup-control-label" style={{ marginRight: '0.25em' }}>
-          right
-        </div>
+      <div className="popup-control-row">
+        <Tooltip label={rightLabel} placement="left" role="label">
+          <span className="popup-control-icon">
+            <Icon name="slope-right" size="30" stroke="1.5" />
+          </span>
+        </Tooltip>
         <SlopeControlTemp
           key={position}
           anchor={1}
@@ -38,8 +57,8 @@ export function SlopeControl({ position }: SlopeControlProps) {
           elevation={slope.values[1]}
           units={units}
         />
-        <SlopeControlPin position={position} anchor={1} />
+        {/* <SlopeControlPin position={position} anchor={1} /> */}
       </div>
-    </>
+    </div>
   )
 }
