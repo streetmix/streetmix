c35a92385069b4323745b65fc64dee4860b67430
Lou Huang <lou@streetmix.net>
2026-07-13 11:04:53 -0400

    chore(util): remove nanoid import workaround

--- client/src/util/random.ts
+++ client/src/util/random.ts
@@ -1,8 +1,6 @@
-// Importing * instead of `import { nanoid }` is a workaround to force Parcel
-// not to tree-shake this library, which can lead to build failures
-import * as nanoid from 'nanoid'
+import { nanoid } from 'nanoid'
 
 // Returns a random string for seeding.
-export function generateRandSeed (): string {
-  return nanoid.nanoid()
+export function generateRandSeed(): string {
+  return nanoid()
 }
