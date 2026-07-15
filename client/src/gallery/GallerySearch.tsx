e15144f603f7880039a67ffb240bcf85dbe472a6
Lou Huang <lou@streetmix.net>
2026-07-15 08:29:42 -0400

    feat(GallerySearch): add i18n strings

--- client/src/gallery/GallerySearch.tsx
+++ client/src/gallery/GallerySearch.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { useIntl } from 'react-intl'
 
 import { Icon } from '~/src/ui/Icon.js'
 import { Tooltip } from '~/src/ui/Tooltip.js'
@@ -8,10 +9,7 @@ import './GallerySearch.css'
 export function GallerySearch() {
   const [input, setInput] = useState<string>('')
   const inputRef = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    inputRef.current?.focus()
-  }, [])
+  const intl = useIntl()
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInput(event.target.value)
@@ -28,13 +26,21 @@ export function GallerySearch() {
       <input
         ref={inputRef}
         type="text"
-        placeholder="Find in streets"
+        placeholder={intl.formatMessage({
+          id: 'gallery.search.label',
+          defaultMessage: 'Find in streets',
+        })}
         value={input}
         onChange={handleChange}
       />
       <div className="gallery-search-clear">
         {input && (
-          <Tooltip label="Clear">
+          <Tooltip
+            label={intl.formatMessage({
+              id: 'gallery.search.clear',
+              defaultMessage: 'Clear',
+            })}
+          >
             <button onClick={handleClear}>
               <Icon name="close" size="14" />
             </button>
