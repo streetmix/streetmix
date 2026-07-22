dea4fb77d21f9d083c862318454332d053ec989f
Lou Huang <lou@streetmix.net>
2026-07-22 12:12:16 -0400

    test(BlockingError): remove obsolete mock

--- client/src/app/BlockingError.test.tsx
+++ client/src/app/BlockingError.test.tsx
@@ -2,18 +2,6 @@ import { render } from '~/test/helpers/render.js'
 import { BlockingError } from './BlockingError.js'
 import { ERRORS } from './errors.js'
 
-// Mocking this prevent a frequently flaky test outcome where redux-toolkit
-// tries to run `cancelAnimationFrame` internally, which is no longer defined,
-// and then the test fails.
-vi.mock('../store/services/api.js', async () => {
-  const actual = await vi.importActual('../store/services/api.js')
-
-  return {
-    ...actual,
-    useGetUserQuery: vi.fn(() => ({ data: null })),
-  }
-})
-
 function getInitialState(errorType: number | null): object {
   return {
     errors: {
