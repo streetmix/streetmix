65b2943a6a7dd4d8ee65699f74ebde89e50ed1f3
Lou Huang <lou@streetmix.net>
2026-09-02 16:26:50 -0400

    feat(WelcomeCoastmix): update content a lil

--- client/src/app/WelcomePanel/WelcomeCoastmix.tsx
+++ client/src/app/WelcomePanel/WelcomeCoastmix.tsx
@@ -1,5 +1,7 @@
+import titleImage from 'url:./welcome-coastmix.png'
 import { CoastmixOnboardingTour } from '~/src/ui/Tours/CoastmixOnboarding.js'
 import { Button } from '~/src/ui/Button.js'
+import './WelcomeCoastmix.css'
 
 interface WelcomeCoastmixProps {
   handleDismiss: React.MouseEventHandler
@@ -15,17 +17,31 @@ export function WelcomeCoastmix({ handleDismiss }: WelcomeCoastmixProps) {
   }
 
   return (
-    <div className="welcome-panel-content">
+    <div className="welcome-panel-content welcome-coastmix">
       <h1>Welcome to Coastmix!</h1>
+      <img
+        src={titleImage}
+        width="480"
+        alt="Welcome to Coastmix!"
+        draggable={false}
+      />
 
-      <p style={{ textAlign: 'left' }}>
+      <p>
         Climate change is causing sea level rise and more intense and frequent
         storms. Cities must adapt their waterfronts to prevent flooding and
         protect communities, buildings, infrastructure, and open spaces.
       </p>
-      <p style={{ textAlign: 'left' }}>
-        Learn about coastal resilience planning, explore ways to prevent
-        flooding, and design your own adaptation strategies.{' '}
+      <p>
+        Coastmix was built in partnership between the{' '}
+        <a href="https://boston.gov/" target="_blank" rel="noopener">
+          City of Boston
+        </a>{' '}
+        and the developers of{' '}
+        <a href="https://streetmix.net/" target="_blank" rel="noopener">
+          Streetmix
+        </a>{' '}
+        to teach coastal resilience planning, explore ways to prevent flooding,
+        and design your own adaptation strategies.{' '}
         <strong>Let’s get started!</strong>
       </p>
       <div className="welcome-panel-buttons">
