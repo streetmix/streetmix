38dc235ed3779ab462463caae49157799ab81901
Lou Huang <lou@streetmix.net>
2026-09-17 14:40:59 -0400

    chore(MainScreen): ugh actually fix typo

--- client/src/app/MainScreen.tsx
+++ client/src/app/MainScreen.tsx
@@ -5,7 +5,7 @@ interface MainScreenProps {
 }
 
 // The "main screen" houses all of the main UI of Streetmix that does not
-// include anccilary overlays and banners. All main interactivity of Streetmix
+// include ancillary overlays and banners. All main interactivity of Streetmix
 // should be contained within this. We also use this component to set up some
 // context providers for portal layers.
 export function MainScreen({ children }: MainScreenProps) {
